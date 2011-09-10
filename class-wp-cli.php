f72cb8a6237f0e5c0f200b636042443ca48694a0
Andreas Creten <andreas@madewithlove.be>
2011-09-10 14:51:26 +0200

    Documentation and command line tools
    - Documented all functions and classes - Added php-cli-tools to interact
    with the cli

--- class-wp-cli.php
+++ class-wp-cli.php
@@ -1,9 +1,190 @@
 <?php
 
+/**
+ * Wrapper class for WP-CLI
+ *
+ * @package wp-cli
+ * @author Andreas Creten
+ */
 class WP_CLI {
 	static $commands = array();
-	
+
+	/**
+	 * Add a command to the wp-cli list of commands
+	 *
+	 * @param string $name The name of the command that will be used in the cli
+	 * @param string $class The class to manage the command 
+	 * @return void
+	 * @author Andreas Creten
+	 */
 	public function addCommand($name, $class) {
 		self::$commands[$name] = $class;
 	}
+	
+	/**
+	 * Display a message in the cli
+	 *
+	 * @param string $message 
+	 * @return void
+	 * @author Andreas Creten
+	 */
+	static function out($message) {
+		\cli\out($message);
+	}
+	
+	/**
+	 * Display a message in the CLI and end with a newline
+	 *
+	 * @param string $message 
+	 * @return void
+	 * @author Andreas Creten
+	 */
+	static function line($message = '') {
+		\cli\line($message);
+	}
+	
+	/**
+	 * Display an error in the CLI and end with a newline
+	 *
+	 * @param string $message 
+	 * @param string $label 
+	 * @return void
+	 * @author Andreas Creten
+	 */
+	static function error($message, $label = 'Error') {
+		\cli\line('%R'.$label.': %n'.self::errorToString($message));
+	}
+	
+	/**
+	 * Display a success in the CLI and end with a newline
+	 *
+	 * @param string $message 
+	 * @param string $label 
+	 * @return void
+	 * @author Andreas Creten
+	 */
+	static function success($message, $label = 'Success') {
+		\cli\line('%G'.$label.': %n'.$message);
+	}
+	
+	/**
+	 * Display a warning in the CLI and end with a newline
+	 *
+	 * @param string $message 
+	 * @param string $label 
+	 * @return void
+	 * @author Andreas Creten
+	 */
+	static function warning($message, $label = 'Warning') {
+		\cli\line('%C'.$label.': %n'.$message);
+	}
+	
+	/**
+	 * Convert a wp_error into a String
+	 *
+	 * @param mixed $errors 
+	 * @return string
+	 * @author Andreas Creten
+	 */
+	static function errorToString($errors) {
+	    if(is_string($errors)){
+	        return $errors;
+	    }
+	    elseif(is_wp_error($errors) && $errors->get_error_code()){
+	        foreach($errors->get_error_messages() as $message){
+	            if($errors->get_error_data() )
+	                return $message . ' ' . $errors->get_error_data();
+	            else
+	                return $message;
+	        }
+	    }
+	}
+	
+	/**
+	 * Display the help function for the wp-cli
+	 *
+	 * @return void
+	 * @author Andreas Creten
+	 */
+	static function generalHelp() {
+		self::line('Example usage:');
+		foreach(self::$commands as $name => $command) {
+			self::out('    wp '.$name);
+			$methods = WP_CLI_Command::getMethods($command);
+			if(!empty($methods)) {
+				self::out(' ['.implode('|', $methods).']');
+			}
+			self::line(' ...');
+		}
+	}
+}
+
+/**
+ * A Upgrader Skin for Wordpress that only generates plain-text
+ *
+ * @package wp-cli
+ * @author Andreas Creten
+ */
+class CLI_Upgrader_Skin {
+    var $upgrader;
+    var $done_header = false;
+    var $result = false;
+
+    function __construct($args = array()) {
+        $defaults = array('url' => '', 'nonce' => '', 'title' => '', 'context' => false);
+        $this->options = wp_parse_args($args, $defaults);
+    }
+
+    function set_upgrader(&$upgrader) {
+        if(is_object($upgrader)) {
+			$this->upgrader =& $upgrader;
+		}
+		
+        $this->add_strings();
+    }
+
+    function add_strings() {}
+
+    function set_result($result) {
+        $this->result = $result;
+    }
+
+    function request_filesystem_credentials($error = false) {
+        $url = $this->options['url'];
+        $context = $this->options['context'];
+        if(!empty($this->options['nonce'])) {
+			$url = wp_nonce_url($url, $this->options['nonce']);
+		}
+     	
+        return request_filesystem_credentials($url, '', $error, $context); //Possible to bring inline, Leaving as is for now.
+    }
+
+    function header() {}
+    function footer() {}
+
+    function error($errors) {
+        $this->feedback(WP_CLI::errorToString($errors));
+    }
+
+    function feedback($string) {
+        if(isset( $this->upgrader->strings[$string]))
+            $string = $this->upgrader->strings[$string];
+
+        if(strpos($string, '%') !== false) {
+            $args = func_get_args();
+            $args = array_splice($args, 1);
+            if(!empty($args)) {
+				$string = vsprintf($string, $args);
+			}
+                
+        }
+        if(empty($string)) {
+			return;
+		}
+
+        echo $string;
+    }
+	
+    function before() {}
+    function after() {}
 }
--- wp-cli.php
+++ wp-cli.php
@@ -5,16 +5,22 @@ if(PHP_SAPI !== 'cli') {
 	die('Only cli access');
 }
 
+// Define the Wordpress location
 define('WP_ROOT', '../wordpress/');
 
+// Set a constant that can be used to check if we are running wp-cli or not
+define('WP_CLI', true);
+
+// Include the wp-cli classes
 include 'class-wp-cli.php';
 include 'class-wp-cli-command.php';
 
+// Include the command line tools, taken from here: https://github.com/jlogsdon/php-cli-tools
+include 'php-cli-tools/lib/cli/cli.php';
+\cli\register_autoload();
+
 // Taken from https://github.com/88mph/wpadmin/blob/master/wpadmin.php
-
-// Does the user have access to read the directory? If so, allow them to use the
-// command line tool.
-
+// Does the user have access to read the directory? If so, allow them to use the command line tool.
 if(true == is_readable(WP_ROOT . 'wp-load.php')){
     // Load WordPress libs.
     require_once(WP_ROOT . 'wp-load.php');
@@ -22,25 +28,46 @@ if(true == is_readable(WP_ROOT . 'wp-load.php')){
     require_once(ABSPATH . 'wp-admin/includes/admin.php');
 }
 else {
-    die("Either this is not a WordPress document root or you do not have permission to administer this site. \n");
+	WP_CLI::error('Either this is not a WordPress document root or you do not have permission to administer this site.');
+    exit();
 }
 
-foreach (glob('library/commands/*.php') as $filename)  {
+// Load all internal commands
+foreach (glob('commands/internals/*.php') as $filename)  {
     include $filename;
 }
 
-// Start output buffering to stop WordPress from spitting out its usual output.
+// Load all plugin commands
+foreach (glob('commands/plugins/*.php') as $filename)  {
+    include $filename;
+}
+
+// Load all theme commands
+foreach (glob('commands/themes/*.php') as $filename)  {
+    include $filename;
+}
+
+// Get the cli arguments
+$arguments = $GLOBALS['argv'];
 
 // Remove the first entry
-$arguments = array_shift($GLOBALS['argv']);
+array_shift($arguments);
 
 // Get the command
-$command = array_shift($GLOBALS['argv']);
+$command = array_shift($arguments);
 
-//
-if(isset(WP_CLI::$commands[$command])) {
-	new WP_CLI::$commands[$command]($GLOBALS['argv']);
+// Check if there are commands installed
+if(empty(WP_CLI::$commands)) {
+	WP_CLI::error('No commands installed');
+	WP_CLI::line();
+	WP_CLI::line('Visit the wp-cli page on github on more information on how to install commands.');
+	exit();
 }
+// Try to load the class, otherwise it's an Unknown command
+elseif(isset(WP_CLI::$commands[$command])) {
+	new WP_CLI::$commands[$command]($arguments);
+}
+// Show the general help for wp-cli
 else {
-	echo 'Unknown command'."\n";
+	WP_CLI::generalHelp();
 }
