82df3186f3eecbebd83248849dc231b2aa7ea072
Andreas Creten <andreas@madewithlove.be>
2011-09-28 09:53:03 +0200

    Example and README update

--- class-wp-cli-command.php
+++ class-wp-cli-command.php
@@ -7,81 +7,81 @@
  * @author Andreas Creten
  */
 abstract class WP_CLI_Command {
-	/**
-	 * Construct for this class, transfers the cli arguments to the right class
-	 *
-	 * @param Array $args
-	 * @author Andreas Creten
-	 */
-	function __construct($args = array()) {
-		// The first command is the sub command
-		$sub_command = array_shift($args);
-		
-		// If the method exists, try to load it
-		if(method_exists($this, $sub_command)) {
-			$this->$sub_command($args);
-		}
-		// Otherwise, show the help for this command
-		else {
-			$this->help($args);
-		}
-	}
-	
-	/**
-	 * General help function for this command
-	 *
-	 * @param Array $args 
-	 * @return void
-	 * @author Andreas Creten
-	 */
-	public function help($args = array()) {
-		// Get the cli arguments
-		$arguments = $GLOBALS['argv'];
-		
-		// Remove the first entry
-		array_shift($arguments);
+    /**
+     * Construct for this class, transfers the cli arguments to the right class
+     *
+     * @param Array $args
+     * @author Andreas Creten
+     */
+    function __construct($args = array()) {
+        // The first command is the sub command
+        $sub_command = array_shift($args);
+        
+        // If the method exists, try to load it
+        if(method_exists($this, $sub_command)) {
+            $this->$sub_command($args);
+        }
+        // Otherwise, show the help for this command
+        else {
+            $this->help($args);
+        }
+    }
+    
+    /**
+     * General help function for this command
+     *
+     * @param Array $args 
+     * @return void
+     * @author Andreas Creten
+     */
+    public function help($args = array()) {
+        // Get the cli arguments
+        $arguments = $GLOBALS['argv'];
+        
+        // Remove the first entry
+        array_shift($arguments);
 
-		// Get the command
-		$used_command = array_shift($arguments);
-		
-		// Show the list of sub-commands for this command
-		WP_CLI::line('Example usage:');
-		WP_CLI::out('    wp '.$used_command);
-		$methods = WP_CLI_Command::getMethods($this);
-		if(!empty($methods)) {
-			WP_CLI::out(' ['.implode('|', $methods).']');
-		}
-		WP_CLI::line(' ...');
-		WP_CLI::line();
-		
-		// Send a warning to the user because there is no custom help function defined in the command
-		// Make usure there always is a help method in your command class
-		WP_CLI::warning('The command has no dedicated help function, ask the creator to fix it.');
-	}
-	
-	/**
-	 * Get the filtered list of methods for a class
-	 *
-	 * @param string $class
-	 * @return Array The list of methods
-	 * @author Andreas Creten
-	 */
-	static function getMethods($class) {
-		// Methods that don't need to be included in the method list
-		$blacklist = array('__construct', 'getMethods');
-		
-		// Get all the methods of the class
-		$methods = get_class_methods($class);
-		
-		// Remove the blacklisted methods
-		foreach($blacklist as $method) {
-			$in_array = array_search($method, $methods);
-			if($in_array !== false) {
-				unset($methods[$in_array]);
-			}
-		}
-		
-		// Only return the values, to fill up the gaps
-		return array_values($methods);
-	}
+        // Get the command
+        $used_command = array_shift($arguments);
+        
+        // Show the list of sub-commands for this command
+        WP_CLI::line('Example usage:');
+        WP_CLI::out('    wp '.$used_command);
+        $methods = WP_CLI_Command::getMethods($this);
+        if(!empty($methods)) {
+            WP_CLI::out(' ['.implode('|', $methods).']');
+        }
+        WP_CLI::line(' ...');
+        WP_CLI::line();
+        
+        // Send a warning to the user because there is no custom help function defined in the command
+        // Make usure there always is a help method in your command class
+        WP_CLI::warning('The command has no dedicated help function, ask the creator to fix it.');
+    }
+    
+    /**
+     * Get the filtered list of methods for a class
+     *
+     * @param string $class
+     * @return Array The list of methods
+     * @author Andreas Creten
+     */
+    static function getMethods($class) {
+        // Methods that don't need to be included in the method list
+        $blacklist = array('__construct', 'getMethods');
+        
+        // Get all the methods of the class
+        $methods = get_class_methods($class);
+        
+        // Remove the blacklisted methods
+        foreach($blacklist as $method) {
+            $in_array = array_search($method, $methods);
+            if($in_array !== false) {
+                unset($methods[$in_array]);
+            }
+        }
+        
+        // Only return the values, to fill up the gaps
+        return array_values($methods);
+    }
 }
--- class-wp-cli.php
+++ class-wp-cli.php
@@ -87,17 +87,17 @@ class WP_CLI {
 	 * @author Andreas Creten
 	 */
 	static function errorToString($errors) {
-	    if(is_string($errors)){
-	        return $errors;
-	    }
-	    elseif(is_wp_error($errors) && $errors->get_error_code()){
-	        foreach($errors->get_error_messages() as $message){
-	            if($errors->get_error_data() )
-	                return $message . ' ' . $errors->get_error_data();
-	            else
-	                return $message;
-	        }
-	    }
+		if(is_string($errors)){
+			return $errors;
+		}
+		elseif(is_wp_error($errors) && $errors->get_error_code()){
+			foreach($errors->get_error_messages() as $message){
+				if($errors->get_error_data() )
+					return $message . ' ' . $errors->get_error_data();
+				else
+					return $message;
+			}
+		}
 	}
 	
 	/**
@@ -109,7 +109,7 @@ class WP_CLI {
 	static function generalHelp() {
 		self::line('Example usage:');
 		foreach(self::$commands as $name => $command) {
-			self::out('    wp '.$name);
+			self::out('	wp '.$name);
 			$methods = WP_CLI_Command::getMethods($command);
 			if(!empty($methods)) {
 				self::out(' ['.implode('|', $methods).']');
@@ -126,65 +126,66 @@ class WP_CLI {
  * @author Andreas Creten
  */
 class CLI_Upgrader_Skin {
-    var $upgrader;
-    var $done_header = false;
-    var $result = false;
+	var $upgrader;
+	var $done_header = false;
+	var $result = false;
 
-    function __construct($args = array()) {
-        $defaults = array('url' => '', 'nonce' => '', 'title' => '', 'context' => false);
-        $this->options = wp_parse_args($args, $defaults);
-    }
-
-    function set_upgrader(&$upgrader) {
-        if(is_object($upgrader)) {
+	function __construct($args = array()) {
+		$defaults = array('url' => '', 'nonce' => '', 'title' => '', 'context' => false);
+		$this->options = wp_parse_args($args, $defaults);
+	}
+	
+	function set_upgrader(&$upgrader) {
+		if(is_object($upgrader)) {
 			$this->upgrader =& $upgrader;
 		}
 		
-        $this->add_strings();
-    }
-
-    function add_strings() {}
-
-    function set_result($result) {
-        $this->result = $result;
-    }
-
-    function request_filesystem_credentials($error = false) {
-        $url = $this->options['url'];
-        $context = $this->options['context'];
-        if(!empty($this->options['nonce'])) {
+		$this->add_strings();
+	}
+	
+	function add_strings() {}
+		
+	function set_result($result) {
+		$this->result = $result;
+	}
+	
+	function request_filesystem_credentials($error = false) {
+		$url = $this->options['url'];
+		$context = $this->options['context'];
+		if(!empty($this->options['nonce'])) {
 			$url = wp_nonce_url($url, $this->options['nonce']);
 		}
-     	
-        return request_filesystem_credentials($url, '', $error, $context); //Possible to bring inline, Leaving as is for now.
-    }
+		
+		// Possible to bring inline, Leaving as is for now.
+		return request_filesystem_credentials($url, '', $error, $context);
+	}
+	
+	function header() {}
+	function footer() {}
+		
+	function error($errors) {
+		$this->feedback(WP_CLI::errorToString($errors));
+	}
 
-    function header() {}
-    function footer() {}
-
-    function error($errors) {
-        $this->feedback(WP_CLI::errorToString($errors));
-    }
-
-    function feedback($string) {
-        if(isset( $this->upgrader->strings[$string]))
-            $string = $this->upgrader->strings[$string];
-
-        if(strpos($string, '%') !== false) {
-            $args = func_get_args();
-            $args = array_splice($args, 1);
-            if(!empty($args)) {
+	function feedback($string) {
+		if(isset( $this->upgrader->strings[$string]))
+			$string = $this->upgrader->strings[$string];
+		
+		if(strpos($string, '%') !== false) {
+			$args = func_get_args();
+			$args = array_splice($args, 1);
+			if(!empty($args)) {
 				$string = vsprintf($string, $args);
 			}
-                
-        }
-        if(empty($string)) {
+			
+		}
+		if(empty($string)) {
 			return;
 		}
-
-        echo $string;
-    }
+		
+		echo $string;
+	}
 	
-    function before() {}
-    function after() {}
+	function before() {}
+	function after() {}
 }
