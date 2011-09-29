25f330df7dac439afd1e72d936a21a943b0a1212
Andreas Creten <andreas@madewithlove.be>
2011-09-29 15:23:10 +0200

    Removed todo's from readme

--- wp-cli.php
+++ wp-cli.php
@@ -7,10 +7,10 @@ if(PHP_SAPI !== 'cli') {
 
 // Define the WordPress location
 if(is_readable($_SERVER['PWD'] . '/../wp-load.php')) {
-    define('WP_ROOT', $_SERVER['PWD'] . '/../');
+	define('WP_ROOT', $_SERVER['PWD'] . '/../');
 }
 else {
-    define('WP_ROOT', $_SERVER['PWD'] . '/');
+	define('WP_ROOT', $_SERVER['PWD'] . '/');
 }
 
 // Define the wp-cli location
@@ -30,24 +30,24 @@ include WP_CLI_ROOT.'php-cli-tools/lib/cli/cli.php';
 // Taken from https://github.com/88mph/wpadmin/blob/master/wpadmin.php
 // Does the user have access to read the directory? If so, allow them to use the command line tool.
 if(true == is_readable(WP_ROOT . 'wp-load.php')){
-    // Load WordPress libs.
-    require_once(WP_ROOT . 'wp-load.php');
-    require_once(ABSPATH . WPINC . '/template-loader.php');
-    require_once(ABSPATH . 'wp-admin/includes/admin.php');
+	// Load WordPress libs.
+	require_once(WP_ROOT . 'wp-load.php');
+	require_once(ABSPATH . WPINC . '/template-loader.php');
+	require_once(ABSPATH . 'wp-admin/includes/admin.php');
 }
 else {
 	WP_CLI::error('Either this is not a WordPress document root or you do not have permission to administer this site.');
-    exit();
+	exit();
 }
 
 // Load all internal commands
 foreach (glob(WP_CLI_ROOT.'/commands/internals/*.php') as $filename)  {
-    include $filename;
+	include $filename;
 }
 
 // Load all plugin commands
 foreach (glob(WP_CLI_ROOT.'/commands/community/*.php') as $filename)  {
-    include $filename;
+	include $filename;
 }
 
 // Get the cli arguments
