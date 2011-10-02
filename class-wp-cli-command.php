f681763bdc665bc6c6612dc06d258af487157974
scribu <mail@scribu.net>
2011-10-03 00:50:34 +0300

    forgot that the help method could be called explicitly

--- class-wp-cli-command.php
+++ class-wp-cli-command.php
@@ -8,28 +8,34 @@
  */
 abstract class WP_CLI_Command {
 
+	/**
+	 * Keeps a reference to the current command name
+	 *
+	 * @param string
+	 */
+	protected $command;
+
     /**
      * Construct for this class, transfers the cli arguments to the right class
      *
      * @param Array $args
      */
     function __construct( $command, $args, $assoc_args ) {
+		$this->command = $command;
+
         // The first command is the sub command
         $sub_command = array_shift($args);
 
-        // If the method exists, try to load it
-        if(method_exists($this, $sub_command)) {
-            $this->$sub_command($args, $assoc_args);
-        }
-        // If a dummy method exists, use it. This if for reserved keywords in php (like list, isset)
-        elseif(method_exists($this, '_'.$sub_command)) {
-            $sub_command = '_'.$sub_command;
-            $this->$sub_command($args, $assoc_args);
-        }
-        // Otherwise, show the help for this command
-        else {
-            $this->help($command, $sub_command);
-        }
+		if ( !method_exists($this, $sub_command) ) {
+			// This if for reserved keywords in php (like list, isset)
+			$sub_command = '_'.$sub_command;
+		}
+
+		if ( !method_exists($this, $sub_command) ) {
+			$sub_command = 'help';
+		}
+
+		$this->$sub_command($args, $assoc_args);
     }
 
     /**
@@ -39,10 +45,10 @@ abstract class WP_CLI_Command {
      * @param string $sub_command
      * @return void
      */
-    public function help( $command, $sub_command = false ) {
+    public function help( $args, $assoc_args ) {
         // Show the list of sub-commands for this command
         WP_CLI::line('Example usage:');
-        WP_CLI::out('    wp '.$command);
+        WP_CLI::out('    wp '.$this->command);
 
         $methods = WP_CLI_Command::getMethods($this);
         if(!empty($methods)) {
