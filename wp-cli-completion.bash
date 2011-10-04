bd762babc93a0892cc9b9adb470e444bc37d5b56
scribu <mail@scribu.net>
2011-10-04 04:05:38 +0300

    remove unneeded assignment of COMPREPLY

--- wp-cli-completion.bash
+++ wp-cli-completion.bash
@@ -1,11 +1,12 @@
-# Very basic code completion
-# 
-# Code taken from here: http://www.debian-administration.org/article/317/An_introduction_to_bash_completion_part_2
+#!bash
+#
+# bash completion support for the wp command
+#
 _wp() {
 	local cur prev opts
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD-1]}
 
 	if [[ 'wp' = $prev ]]; then
 		opts=$(wp --completions | cut -d ' ' -f 1 | tr '\n' ' ')
