69f8f00958f3426a683a7070705eb38e14512e97
Andreas Creten <andreas@madewithlove.be>
2011-10-07 10:35:21 +0200

    Pushed to version 0.2

--- wp-cli.php
+++ wp-cli.php
@@ -5,7 +5,7 @@ if(PHP_SAPI !== 'cli') {
 	die('Only cli access');
 }
 
-define( 'WP_CLI_VERSION', '0.1' );
+define( 'WP_CLI_VERSION', '0.2' );
 
 // Define the wp-cli location
 define('WP_CLI_ROOT', __DIR__ . '/');
