3eef8c0009cee8f97414d241ca01f981f72769f6
scribu <mail@scribu.net>
2011-10-12 22:28:04 +0300

    make `version` a subcommand of `core`. see #31

--- commands/internals/core.php
+++ commands/internals/core.php
@@ -12,6 +12,40 @@ WP_CLI::addCommand('core', 'CoreCommand');
 class CoreCommand extends WP_CLI_Command {
 
 	/**
+	 * Display the WordPress version.
+	 */
+	public function version( $args = array(), $assoc_args = array() ) {
+		global $wp_version, $wp_db_version, $tinymce_version, $manifest_version;
+
+		$color = '%G';
+		$version_text = $wp_version;
+		$version_types = array(
+			'-RC' => array( 'release candidate', '%y' ),
+			'-beta' => array( 'beta', '%B' ),
+			'-' => array( 'in development', '%R' ),
+		);
+		foreach( $version_types as $needle => $type ) {
+			if ( stristr( $wp_version, $needle ) ) {
+				list( $version_text, $color ) = $type;
+				$version_text = "$color$wp_version%n (stability: $version_text)";
+				break;
+			}
+		}
+		WP_CLI::line( "WordPress version:\t$version_text" );
+
+		if ( isset( $assoc_args['extra'] ) ) {
+			WP_CLI::line();
+			WP_CLI::line( "Database revision:\t$wp_db_version" );
+
+			preg_match( '/(\d)(\d+)-/', $tinymce_version, $match );
+			$human_readable_tiny_mce = $match? $match[1] . '.' . $match[2] : '';
+			WP_CLI::line( "TinyMCE version:\t"  . ( $human_readable_tiny_mce? "$human_readable_tiny_mce ($tinymce_version)" : $tinymce_version ) );
+
+			WP_CLI::line( "Manifest revision:\t$manifest_version" );
+		}
+	}
+
+	/**
 	 * Update the WordPress core
 	 *
 	 * @param array $args
