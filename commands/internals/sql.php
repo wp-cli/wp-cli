40400c4d07ba053916aa67906ce9864816e5bc47
scribu <mail@scribu.net>
2011-10-27 23:29:28 +0300

    some string fixes. see #38

--- commands/internals/sql.php
+++ commands/internals/sql.php
@@ -29,7 +29,7 @@ class SqlCommand extends WP_CLI_Command {
   /**
    * A string for connecting to the DB.
    *
-   * @param string $args 
+   * @param string $args
    * @return void
    */
   function connect( $args = array() ) {
@@ -38,19 +38,19 @@ class SqlCommand extends WP_CLI_Command {
   }
 
   /**
-   * Open a SQL command-line interface using Wordpress's credentials. 
-   * @param string $args 
+   * Open a SQL command-line interface using WordPress's credentials.
+   * @param string $args
    * @return void
    */
   function cli() {
     $exec = $this->connect_string();
 
     proc_close( proc_open( $exec , array( 0 => STDIN, 1 => STDOUT, 2 => STDERR ), $pipes ) );
-  } 
+  }
 
   /**
-   * Exports the wordpress DB as SQL using mysqldump or equivalent.
-   * @param string $args 
+   * Exports the WordPress DB as SQL using mysqldump or equivalent.
+   * @param string $args
    * @return void
    */
   function dump( $args, $assoc_args ) {
@@ -63,11 +63,11 @@ class SqlCommand extends WP_CLI_Command {
     $exec = sprintf( 'mysqldump %s --result-file %s --user=%s --password=%s', DB_NAME, $result_file, DB_USER, DB_PASSWORD );
 
     exec( $exec );
-  } 
-  
+  }
+
   /**
    * Execute a query against the site database.
-   * @param string $args 
+   * @param string $args
    * @return void
    */
 
@@ -90,10 +90,10 @@ class SqlCommand extends WP_CLI_Command {
 	 */
 	public static function help() {
 		WP_CLI::line( <<<EOB
-wp sql cli               Open a SQL command-line interface using Wordpress's credentials.
-wp sql connect           A string for connecting to the DB.            
-wp sql dump              Exports the wordpress DB as SQL using mysqldump or equivalent. 
-wp sql query             Execute a query against the site database. 
+wp sql cli               Open a SQL command-line interface using the WordPress credentials.
+wp sql connect           A string for connecting to the database.
+wp sql dump              Exports the WordPress database as SQL using mysqldump or equivalent.
+wp sql query             Execute a query against the WordPress database.
 EOB
 		);
 	}
