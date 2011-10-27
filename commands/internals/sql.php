b22dc21ebb12bd146094c5a7308e052e65f1bc29
Edgar Marca <matiskay@gmail.com>
2011-10-27 11:40:10 -0500

    Following wordpress coding standards

--- commands/internals/sql.php
+++ commands/internals/sql.php
@@ -9,7 +9,7 @@
 
 
 // Add the command to the wp-cli
-WP_CLI::addCommand('sql', 'SqlCommand');
+WP_CLI::addCommand( 'sql', 'SqlCommand' );
 
 /**
  * Implement sql command
@@ -39,9 +39,9 @@ class SqlCommand extends WP_CLI_Command {
    * @return void
    */
   function cli() {
-    $exec = sprintf('mysql --user=%s --password=%s', DB_USER, DB_PASSWORD);
+    $exec = sprintf( 'mysql --user=%s --password=%s', DB_USER, DB_PASSWORD );
 
-    proc_close( proc_open($exec , array(0 => STDIN, 1 => STDOUT, 2 => STDERR), $pipes) );
+    proc_close( proc_open( $exec , array( 0 => STDIN, 1 => STDOUT, 2 => STDERR ), $pipes ) );
   } 
 
   /**
@@ -50,14 +50,14 @@ class SqlCommand extends WP_CLI_Command {
    * @return void
    */
   function dump( $args, $assoc_args ) {
-    if ( !isset($assoc_args['file']) ) {
-      $result_file = sprintf('%s.sql', DB_NAME);
+    if ( !isset( $assoc_args['file'] ) ) {
+      $result_file = sprintf( '%s.sql', DB_NAME );
     } else {
       $result_file = $assoc_args['file'];
     }
 
-    $exec = sprintf('mysqldump %s --result-file %s --user=%s --password=%s', DB_NAME, $result_file, DB_USER, DB_PASSWORD);
-    exec($exec);
+    $exec = sprintf( 'mysqldump %s --result-file %s --user=%s --password=%s', DB_NAME, $result_file, DB_USER, DB_PASSWORD );
+    exec( $exec );
   } 
   
 }
