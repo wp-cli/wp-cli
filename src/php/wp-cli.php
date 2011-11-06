bb81b64fb39192c5a0746307f256c0e809bcb2e2
scribu <mail@scribu.net>
2011-11-06 03:18:32 +0200

    add pear package files

--- src/php/wp-cli.php
+++ src/php/wp-cli.php
@@ -4,7 +4,7 @@ if(PHP_SAPI !== 'cli') {
 	die('Only cli access');
 }
 
-define( 'WP_CLI_VERSION', '0.2' );
+define( 'WP_CLI_VERSION', '1.0.0' );
 
 // Define the wp-cli location
 define( 'WP_CLI_ROOT', __DIR__ . '/' );
