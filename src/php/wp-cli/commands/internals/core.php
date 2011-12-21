5d4cd7823d80a823c2a83f036a1a84aff0c656d3
Michael Williamson <michael.williamson@red-gate.com>
2011-12-21 14:06:04 +0000

    Move installation commands to core

--- src/php/wp-cli/commands/internals/core.php
+++ src/php/wp-cli/commands/internals/core.php
@@ -2,6 +2,8 @@
 
 WP_CLI::addCommand('core', 'CoreCommand');
 
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
 /**
  * Implement core command
  *
@@ -73,6 +75,38 @@ class CoreCommand extends WP_CLI_Command {
 			WP_CLI::success('WordPress upgraded successfully.');
 		}
 	}
+    
+    public function install( $args, $assoc_args ) {
+        if ( is_blog_installed() ) {
+            WP_CLI::error( 'Wordpress is already installed.' );
+            exit( 1 );
+        }
+        $site_title = $assoc_args["site_title"];
+        $username = $assoc_args["username"];
+        $admin_email = $assoc_args["email_address"];
+        $public = true;
+        $admin_password = $assoc_args["password"];
+        
+        if ( ! $site_title || ! $username || ! $admin_email || ! $admin_password ) {
+            WP_CLI::error( 'Missing installation arguments' );
+            exit( 1 );
+        }
+        
+        $result = wp_install( $site_title, $username, $admin_email, $public, '', $admin_password );
+        if ( is_wp_error( $result ) ) {
+            WP_CLI::error( 'Installation failed (' . WP_CLI::errorToString($result) . ').' );
+        } else {
+            WP_CLI::success( 'WordPress installed successfully.' );
+        }
+    }
+
+    public function is_installed( $filename ) {
+        if ( is_blog_installed() ) {
+            exit( 0 );
+        } else {
+            exit( 1 );
+        }
+    }
 
 	/**
 	 * Help function for this command
@@ -81,6 +115,8 @@ class CoreCommand extends WP_CLI_Command {
 		WP_CLI::line( <<<EOB
 usage: wp core update
    or: wp core version [--extra]
+   or: wp core install --site_title=<site-title> --username=<username> --password=<password> --email_address=<email-address>
+   or: wp core is_installed
 EOB
 	);
 	}
