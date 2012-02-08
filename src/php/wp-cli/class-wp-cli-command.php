f94c2b17fc1d3bce76750e76523f97bb542fbccd
scribu <mail@scribu.net>
2012-02-08 07:31:28 +0200

    fix indentation

--- src/php/wp-cli/class-wp-cli-command.php
+++ src/php/wp-cli/class-wp-cli-command.php
@@ -11,8 +11,8 @@ abstract class WP_CLI_Command {
 
 	/**
 	 * Transfers the handling to the appropriate method
-     *
-     * @param array $args
+	 *
+	 * @param array $args
 	 * @param array $assoc_args
 	 */
 	public function __construct( $args, $assoc_args ) {
@@ -23,7 +23,7 @@ abstract class WP_CLI_Command {
 
 		if ( !method_exists( $this, $subcommand ) ) {
 			// This if for reserved keywords in php (like list, isset)
-			$subcommand = '_'.$subcommand;
+			$subcommand = '_' . $subcommand;
 		}
 
 		if ( !method_exists( $this, $subcommand ) || isset( $assoc_args[ 'help' ] ) ) {
@@ -31,18 +31,18 @@ abstract class WP_CLI_Command {
 		}
 
 		$this->$subcommand( $args, $assoc_args );
-    }
+	}
 
-    /**
-     * Get the list of subcommands for a class.
-     *
-     * @param string $class
-     * @return array The list of methods
-     */
-    static function get_subcommands( $class ) {
+	/**
+	 * Get the list of subcommands for a class.
+	 *
+	 * @param string $class
+	 * @return array The list of methods
+	 */
+	static function get_subcommands( $class ) {
 		$reflection = new ReflectionClass( $class );
 
-        $methods = array();
+		$methods = array();
 
 		foreach ( $reflection->getMethods() as $method ) {
 			if ( $method->isPublic() && !$method->isStatic() && !$method->isConstructor() ) {
@@ -54,8 +54,8 @@ abstract class WP_CLI_Command {
 
 				$methods[] = $name;
 			}
-        }
+		}
 
 		return $methods;
-    }
+	}
 }
