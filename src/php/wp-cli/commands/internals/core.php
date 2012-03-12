bd9bef64dc7e5d2d03687fcbdc31c889c57ac2e5
scribu <mail@scribu.net>
2012-03-12 17:14:05 +0200

    move core download and core config to core.php

--- src/php/wp-cli/commands/internals/core.php
+++ src/php/wp-cli/commands/internals/core.php
@@ -11,6 +11,83 @@ WP_CLI::addCommand('core', 'CoreCommand');
 class CoreCommand extends WP_CLI_Command {
 
 	/**
+	 * Download the core files from wordpress.org
+	 */
+	public function download( $args, $assoc_args ) {
+		if ( is_readable( WP_ROOT . 'wp-load.php' ) )
+			WP_CLI::error( 'WordPress files seem to already be present here.' );
+
+		if (isset($assoc_args['path']))
+			$docroot = $assoc_args['path'];
+		else
+			$docroot = './';
+
+		WP_CLI::line('Downloading WordPress...');
+		exec("curl http://wordpress.org/latest.zip > /tmp/wordpress.zip");
+		exec("unzip /tmp/wordpress.zip");
+		exec("mv wordpress/* $docroot");
+		exec("rm -r wordpress");
+		WP_CLI::success('WordPress downloaded.');
+	}
+
+	/**
+	 * Set up a wp-config.php file.
+	 */
+	public function config( $args, $assoc_args ) {
+		$_POST['dbname'] = $assoc_args['name'];
+		$_POST['uname'] = $assoc_args['user'];
+		$_POST['pwd'] = $assoc_args['pass'];
+		$_POST['dbhost'] = isset( $assoc_args['host'] ) ? $assoc_args['host'] : 'localhost';
+		$_POST['prefix'] = isset( $assoc_args['prefix'] ) ? $assoc_args['prefix'] : 'wp_';
+
+		$_GET['step'] = 2;
+		require WP_ROOT . '/wp-admin/setup-config.php';
+	}
+
+	/**
+	 * Run wp_install. Assumes that wp-config.php is already in place.
+	 */
+	public function install( $args, $assoc_args ) {
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		if ( is_blog_installed() ) {
+			WP_CLI::error( 'WordPress is already installed.' );
+		}
+
+		extract( wp_parse_args( $assoc_args, array(
+			'site_url' => defined( 'WP_SITEURL' ) ? WP_SITEURL : '',
+			'site_title' => '',
+			'admin_name' => 'admin',
+			'admin_email' => '',
+			'admin_password' => ''
+		) ), EXTR_SKIP );
+
+		$missing = false;
+		foreach ( array( 'site_url', 'site_title', 'admin_email', 'admin_password' ) as $required_arg ) {
+			if ( empty( $$required_arg ) ) {
+				WP_CLI::warning( "missing --$required_arg parameter" );
+				$missing = true;
+			}
+		}
+
+		if ( $site_url )
+			WP_CLI::set_url_params( $site_url );
+
+		if ( $missing )
+			exit(1);
+
+		$public = true;
+
+		$result = wp_install( $site_title, $admin_name, $admin_email, $public, '', $admin_password );
+
+		if ( is_wp_error( $result ) ) {
+			WP_CLI::error( 'Installation failed (' . WP_CLI::errorToString($result) . ').' );
+		} else {
+			WP_CLI::success( 'WordPress installed successfully.' );
+		}
+	}
+
+	/**
 	 * Display the WordPress version.
 	 */
 	public function version( $args = array(), $assoc_args = array() ) {
@@ -50,7 +127,7 @@ class CoreCommand extends WP_CLI_Command {
 	 *
 	 * @param array $args
 	 */
-	function update($args) {
+	function update( $args ) {
 		wp_version_check();
 
 		$from_api = get_site_transient( 'update_core' );
@@ -76,49 +153,6 @@ class CoreCommand extends WP_CLI_Command {
 			WP_CLI::success('WordPress updated successfully.');
 		}
 	}
-
-    /**
-     * Run wp_install. Assumes that wp-config.php is already in place.
-     */
-    public function install( $args, $assoc_args ) {
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-        if ( is_blog_installed() ) {
-            WP_CLI::error( 'WordPress is already installed.' );
-        }
-
-		extract( wp_parse_args( $assoc_args, array(
-			'site_url' => defined( 'WP_SITEURL' ) ? WP_SITEURL : '',
-			'site_title' => '',
-			'admin_name' => 'admin',
-			'admin_email' => '',
-			'admin_password' => ''
-		) ), EXTR_SKIP );
-
-		$missing = false;
-		foreach ( array( 'site_url', 'site_title', 'admin_email', 'admin_password' ) as $required_arg ) {
-			if ( empty( $$required_arg ) ) {
-				WP_CLI::warning( "missing --$required_arg parameter" );
-				$missing = true;
-			}
-		}
-
-		if ( $site_url )
-			WP_CLI::set_url_params( $site_url );
-
-		if ( $missing )
-			exit(1);
-
-        $public = true;
-
-        $result = wp_install( $site_title, $admin_name, $admin_email, $public, '', $admin_password );
-
-        if ( is_wp_error( $result ) ) {
-            WP_CLI::error( 'Installation failed (' . WP_CLI::errorToString($result) . ').' );
-        } else {
-            WP_CLI::success( 'WordPress installed successfully.' );
-        }
-    }
 
 	/**
 	 * Help function for this command
