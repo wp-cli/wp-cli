ae947a2f3903c3579d1ec79a8c087fd6b0973942
scribu <mail@scribu.net>
2012-04-27 03:16:52 +0300

    add wp core config example. see #65

--- src/php/wp-cli/commands/internals/core.php
+++ src/php/wp-cli/commands/internals/core.php
@@ -171,6 +171,7 @@ class CoreCommand extends WP_CLI_Command {
 usage: wp core update
    or: wp core version [--extra]
    or: wp core download [--version=1.2.3]
+   or: wp core config --dbname=<name> --dbuser=<user> --dbpass=<password> [--dbhost=localhost] [--dbprefix=wp_]
    or: wp core install --site_url=example.com --site_title=<site-title> [--admin_name=<username>] --admin_password=<password> --admin_email=<email-address>
 EOB
 	);
