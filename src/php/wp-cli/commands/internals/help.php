a49ac68614794c50d7ce9e9f768aece1888744c6
scribu <mail@scribu.net>
2012-05-02 00:35:05 +0300

    move general help text back to the help command

--- src/php/wp-cli/commands/internals/help.php
+++ src/php/wp-cli/commands/internals/help.php
@@ -66,7 +66,18 @@ class Help_Command extends WP_CLI_Command {
 			$this->single_command_help( $name, $command );
 		}
 
-		$this->show_help( 'general' );
+		WP_CLI::line(<<<EOB
+
+See 'wp help <command>' for more information on a specific command.
+
+Global parameters:
+    --user=<id|login>   set the current user
+    --url=<url>         set the current URL
+    --path=<path>       set the current path to the WP install
+    --require=<path>    load a certain file before running the command
+    --version           print wp-cli version
+EOB
+		);
 	}
 
 	private function single_command_help( $name, $command ) {
