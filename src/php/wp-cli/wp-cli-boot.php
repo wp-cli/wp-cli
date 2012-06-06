8c60b54645810bc16e4a9cdb0d705fe7dc13ab27
scribu <mail@scribu.net>
2012-06-06 23:33:52 +0300

    make error message clearer

--- src/php/wp-cli/wp-cli-boot.php
+++ src/php/wp-cli/wp-cli-boot.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( version_compare( PHP_VERSION, '5.3.0', '<' ) ) {
-    printf( "Error: PHP version %s is required to run wp-cli. You are running version %s.\n", '5.3.0', PHP_VERSION );
+    printf( "Error: wp-cli requires PHP %s or newer. You are running version %s.\n", '5.3.0', PHP_VERSION );
 	die(-1);
 }
 
