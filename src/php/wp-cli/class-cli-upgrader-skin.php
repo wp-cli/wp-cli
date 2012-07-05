cc0c525352437a6c5d178c5d7c89b8441583d3f9
scribu <mail@scribu.net>
2012-07-05 23:30:36 +0300

    convert error message into string. see #140

--- src/php/wp-cli/class-cli-upgrader-skin.php
+++ src/php/wp-cli/class-cli-upgrader-skin.php
@@ -16,8 +16,13 @@ class CLI_Upgrader_Skin extends WP_Upgrader_Skin {
 		if ( !$error )
 			return;
 
+		if ( isset( $this->upgrader->strings[$error] ) )
+			$string = $this->upgrader->strings[$error];
+		else
+			$string = $error;
+
 		// TODO: show all errors, not just the first one
-		WP_CLI::warning( $error );
+		WP_CLI::warning( $string );
 	}
 
 	function feedback( $string ) {
