b4579c1a3e7f89638e47a8a6fae74b9fae2aa597
scribu <mail@scribu.net>
2012-10-11 17:33:11 +0300

    add validation for mandatory positional args

--- src/php/wp-cli/dispatcher.php
+++ src/php/wp-cli/dispatcher.php
@@ -65,9 +65,9 @@ function describe_command( $class, $command ) {
 	$i = 0;
 
 	foreach ( $methods as $name => $subcommand ) {
-		$prefix = ( 0 == $i++ ) ? 'usage:' : '   or:';
+		$prefix = ( 0 == $i++ ) ? 'usage: ' : '   or: ';
 
-		\WP_CLI::line( "$prefix wp $command $name " . $subcommand->get_synopsis() );
+		$subcommand->show_usage( $prefix );
 	}
 
 	\WP_CLI::line();
@@ -92,7 +92,7 @@ function get_subcommands( $class ) {
 		if ( !_is_good_method( $method ) )
 			continue;
 
-		$subcommand = new Subcommand( $method );
+		$subcommand = new Subcommand( $method, 'TODO' );
 
 		$subcommands[ $subcommand->get_name() ] = $subcommand;
 	}
@@ -107,8 +107,9 @@ function _is_good_method( $method ) {
 
 class Subcommand {
 
-	function __construct( $method ) {
+	function __construct( $method, $command ) {
 		$this->method = $method;
+		$this->command = $command;
 	}
 
 	function get_name() {
@@ -120,7 +121,19 @@ class Subcommand {
 		return $this->method->name;
 	}
 
-	function get_synopsis() {
+	function show_usage( $prefix = 'usage: ' ) {
+		$name = $this->get_name();
+		$synopsis = $this->get_synopsis();
+
+		\WP_CLI::line( $prefix . "wp $this->command $name $synopsis" );
+	}
+
+	function invoke( $instance, $args, $assoc_args ) {
+		$this->check_args( $args, $assoc_args );
+		return $this->method->invoke( $instance, $args, $assoc_args );
+	}
+
+	protected function get_synopsis() {
 		$comment = $this->method->getDocComment();
 
 		if ( !preg_match( '/@synopsis\s+([^\n]+)/', $comment, $matches ) )
@@ -129,8 +142,64 @@ class Subcommand {
 		return $matches[1];
 	}
 
-	function invoke() {
-		return call_user_func_array( array( $this->method, 'invoke' ), func_get_args() );
+	protected function check_args( $args, $assoc_args ) {
+		$accepted_params = $this->parse_synopsis( $this->get_synopsis() );
+
+		$mandatory_positinal = wp_list_filter( $accepted_params, array(
+			'type' => 'positional',
+			'optional' => false
+		) );
+
+		if ( count( $args ) < count( $mandatory_positinal ) ) {
+			$this->show_usage();
+			exit(1);
+		}
+	}
+
+	protected function parse_synopsis( $synopsis ) {
+		$patterns = self::get_patterns();
+
+		$tokens = preg_split( '/[\s\t]+/', $synopsis );
+
+		$params = array();
+
+		foreach ( $tokens as $token ) {
+			foreach ( $patterns as $regex => $desc ) {
+				if ( preg_match( $regex, $token, $matches ) ) {
+					$params[] = array_merge( $matches, $desc );
+					break;
+				}
+			}
+		}
+
+		return $params;
+	}
+
+	private static function get_patterns() {
+		$p_name = '(?P<name>[a-z-]+)';
+		$p_value = '<(?P<value>[a-z-]+)>';
+
+		$param_types = array(
+			'positional' => $p_value,
+			'assoc' => "--$p_name=$p_value",
+			'flag' => "--$p_name"
+		);
+
+		$patterns = array();
+
+		foreach ( $param_types as $type => $pattern ) {
+			$patterns[ "/^$pattern$/" ] = array(
+				'type' => $type,
+				'optional' => false
+			);
+
+			$patterns[ "/^\[$pattern\]$/" ] = array(
+				'type' => $type,
+				'optional' => true
+			);
+		}
+
+		return $patterns;
 	}
 }
 
