8758fea612b985ead2da3ad78c88e07f5cef3f07
scribu <mail@scribu.net>
2012-10-12 01:17:17 +0300

    add @synopsis for eval and eval-file

--- src/php/wp-cli/commands/internals/eval-file.php
+++ src/php/wp-cli/commands/internals/eval-file.php
@@ -1,17 +1,22 @@
 <?php
 
-WP_CLI::add_command( 'eval-file', function( $args, $assoc_args ) {
-	if ( empty( $args ) ) {
-		WP_CLI::line( "usage: wp eval-file <path>" );
-		exit;
-	}
+WP_CLI::add_command( 'eval-file', new EvalFile_Command );
 
-	foreach ( $args as $file ) {
-		if ( !file_exists( $file ) ) {
-			WP_CLI::error( "'$file' does not exist." );
-		} else {
-			include( $file );
+class EvalFile_Command extends WP_CLI_Command {
+
+	/**
+	 * Loads and executes a PHP file after loading WordPress.
+	 *
+	 * @synopsis <path>
+	 */
+	public function __invoke( $args, $assoc_args ) {
+		foreach ( $args as $file ) {
+			if ( !file_exists( $file ) ) {
+				WP_CLI::error( "'$file' does not exist." );
+			} else {
+				include( $file );
+			}
 		}
 	}
-} );
+}
 
