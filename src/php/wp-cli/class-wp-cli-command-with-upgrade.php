d2ad080ce8c1e282c87faf8d310b4c9500dfccd4
scribu <mail@scribu.net>
2012-11-06 02:06:19 +0200

    show plugin/theme count. fixes #205

--- src/php/wp-cli/class-wp-cli-command-with-upgrade.php
+++ src/php/wp-cli/class-wp-cli-command-with-upgrade.php
@@ -39,7 +39,7 @@ abstract class WP_CLI_Command_With_Upgrade extends WP_CLI_Command {
 	private function status_all() {
 		$items = $this->get_all_items();
 
-		WP_CLI::line( "Installed {$this->item_type}s:" );
+		WP_CLI::line( count( $items ) . " installed {$this->item_type}s:" );
 
 		foreach ( $items as $file => $details ) {
 			if ( $details['update'] ) {
