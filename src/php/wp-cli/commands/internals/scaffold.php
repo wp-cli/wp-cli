f4eb255e2aab8a7eea3aaf11a804891d88d6b4e5
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2012-11-28 10:06:35 +0100

    Nothing really changed, just cleaned up the code formatting

--- src/php/wp-cli/commands/internals/scaffold.php
+++ src/php/wp-cli/commands/internals/scaffold.php
@@ -37,7 +37,6 @@ class Scaffold_Command extends WP_CLI_Command {
       $label                  = preg_replace( '/_|-/', ' ', strtolower( $post_type ) );
       $label_ucfirst          = ucfirst( $label );
       $label_plural           = $this->pluralize( $label );
-      
       $label_plural_ucfirst   = ucfirst( $label_plural );
     }
     
@@ -61,7 +60,7 @@ class Scaffold_Command extends WP_CLI_Command {
       'pages'               => 'true',
       'query_var'           => 'true',
       'can_export'          => 'true',
-      'textdomain'             => strtolower( wp_get_theme()->template ),
+      'textdomain'          => strtolower( wp_get_theme()->template ),
     );
 
     // Generate the variables from the defaults and associated arguments if they are set
@@ -137,54 +136,56 @@ class Scaffold_Command extends WP_CLI_Command {
     }
   }
 
-  private function pluralize($word) {
+  private function pluralize( $word ) {
     $plural = array(
-    '/(quiz)$/i'                => '\1zes',
-    '/^(ox)$/i'                 => '\1en',
-    '/([m|l])ouse$/i'           => '\1ice',
-    '/(matr|vert|ind)ix|ex$/i'  => '\1ices',
-    '/(x|ch|ss|sh)$/i'          => '\1es',
-    '/([^aeiouy]|qu)ies$/i'     => '\1y',
-    '/([^aeiouy]|qu)y$/i'       => '\1ies',
-    '/(hive)$/i'                => '\1s',
-    '/(?:([^f])fe|([lr])f)$/i'  => '\1\2ves',
-    '/sis$/i'                   => 'ses',
-    '/([ti])um$/i'              => '\1a',
-    '/(buffal|tomat)o$/i'       => '\1oes',
-    '/(bu)s$/i'                 => '1ses',
-    '/(alias|status)/i'         => '\1es',
-    '/(octop|vir)us$/i'         => '1i',
-    '/(ax|test)is$/i'           => '\1es',
-    '/s$/i'                     => 's',
-    '/$/'                       => 's');
+      '/(quiz)$/i'                => '\1zes',
+      '/^(ox)$/i'                 => '\1en',
+      '/([m|l])ouse$/i'           => '\1ice',
+      '/(matr|vert|ind)ix|ex$/i'  => '\1ices',
+      '/(x|ch|ss|sh)$/i'          => '\1es',
+      '/([^aeiouy]|qu)ies$/i'     => '\1y',
+      '/([^aeiouy]|qu)y$/i'       => '\1ies',
+      '/(hive)$/i'                => '\1s',
+      '/(?:([^f])fe|([lr])f)$/i'  => '\1\2ves',
+      '/sis$/i'                   => 'ses',
+      '/([ti])um$/i'              => '\1a',
+      '/(buffal|tomat)o$/i'       => '\1oes',
+      '/(bu)s$/i'                 => '1ses',
+      '/(alias|status)/i'         => '\1es',
+      '/(octop|vir)us$/i'         => '1i',
+      '/(ax|test)is$/i'           => '\1es',
+      '/s$/i'                     => 's',
+      '/$/'                       => 's'
+    );
 
-    $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
+    $uncountable = array( 'equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep' );
 
     $irregular = array(
-    'person'    => 'people',
-    'man'       => 'men',
-    'woman'     => 'women',
-    'child'     => 'children',
-    'sex'       => 'sexes',
-    'move'      => 'moves');
+      'person'    => 'people',
+      'man'       => 'men',
+      'woman'     => 'women',
+      'child'     => 'children',
+      'sex'       => 'sexes',
+      'move'      => 'moves'
+    );
 
-    $lowercased_word = strtolower($word);
+    $lowercased_word = strtolower( $word );
 
-    foreach ($uncountable as $_uncountable){
-      if(substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable){
+    foreach ( $uncountable as $_uncountable ){
+      if( substr( $lowercased_word, ( -1 * strlen( $_uncountable) ) ) == $_uncountable ){
         return $word;
       }
     }
 
-    foreach ($irregular as $_plural=> $_singular){
-      if (preg_match('/('.$_plural.')$/i', $word, $arr)) {
-        return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $word);
+    foreach ( $irregular as $_plural=> $_singular ){
+      if ( preg_match( '/('.$_plural.')$/i', $word, $arr ) ) {
+        return preg_replace( '/('.$_plural.')$/i', substr( $arr[0], 0, 1 ).substr( $_singular, 1 ), $word );
       }
     }
 
-    foreach ($plural as $rule => $replacement) {
-      if (preg_match($rule, $word)) {
-        return preg_replace($rule, $replacement, $word);
+    foreach ( $plural as $rule => $replacement ) {
+      if ( preg_match( $rule, $word ) ) {
+        return preg_replace( $rule, $replacement, $word );
       }
     }
     return false;
