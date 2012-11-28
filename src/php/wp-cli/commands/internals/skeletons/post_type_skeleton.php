0a5fa7bccf42a3022a247a5b58b8d97f96b8d0e4
jmslbam <jmslbam@gmail.com>
2012-11-28 21:35:26 +0100

    Removed unneccesary indentation

--- src/php/wp-cli/commands/internals/skeletons/post_type_skeleton.php
+++ src/php/wp-cli/commands/internals/skeletons/post_type_skeleton.php
@@ -1,66 +1,66 @@
 <?php
-  $output = "<?php
+$output = "<?php
 
-  function {$machine_name}_init() {
-    register_post_type( '{$post_type}', 
-      array(
-        'label'               => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
-        'description'         => __( '{$description}', '{$textdomain}' ),
-        'public'              => {$public},
-        'exclude_from_search' => {$exclude_from_search},
-        'show_ui'             => {$show_ui},
-        'show_in_nav_menus'   => {$show_in_nav_menus},
-        'show_in_menu'        => {$show_in_menu},
-        'show_in_admin_bar'   => {$show_in_admin_bar},
-        'menu_position'       => {$menu_position},
-        'menu_icon'           => {$menu_icon},
-        'capability_type'     => '{$capability_type}',
-        'hierarchical'        => {$hierarchical},
-        'supports'            => array( {$supports} ),
-        'has_archive'         => {$has_archive},
-        'rewrite'             => array( 'slug' => '{$slug}', 'feeds' => {$feeds}, 'pages' => {$pages} ),
-        'query_var'           => {$query_var},
-        'can_export'          => {$can_export},
-        'labels'              => array(
-          'name'                => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
-          'singular_name'       => __( '{$label_ucfirst}', '{$textdomain}' ),
-          'add_new'             => __( 'Add new {$label}', '{$textdomain}' ),
-          'all_items'           => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
-          'add_new_item'        => __( 'Add new {$label}', '{$textdomain}' ),
-          'edit_item'           => __( 'Edit {$label}', '{$textdomain}' ),
-          'new_item'            => __( 'New {$label}', '{$textdomain}' ),
-          'view_item'           => __( 'View {$label}', '{$textdomain}' ),
-          'search_items'        => __( 'Search {$label_plural}', '{$textdomain}' ),
-          'not_found'           => __( 'No {$label_plural} found', '{$textdomain}' ),
-          'not_found_in_trash'  => __( 'No {$label_plural} found in trash', '{$textdomain}' ),
-          'parent_item_colon'   => __( 'Parent {$label}', '{$textdomain}' ),
-          'menu_name'           => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
-        ),
-      ) 
-    );
-  }
-  add_action( 'init', '{$machine_name}_init' );
+function {$machine_name}_init() {
+  register_post_type( '{$post_type}', 
+    array(
+      'label'               => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
+      'description'         => __( '{$description}', '{$textdomain}' ),
+      'public'              => {$public},
+      'exclude_from_search' => {$exclude_from_search},
+      'show_ui'             => {$show_ui},
+      'show_in_nav_menus'   => {$show_in_nav_menus},
+      'show_in_menu'        => {$show_in_menu},
+      'show_in_admin_bar'   => {$show_in_admin_bar},
+      'menu_position'       => {$menu_position},
+      'menu_icon'           => {$menu_icon},
+      'capability_type'     => '{$capability_type}',
+      'hierarchical'        => {$hierarchical},
+      'supports'            => array( {$supports} ),
+      'has_archive'         => {$has_archive},
+      'rewrite'             => array( 'slug' => '{$slug}', 'feeds' => {$feeds}, 'pages' => {$pages} ),
+      'query_var'           => {$query_var},
+      'can_export'          => {$can_export},
+      'labels'              => array(
+        'name'                => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
+        'singular_name'       => __( '{$label_ucfirst}', '{$textdomain}' ),
+        'add_new'             => __( 'Add new {$label}', '{$textdomain}' ),
+        'all_items'           => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
+        'add_new_item'        => __( 'Add new {$label}', '{$textdomain}' ),
+        'edit_item'           => __( 'Edit {$label}', '{$textdomain}' ),
+        'new_item'            => __( 'New {$label}', '{$textdomain}' ),
+        'view_item'           => __( 'View {$label}', '{$textdomain}' ),
+        'search_items'        => __( 'Search {$label_plural}', '{$textdomain}' ),
+        'not_found'           => __( 'No {$label_plural} found', '{$textdomain}' ),
+        'not_found_in_trash'  => __( 'No {$label_plural} found in trash', '{$textdomain}' ),
+        'parent_item_colon'   => __( 'Parent {$label}', '{$textdomain}' ),
+        'menu_name'           => __( '{$label_plural_ucfirst}', '{$textdomain}' ),
+      ),
+    ) 
+  );
+}
+add_action( 'init', '{$machine_name}_init' );
 
-  function {$machine_name}_updated_messages( \$messages ) {
-    global \$post, \$post_ID;
+function {$machine_name}_updated_messages( \$messages ) {
+  global \$post, \$post_ID;
 
-    \$messages['{$post_type}'] = array(
-      0 => '', // Unused. Messages start at index 1.
-      1 => sprintf( __('{$label_ucfirst} updated. <a href=\"\">View {$label}</a>', '{$textdomain}'), esc_url( get_permalink(\$post_ID) ) ),
-      2 => __('Custom field updated.', '{$textdomain}'),
-      3 => __('Custom field deleted.', '{$textdomain}'),
-      4 => __('{$label_ucfirst} updated.', '{$textdomain}'),
-      /* translators: %s: date and time of the revision */
-      5 => isset(\$_GET['revision']) ? sprintf( __('{$label_ucfirst} restored to revision from %s', '{$textdomain}'), wp_post_revision_title( (int) \$_GET['revision'], false ) ) : false,
-      6 => sprintf( __('{$label_ucfirst} published. <a href=\"\">View {$label}</a>', '{$textdomain}'), esc_url( get_permalink(\$post_ID) ) ),
-      7 => __('{$label_ucfirst} saved.', '{$textdomain}'),
-      8 => sprintf( __('{$label_ucfirst} submitted. <a target=\"_blank\" href=\"\">Preview {$post_type}</a>', '{$textdomain}'), esc_url( add_query_arg( 'preview', 'true', get_permalink(\$post_ID) ) ) ),
-      9 => sprintf( __('{$label_ucfirst} scheduled for: <strong>%1\$s</strong>. <a target=\"_blank\" href=\"\">Preview {$label}</a>', '{$textdomain}'),
-        // translators: Publish box date format, see http://php.net/date
-        date_i18n( __( 'M j, Y @ G:i' ), strtotime( \$post->post_date ) ), esc_url( get_permalink( \$post_ID ) ) ),
-      10 => sprintf( __('{$label_ucfirst} draft updated. <a target=\"_blank\" href=\"\">Preview {$post_type}</a>', '{$textdomain}'), esc_url( add_query_arg( 'preview', 'true', get_permalink( \$post_ID ) ) ) ),
-    );
+  \$messages['{$post_type}'] = array(
+    0 => '', // Unused. Messages start at index 1.
+    1 => sprintf( __('{$label_ucfirst} updated. <a href=\"\">View {$label}</a>', '{$textdomain}'), esc_url( get_permalink(\$post_ID) ) ),
+    2 => __('Custom field updated.', '{$textdomain}'),
+    3 => __('Custom field deleted.', '{$textdomain}'),
+    4 => __('{$label_ucfirst} updated.', '{$textdomain}'),
+    /* translators: %s: date and time of the revision */
+    5 => isset(\$_GET['revision']) ? sprintf( __('{$label_ucfirst} restored to revision from %s', '{$textdomain}'), wp_post_revision_title( (int) \$_GET['revision'], false ) ) : false,
+    6 => sprintf( __('{$label_ucfirst} published. <a href=\"\">View {$label}</a>', '{$textdomain}'), esc_url( get_permalink(\$post_ID) ) ),
+    7 => __('{$label_ucfirst} saved.', '{$textdomain}'),
+    8 => sprintf( __('{$label_ucfirst} submitted. <a target=\"_blank\" href=\"\">Preview {$post_type}</a>', '{$textdomain}'), esc_url( add_query_arg( 'preview', 'true', get_permalink(\$post_ID) ) ) ),
+    9 => sprintf( __('{$label_ucfirst} scheduled for: <strong>%1\$s</strong>. <a target=\"_blank\" href=\"\">Preview {$label}</a>', '{$textdomain}'),
+      // translators: Publish box date format, see http://php.net/date
+      date_i18n( __( 'M j, Y @ G:i' ), strtotime( \$post->post_date ) ), esc_url( get_permalink( \$post_ID ) ) ),
+    10 => sprintf( __('{$label_ucfirst} draft updated. <a target=\"_blank\" href=\"\">Preview {$post_type}</a>', '{$textdomain}'), esc_url( add_query_arg( 'preview', 'true', get_permalink( \$post_ID ) ) ) ),
+  );
 
-    return \$messages;
-  }
-  add_filter( 'post_updated_messages', '{$machine_name}_updated_messages' );";
+  return \$messages;
+}
+add_filter( 'post_updated_messages', '{$machine_name}_updated_messages' );";
--- src/php/wp-cli/commands/internals/skeletons/taxonomy_skeleton.php
+++ src/php/wp-cli/commands/internals/skeletons/taxonomy_skeleton.php
@@ -1,47 +1,47 @@
 <?php
-  $output = "<?php
+$output = "<?php
 
-  if ( !taxonomy_exists( '{$taxonomy}' ) ) :
-    \$labels = array( 
-      'name'                        =>  __( '{$taxonomy}', '{$textdomain}' ),
-      'singular_name'               =>  __( '{$taxonomy}', '{$textdomain}' ),
-      'search_items'                =>  __( 'Search {$taxonomy}', '{$textdomain}' ),
-      'popular_items'               =>  __( 'Popular {$taxonomy}', '{$textdomain}' ),
-      'all_items'                   =>  __( 'All {$taxonomy}', '{$textdomain}' ),
-      'parent_item'                 =>  __( 'Parent {$taxonomy}', '{$textdomain}' ),
-      'parent_item_colon'           =>  __( 'Parent {$taxonomy}:', '{$textdomain}' ),
-      'edit_item'                   =>  __( 'Edit {$taxonomy}', '{$textdomain}' ),
-      'update_item'                 =>  __( 'Update {$taxonomy}', '{$textdomain}' ),
-      'add_new_item'                =>  __( 'New {$taxonomy}', '{$textdomain}' ),
-      'new_item_name'               =>  __( 'New {$taxonomy}', '{$textdomain}' ),
-      'separate_items_with_commas'  =>  __( '{$taxonomy}s seperated by comma', '{$textdomain}' ),
-      'add_or_remove_items'         =>  __( 'Add or remove {$taxonomy}s', '{$textdomain}' ),
-      'choose_from_most_used'       =>  __( 'Choose from the most used {$taxonomy}', '{$textdomain}' ),
-      'menu_name'                   =>  __( '{$taxonomy}s', '{$textdomain}' ),
-    );
-  
-    \$args = array( 
-      'labels'                  => \$labels,
-      'public'                  => {$public},
-      'show_in_nav_menus'       => {$show_in_nav_menus},
-      'show_ui'                 => {$show_ui},
-      'show_tagcloud'           => {$show_tagcloud},
-      'hierarchical'            => {$hierarchical},
-      'update_count_callback'   => '_update_post_term_count',
-      'rewrite'                 => {$rewrite},
-      'query_var'               => {$query_var},
-      'capabilities'            => array (
-        'manage_terms'  => 'edit_posts',
-        'edit_terms'    => 'edit_posts',
-        'delete_terms'  => 'edit_posts',
-        'assign_terms'  => 'edit_posts'
-      ),      
-      'rewrite' => array( 
-        'slug'          => '{$taxonomy}', 
-        'hierarchical'  => {$hierarchical} 
-      ),
-    );
-      
-    register_taxonomy( '{$taxonomy}', array( '{$post_types}' ), \$args );
+if ( !taxonomy_exists( '{$taxonomy}' ) ) :
+  \$labels = array( 
+    'name'                        =>  __( '{$taxonomy}', '{$textdomain}' ),
+    'singular_name'               =>  __( '{$taxonomy}', '{$textdomain}' ),
+    'search_items'                =>  __( 'Search {$taxonomy}', '{$textdomain}' ),
+    'popular_items'               =>  __( 'Popular {$taxonomy}', '{$textdomain}' ),
+    'all_items'                   =>  __( 'All {$taxonomy}', '{$textdomain}' ),
+    'parent_item'                 =>  __( 'Parent {$taxonomy}', '{$textdomain}' ),
+    'parent_item_colon'           =>  __( 'Parent {$taxonomy}:', '{$textdomain}' ),
+    'edit_item'                   =>  __( 'Edit {$taxonomy}', '{$textdomain}' ),
+    'update_item'                 =>  __( 'Update {$taxonomy}', '{$textdomain}' ),
+    'add_new_item'                =>  __( 'New {$taxonomy}', '{$textdomain}' ),
+    'new_item_name'               =>  __( 'New {$taxonomy}', '{$textdomain}' ),
+    'separate_items_with_commas'  =>  __( '{$taxonomy}s seperated by comma', '{$textdomain}' ),
+    'add_or_remove_items'         =>  __( 'Add or remove {$taxonomy}s', '{$textdomain}' ),
+    'choose_from_most_used'       =>  __( 'Choose from the most used {$taxonomy}', '{$textdomain}' ),
+    'menu_name'                   =>  __( '{$taxonomy}s', '{$textdomain}' ),
+  );
 
-  endif;";
+  \$args = array( 
+    'labels'                  => \$labels,
+    'public'                  => {$public},
+    'show_in_nav_menus'       => {$show_in_nav_menus},
+    'show_ui'                 => {$show_ui},
+    'show_tagcloud'           => {$show_tagcloud},
+    'hierarchical'            => {$hierarchical},
+    'update_count_callback'   => '_update_post_term_count',
+    'rewrite'                 => {$rewrite},
+    'query_var'               => {$query_var},
+    'capabilities'            => array (
+      'manage_terms'  => 'edit_posts',
+      'edit_terms'    => 'edit_posts',
+      'delete_terms'  => 'edit_posts',
+      'assign_terms'  => 'edit_posts'
+    ),      
+    'rewrite' => array( 
+      'slug'          => '{$taxonomy}', 
+      'hierarchical'  => {$hierarchical} 
+    ),
+  );
+    
+  register_taxonomy( '{$taxonomy}', array( '{$post_types}' ), \$args );
+
+endif;";
