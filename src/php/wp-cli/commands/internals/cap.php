e31cb1941eaf9eb33b5cf2d1024755ec73a0b5db
scribu <mail@scribu.net>
2012-12-16 06:14:37 +0200

    implement `wp cap add` and `wp cap remove`

--- src/php/wp-cli/commands/internals/cap.php
+++ src/php/wp-cli/commands/internals/cap.php
@@ -17,17 +17,80 @@ class Capabilities_Command extends WP_CLI_Command {
 	 * @synopsis <role>
 	 */
 	public function _list( $args ) {
-		global $wp_roles;
+		$role_obj = self::get_role( $args[0] );
 
-		list( $role ) = $args;
+		foreach ( array_keys( $role_obj->capabilities ) as $cap )
+			WP_CLI::line( $cap );
+	}
+
+	/**
+	 * Add capabilities to a given role.
+	 *
+	 * @synopsis <role> <cap>...
+	 */
+	public function add( $args ) {
+		self::persistence_check();
+
+		$role = array_shift( $args );
+
+		$role_obj = self::get_role( $role );
+
+		$count = 0;
+
+		foreach ( $args as $cap ) {
+			if ( $role_obj->has_cap( $cap ) )
+				continue;
+
+			$role_obj->add_cap( $cap );
+
+			$count++;
+		}
+
+		WP_CLI::success( sprintf( "Added %d capabilities to '%s' role." , $count, $role ) );
+	}
+
+	/**
+	 * Add capabilities to a given role.
+	 *
+	 * @synopsis <role> <cap>...
+	 */
+	public function remove( $args ) {
+		self::persistence_check();
+
+		$role = array_shift( $args );
+
+		$role_obj = self::get_role( $role );
+
+		$count = 0;
+
+		foreach ( $args as $cap ) {
+			if ( !$role_obj->has_cap( $cap ) )
+				continue;
+
+			$role_obj->remove_cap( $cap );
+
+			$count++;
+		}
+
+		WP_CLI::success( sprintf( "Removed %d capabilities from '%s' role." , $count, $role ) );
+	}
+
+	private static function get_role( $role ) {
+		global $wp_roles;
 
 		$role_obj = $wp_roles->get_role( $role );
 
 		if ( !$role_obj )
-			WP_CLI::error( "$role does not exist" );
+			WP_CLI::error( "'$role' role not found." );
 
-		foreach ( array_keys( $role_obj->capabilities ) as $cap )
-			WP_CLI::line( $cap );
+		return $role_obj;
+	}
+
+	private static function persistence_check() {
+		global $wp_roles;
+
+		if ( !$wp_roles->use_db )
+			WP_CLI::error( "Role definitions are not persistent." );
 	}
 }
 
