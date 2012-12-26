812d9b26d42e9de9c4a207fa4cf627b74c107d42
scribu <mail@scribu.net>
2012-12-26 11:48:00 +0200

    autoload query iterator classes

---
+++ php/wp-cli/WP_CLI/TableIterator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace WP_CLI;
+
+/**
+ * @source https://gist.github.com/4060005
+ */
+class TableIterator extends QueryIterator {
+
+	/**
+	 * Creates an iterator over a database table
+	 *
+	 * <code>
+	 * foreach( new TableIterator( array( 'table' => $wpdb->posts, 'fields' => array( 'ID', 'post_content' ) ) ) as $post ) {
+	 *     count_words_for( $post->ID, $post->post_content );
+	 * }
+	 * </code>
+	 *
+	 * <code>
+	 * foreach( new TableIterator( array( 'table' => $wpdb->posts, 'where' => 'ID = 8 OR post_status = "publish"' ) ) as $post ) {
+	 *     …
+	 * }
+	 * </code>
+	 *
+	 * <code>
+	 * foreach( new PostIterator( array( 'table' => $wpdb->posts, 'where' => array( 'post_status' => 'publish', 'post_date_gmt BETWEEN x AND y' ) ) ) as $post ) {
+	 *     …
+	 * }
+	 * </code>
+	 *
+	 *
+	 * @param array $args Supported arguments:
+	 *		table – the name of the database table
+	 *		fields – an array of columns to get from the posst table, * is a valid value and the default
+	 *		where – conditions for filtering rows. Supports two formats:
+	 *			= string – this will be the where clause
+	 *			= array – each element is treated as a condition if it's positional, or as column => value if
+	 *				it's a key/value pair. In the latter case the value is automatically quoted and escaped
+	 */
+	function __construct( $args = array() ) {
+		global $wpdb;
+
+		$defaults = array(
+			'fields' => array( '*' ),
+			'where' => array(),
+			'table' => null,
+		);
+		$table = $args['table'];
+		$args = array_merge( $defaults, $args );
+
+		$fields = self::build_fields( $args['fields'] );
+		$conditions = self::build_where_conditions( $args['where'] );
+		$where_sql = $conditions? " WHERE $conditions" : '';
+		$query = "SELECT $fields FROM $table $where_sql";
+		$parent_args = compact( 'query' );
+		if ( isset( $args['limit'] ) ) {
+			$parent_args['limit'] = $args['limit'];
+		}
+		parent::__construct( $parent_args );
+	}
+
+	static function build_fields( $fields ) {
+		return implode( ', ', $fields );
+	}
+
+	static function build_where_conditions( $where ) {
+		global $wpdb;
+		if ( is_array( $where ) ) {
+			$conditions = array();
+			foreach( $where as $key => $value ) {
+				if ( is_numeric( $key ) )
+					$conditions[] = $value;
+				else
+					$conditions[] = $key . ' = "' . $wpdb->escape( $value ) .'"';
+			}
+			$where = implode( ' AND ', $conditions );
+		}
+		return $where;
+	}
+}
+
