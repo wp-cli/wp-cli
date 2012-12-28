43f6097120bf27b5b1e749f3a7db115e49eb4260
scribu <mail@scribu.net>
2012-12-28 13:46:06 +0200

    separate wp-dependent utilities from independent ones

--- php/utils.php
+++ php/utils.php
@@ -1,5 +1,7 @@
 <?php
 
+// Utilities that do NOT depend on WordPress code.
+
 namespace WP_CLI\Utils;
 
 function bootstrap() {
@@ -173,44 +175,6 @@ function load_wp_config() {
 		require locate_wp_config();
 	else
 		\WP_CLI::error( 'No wp-config.php file.' );
-}
-
-
-
-// ---- AFTER WORDPRESS IS LOADED ---- //
-
-
-
-// Handle --user parameter
-function set_user( $assoc_args ) {
-	if ( !isset( $assoc_args['user'] ) )
-		return;
-
-	$user = $assoc_args['user'];
-
-	if ( is_numeric( $user ) ) {
-		$user_id = (int) $user;
-	} else {
-		$user_id = (int) username_exists( $user );
-	}
-
-	if ( !$user_id || !wp_set_current_user( $user_id ) ) {
-		\WP_CLI::error( sprintf( 'Could not get a user_id for this user: %s', var_export( $user, true ) ) );
-	}
-}
-
-function set_wp_query() {
-	if ( isset( $GLOBALS['wp_query'] ) && isset( $GLOBALS['wp'] ) ) {
-		$GLOBALS['wp']->parse_request();
-		$GLOBALS['wp_query']->query($GLOBALS['wp']->query_vars);
-	}
-}
-
-function get_upgrader( $class ) {
-	if ( !class_exists( '\WP_Upgrader' ) )
-		require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-
-	return new $class( new \WP_CLI\UpgraderSkin );
 }
 
 function parse_csv( $filepath, $has_headers = true ) {
