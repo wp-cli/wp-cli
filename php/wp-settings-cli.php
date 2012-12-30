03aac43d0792d4f19ef060726a7158f9def01443
scribu <mail@scribu.net>
2012-12-30 12:20:46 +0200

    change header for wp-settings-cli.php

--- php/wp-settings-cli.php
+++ php/wp-settings-cli.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * Used to set up and fix common variables and include
- * the WordPress procedural and class library.
- *
- * Allows for some configuration in wp-config.php (see default-constants.php)
- *
- * @internal This file must be parsable by PHP4.
- *
- * @package WordPress
+ * A modified version of wp-settings.php, tailored for CLI use.
  */
 
 /**
@@ -23,7 +16,7 @@ require( ABSPATH . WPINC . '/default-constants.php' );
 require( ABSPATH . WPINC . '/version.php' );
 
 // Set initial default constants including WP_MEMORY_LIMIT, WP_MAX_MEMORY_LIMIT, WP_DEBUG, WP_CONTENT_DIR and WP_CACHE.
-wp_initial_constants( );
+wp_initial_constants();
 
 // Check for the required PHP version and for the MySQL extension or a database drop-in.
 wp_check_php_mysql_versions();
