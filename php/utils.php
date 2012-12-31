ea80ac02b1268b9962804ce6fa94a83a8fc9c57e
scribu <mail@scribu.net>
2012-12-31 15:10:01 +0200

    fix punctuation for deprecation messages

--- php/utils.php
+++ php/utils.php
@@ -148,14 +148,14 @@ function set_url( $assoc_args ) {
 	if ( isset( $assoc_args['url'] ) ) {
 		$url = $assoc_args['url'];
 	} elseif ( isset( $assoc_args['blog'] ) ) {
-		\WP_CLI::warning( 'The --blog parameter is deprecated. use --url instead' );
+		\WP_CLI::warning( 'The --blog parameter is deprecated. Use --url instead.' );
 
 		$url = $assoc_args['blog'];
 		if ( true === $url ) {
 			\WP_CLI::line( 'usage: wp --blog=example.com' );
 		}
 	} elseif ( is_readable( WP_ROOT . 'wp-cli-blog' ) ) {
-		\WP_CLI::warning( 'The wp-cli-blog file is deprecated. use wp-cli.yml instead' );
+		\WP_CLI::warning( 'The wp-cli-blog file is deprecated. Use wp-cli.yml instead.' );
 
 		$url = trim( file_get_contents( WP_ROOT . 'wp-cli-blog' ) );
 	} elseif ( $wp_config_path = locate_wp_config() ) {
