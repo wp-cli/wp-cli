535d85d4e6386d58f44453e6d17ebef0b5995260
scribu <mail@scribu.net>
2013-02-11 21:41:05 +0200

    check that other parameter types are 0

--- tests/test-synopsis.php
+++ tests/test-synopsis.php
@@ -7,31 +7,39 @@ class SynopsisParserTest extends PHPUnit_Framework_TestCase {
 	function testPositional() {
 		$r = SynopsisParser::parse( '<foo> [<bar>]' );
 
-		$this->assertEquals( 2, count( $r['positional'] ) );
+		$this->assertFoundParameters( 2, 'positional', $r );
 		$this->assertFalse( $r['positional'][0]['optional'] );
 		$this->assertTrue( $r['positional'][1]['optional'] );
 	}
 
 	function testFlag() {
 		$r = SynopsisParser::parse( '--foo' );
-		$this->assertEquals( 0, count( $r['flag'] ) ); // flags can't be mandatory
+		$this->assertFoundParameters( 0, 'flag', $r ); // flags can't be mandatory
 
 		$r = SynopsisParser::parse( '[--foo]' );
-		$this->assertEquals( 1, count( $r['flag'] ) );
+		$this->assertFoundParameters( 1, 'flag', $r );
 	}
 
 	function testGeneric() {
 		$r = SynopsisParser::parse( '--<field>=<value>' );
 
-		$this->assertEquals( 1, count( $r['generic'] ) );
+		$this->assertFoundParameters( 1, 'generic', $r );
 	}
 
 	function testAssoc() {
 		$r = SynopsisParser::parse( '--foo=<value> [--bar=<value>]' );
 
-		$this->assertEquals( 2, count( $r['assoc'] ) );
+		$this->assertFoundParameters( 2, 'assoc', $r );
 		$this->assertFalse( $r['assoc'][0]['optional'] );
 		$this->assertTrue( $r['assoc'][1]['optional'] );
+	}
+
+	protected function assertFoundParameters( $count, $type, $r ) {
+		foreach ( $r as $key => $params ) {
+			$expected = ( $key == $type ) ? $count : 0;
+
+			$this->assertEquals( $expected, count( $params ) );
+		}
 	}
 }
 
