70de7f4b857a4ee17422dc9e016a8ebb0c8fe035
Lee Willis <leewillis77@gmail.com>
2013-03-09 20:25:58 +0000

    Remove spaces before function calls

--- php/commands/roles.php
+++ php/commands/roles.php
@@ -16,13 +16,13 @@ class Roles_Command extends WP_CLI_Command {
 	public function _list( $args ) {
 		global $wp_roles;
 
-		if ( isset ( $args[0] ) )
+		if ( isset( $args[0] ) )
 			$target_role = $args[0];
 		else
 			$target_role = '';
 
 		foreach ( $wp_roles->roles as $key => $role ) {
-			if ( empty ( $target_role ) || $key == $target_role )
+			if ( empty( $target_role ) || $key == $target_role )
 				WP_CLI::line( $role['name'] . " ($key)");
 		}
 	}
@@ -39,10 +39,10 @@ class Roles_Command extends WP_CLI_Command {
 		$role_key = array_shift( $args );
 		$role_name = array_shift( $args );
 
-		if ( empty ( $role_key ) || empty ( $role_name ) )
+		if ( empty( $role_key ) || empty( $role_name ) )
 			WP_CLI::error( "Can't create role, insufficient information provided.");
 
-		if ( ! add_role ( $role_key, $role_name ) )
+		if ( ! add_role( $role_key, $role_name ) )
 			WP_CLI::error( "Role couldn't be created." );
 		else
 			WP_CLI::success( sprintf( "Role with key %s created.", $role_key ) );
@@ -63,14 +63,14 @@ class Roles_Command extends WP_CLI_Command {
 
 		$role_key = array_shift( $args );
 
-		if ( empty ( $role_key ) || ! isset ( $wp_roles->roles[$role_key] ) )
+		if ( empty( $role_key ) || ! isset( $wp_roles->roles[$role_key] ) )
 			WP_CLI::error( "Role key not provided, or is invalid." );
 
-		remove_role ( $role_key );
+		remove_role( $role_key );
 
 		// Note: remove_role() doesn't indicate success or otherwise, so we have to
 		// check ourselves
-		if ( ! isset ( $wp_roles->roles[$role_key] ) )
+		if ( ! isset( $wp_roles->roles[$role_key] ) )
 			WP_CLI::success( sprintf( "Role with key %s deleted.", $role_key ) );
 		else
 			WP_CLI::error( sprintf( "Role with key %s could not be deleted.", $role_key ) );
