4aa9a22c181805fda1a20475c6cc6d3a4d2999db
scribu <mail@scribu.net>
2013-03-11 23:59:03 +0200

    add show_help_early()

--- php/commands/help.php
+++ php/commands/help.php
@@ -10,44 +10,15 @@ class Help_Command extends WP_CLI_Command {
 	 * @synopsis [<command>]
 	 */
 	function __invoke( $args ) {
-		self::maybe_load_man_page( $args );
+		\WP_CLI\Man\maybe_show_manpage( $args );
 
-		$arg_copy = $args;
-
-		$command = \WP_CLI::$root;
-
-		while ( !empty( $args ) && $command && $command instanceof CommandContainer ) {
-			$command = $command->find_subcommand( $args );
-		}
+		$command = WP_CLI\Utils\find_subcommand( $args );
 
 		if ( !$command ) {
-			\WP_CLI::error( sprintf( "'%s' is not a registered wp command.", $arg_copy[0] ) );
+			\WP_CLI::error( sprintf( "'%s' is not a registered wp command.", $args[0] ) );
 		}
 
 		$command->show_usage();
-	}
-
-	private static function maybe_load_man_page( $args ) {
-		$man_file = \WP_CLI\Man\get_file_name( $args );
-
-		foreach ( \WP_CLI::get_man_dirs() as $dest_dir => $_ ) {
-			$man_path = $dest_dir . $man_file;
-
-			if ( is_readable( $man_path ) ) {
-				self::show_manpage( $man_path );
-			}
-		}
-	}
-
-	private static function show_manpage( $path ) {
-		// man can't read phar://, so need to copy to a temporary file
-		$tmp_path = tempnam( sys_get_temp_dir(), 'wp-cli-man-' );
-
-		copy( $path, $tmp_path );
-
-		WP_CLI::launch( "man $tmp_path" );
-
-		unlink( $tmp_path );
 	}
 }
 
