6324b4219e1d29f24f8ae2d44aeb0db8b15f2f75
scribu <mail@scribu.net>
2013-03-11 23:59:03 +0200

    add show_help_early()

--- php/utils.php
+++ php/utils.php
@@ -316,3 +316,14 @@ function launch_editor_for_input( $input, $title = 'WP-CLI' ) {
 
 	return $output;
 }
+
+function find_subcommand( $args ) {
+		$command = \WP_CLI::$root;
+
+		while ( !empty( $args ) && $command && $command instanceof Dispatcher\CommandContainer ) {
+			$command = $command->find_subcommand( $args );
+		}
+
+		return $command;
+}
+
