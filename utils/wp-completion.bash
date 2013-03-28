51be561678556db85d2afbcdeff4eaa945a5188d
goldenapples <goldenapplesdesign@gmail.com>
2013-03-28 10:19:28 -0700

    Remove filename completion for wp media import
    Filtering bash completions breaks bash's default filename globbing,
    which is a more useful feature here.

--- utils/wp-completion.bash
+++ utils/wp-completion.bash
@@ -1,7 +1,7 @@
 # bash completion for the wp command
 
 _wp() {
-	local cur prev opts file_ops
+	local cur prev opts
 
 	cur=${COMP_WORDS[COMP_CWORD]}
 	prev=${COMP_WORDS[COMP_CWORD-1]}
@@ -12,10 +12,7 @@ _wp() {
 		opts=$(wp --completions | grep ^$prev | cut -d ' ' -f 2- | tr '\n' ' ')
 	fi
 
-	# An array of prev keywords that should get file completion
-	declare -A file_ops=([create]=1 [import]=1)
-
-	if [[ ${file_ops[$prev]} ]]; then
+	if [[ 'create' = $prev ]]; then
 		COMPREPLY=( $(compgen -f "$cur") )
 	else
 		COMPREPLY=( $(compgen -W "$opts" -- $cur) )
