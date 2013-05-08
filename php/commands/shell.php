883411c7d1d84ec8392aded2ae345ab080aa7d0f
scribu <mail@scribu.net>
2013-05-08 15:06:46 +0300

    add --basic flag to `wp shell` command

--- php/commands/shell.php
+++ php/commands/shell.php
@@ -4,14 +4,20 @@ class Shell_Command extends \WP_CLI_Command {
 
 	/**
 	 * Interactive PHP console.
+	 *
+	 * @synopsis [--basic]
 	 */
-	public function __invoke() {
+	public function __invoke( $_, $assoc_args ) {
 		\WP_CLI::line( 'Type "exit" to close session.' );
 
 		$implementations = array(
 			'\\Boris\\Boris',
 			'\\WP_CLI\\REPL',
 		);
+
+		if ( isset( $assoc_args['basic'] ) ) {
+			unset( $implementations[0] );
+		}
 
 		foreach ( $implementations as $class ) {
 			if ( class_exists( $class ) ) {
