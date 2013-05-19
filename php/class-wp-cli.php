c5e818ff8b3c98f008035232d04a404bbf5f17f0
scribu <mail@scribu.net>
2013-05-19 19:52:23 +0300

    save parent class in Subcommand
    ReflectionMethod only stores the name of the original class, which might
    be abstract.

--- php/class-wp-cli.php
+++ php/class-wp-cli.php
@@ -40,7 +40,7 @@ class WP_CLI {
 		$reflection = new \ReflectionClass( $class );
 
 		if ( $reflection->hasMethod( '__invoke' ) ) {
-			$command = new Dispatcher\Subcommand( self::$root,
+			$command = new Dispatcher\Subcommand( self::$root, $reflection->name,
 				$reflection->getMethod( '__invoke' ), $name );
 		} else {
 			$command = self::create_composite_command( $name, $reflection );
@@ -58,7 +58,7 @@ class WP_CLI {
 			if ( !self::_is_good_method( $method ) )
 				continue;
 
-			$subcommand = new Dispatcher\Subcommand( $container, $method );
+			$subcommand = new Dispatcher\Subcommand( $container, $reflection->name, $method );
 
 			$subcommand_name = $subcommand->get_name();
 			$full_name = self::get_full_name( $subcommand );
