dcbc218373e059286d87c0ba25533a088bb76238
scribu <mail@scribu.net>
2013-05-20 15:20:07 +0300

    convert `wp --man` to `wp help --gen`

--- php/commands/help.php
+++ php/commands/help.php
@@ -1,15 +1,20 @@
 <?php
 
-use \WP_CLI\Dispatcher\CommandContainer;
-
 class Help_Command extends WP_CLI_Command {
 
 	/**
 	 * Get help on a certain topic.
 	 *
-	 * @synopsis [<command>]
+	 * @synopsis [<command>] [--gen]
 	 */
-	function __invoke( $args ) {
+	function __invoke( $args, $assoc_args ) {
+		if ( isset( $assoc_args['gen'] ) )
+			$this->generate( $args );
+		else
+			$this->show( $args );
+	}
+
+	private function show( $args ) {
 		if ( \WP_CLI\Man\maybe_show_manpage( $args ) ) {
 			exit;
 		}
@@ -25,6 +30,27 @@ class Help_Command extends WP_CLI_Command {
 		if ( function_exists( 'add_filter' ) ) {
 			\WP_CLI::error( sprintf( "'%s' is not a registered wp command.", $args[0] ) );
 		}
+	}
+
+	private function generate( $args ) {
+		if ( '' === exec( 'which ronn' ) ) {
+			WP_CLI::error( '`ronn` executable not found.' );
+		}
+
+		$arg_copy = $args;
+
+		$command = WP_CLI\Utils\find_subcommand( $args );
+
+		if ( !$command ) {
+			WP_CLI::error( sprintf( "'%s' command not found.",
+				implode( ' ', $arg_copy ) ) );
+		}
+
+		foreach ( WP_CLI::get_man_dirs() as $dest_dir => $src_dir ) {
+			WP_CLI\Man\generate( $src_dir, $dest_dir, $command );
+		}
+
+		exit;
 	}
 }
 
