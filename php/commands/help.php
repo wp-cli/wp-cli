770f53c3a38723ffa3f16f1e1f165b5b4ff65d03
scribu <mail@scribu.net>
2013-05-20 17:59:38 +0300

    introduce man.mustache

--- php/commands/help.php
+++ php/commands/help.php
@@ -1,5 +1,6 @@
 <?php
 
+use \WP_CLI\Utils;
 use \WP_CLI\Dispatcher;
 
 class Help_Command extends WP_CLI_Command {
@@ -121,51 +122,33 @@ class Help_Command extends WP_CLI_Command {
 	private static function add_initial_markdown( $fd, $command ) {
 		$path = Dispatcher\get_path( $command );
 
-		$shortdesc = $command->get_shortdesc();
+		$binding = array(
+			'name_m' => implode( '-', $path ),
+			'shortdesc' => $command->get_shortdesc(),
+		);
+
 		$synopsis = $command->get_full_synopsis();
 
 		$synopsis = str_replace( '_', '\_', $synopsis );
 		$synopsis = str_replace( array( '<', '>' ), '_', $synopsis );
 
-		$name_m = implode( '-', $path );
-		$name_s = implode( ' ', $path );
+		$binding['synopsis'] = $synopsis;
 
-		if ( !$shortdesc ) {
+		if ( !$binding['shortdesc'] ) {
+			$name_s = implode( ' ', $path );
 			\WP_CLI::warning( "No shortdesc for $name_s" );
 		}
 
-		fwrite( $fd, <<<DOC
-$name_m(1) -- $shortdesc
-====
-
-## SYNOPSIS
-
-$synopsis
-
-DOC
-		);
-
 		if ( $command instanceof Dispatcher\CommandContainer ) {
-
-			fwrite( $fd, <<<DOC
-## SUBCOMMANDS
-
-DOC
-			);
-
 			foreach ( $command->get_subcommands() as $subcommand ) {
-				$name = $subcommand->get_name();
-				$desc = $subcommand->get_shortdesc();
-
-				fwrite( $fd, <<<DOC
-* `$name`:
-
-	$desc
-
-DOC
+				$binding['has-subcommands']['subcommands'][] = array(
+					'name' => $subcommand->get_name(),
+					'desc' => $subcommand->get_shortdesc(),
 				);
 			}
 		}
+
+		fwrite( $fd, Utils\mustache_render( 'man.mustache', $binding ) );
 	}
 
 	private static function call_ronn( $markdown, $dest ) {
