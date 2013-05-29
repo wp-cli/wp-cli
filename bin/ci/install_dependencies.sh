f121dfac28e9268f55a03f911e8c2f366441ec46
scribu <mail@scribu.net>
2013-05-29 20:00:24 +0300

    travis: move init script to separate file

---
+++ bin/ci/install_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# called by Travis CI
+
+set -ex
+
+# install dependencies
+composer install --dev --no-interaction --prefer-source
+composer require d11wtq/boris=dev-master --no-interaction --prefer-source
+gem install ronn
+
+# set up WP install
+./bin/wp core download --version=$WP_VERSION --path=/tmp/wp-cli-test-core-download-cache/
+
+# set up database
+mysql -e 'CREATE DATABASE wp_cli_test;' -uroot
+mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
