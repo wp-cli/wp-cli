ce7f0e41b59c650da85a46ffc66547c203710e8e
Tomasz Ratajczak <tomasz.ratajczak@espeo.pl>
2013-06-10 09:53:30 +0200

    enable site-meta only on MULTISITE installs

--- php/commands/site-meta.php
+++ php/commands/site-meta.php
@@ -9,5 +9,6 @@ class Site_Meta_Command extends \WP_CLI\CommandWithMeta {
 	protected $meta_type = 'site';
 }
 
-WP_CLI::add_command( 'site-meta', 'Site_Meta_Command' );
+if (defined('MULTISITE'))
+    WP_CLI::add_command( 'site-meta', 'Site_Meta_Command' );
 
