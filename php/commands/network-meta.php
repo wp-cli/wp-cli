b672bc767c8439790910957a67bfdb95f6921437
scribu <mail@scribu.net>
2013-06-15 13:02:50 +0300

    rename `wp site-meta` to `wp network-meta`

--- php/commands/network-meta.php
+++ php/commands/network-meta.php
@@ -5,13 +5,13 @@
  *
  * @package wp-cli
  */
-class Site_Meta_Command extends \WP_CLI\CommandWithMeta {
+class Network_Meta_Command extends \WP_CLI\CommandWithMeta {
 	protected $meta_type = 'site';
 }
 
-WP_CLI::add_command( 'site-meta', 'Site_Meta_Command' );
+WP_CLI::add_command( 'network-meta', 'Network_Meta_Command' );
 
-WP_CLI::add_hook( 'before_invoke:site-meta', function () {
+WP_CLI::add_hook( 'before_invoke:network-meta', function () {
 	if ( !is_multisite() ) {
 		WP_CLI::error( 'This is not a multisite install.' );
 	}
