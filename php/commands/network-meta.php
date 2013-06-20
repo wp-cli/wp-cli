6f3b77c7938044fc491d318608e17ad431fc3056
scribu <mail@scribu.net>
2013-06-20 04:27:56 +0300

    add optional 'before_invoke' parameter to WP_CLI::add_command()
    this avoids repeating the command name and makes it clearer when the
    callback is fired

--- php/commands/network-meta.php
+++ php/commands/network-meta.php
@@ -9,11 +9,11 @@ class Network_Meta_Command extends \WP_CLI\CommandWithMeta {
 	protected $meta_type = 'site';
 }
 
-WP_CLI::add_command( 'network-meta', 'Network_Meta_Command' );
-
-WP_CLI::add_action( 'before_invoke:network-meta', function () {
-	if ( !is_multisite() ) {
-		WP_CLI::error( 'This is not a multisite install.' );
+WP_CLI::add_command( 'network-meta', 'Network_Meta_Command', array(
+	'before_invoke' => function () {
+		if ( !is_multisite() ) {
+			WP_CLI::error( 'This is not a multisite install.' );
+		}
 	}
-} );
+) );
 
