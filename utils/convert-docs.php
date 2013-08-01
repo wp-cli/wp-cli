3f5d5c2f07d05ec608533ba463d5773587b7c254
scribu <mail@scribu.net>
2013-08-01 16:46:56 +0300

    introduce utils/convert-docs.php script

---
+++ utils/convert-docs.php
@@ -0,0 +1,26 @@
+<?php
+
+# usage: php utils/convert-docs.php man-src/*.txt
+
+function convert_file( $path ) {
+	$out = file_get_contents( $path );
+
+	// options to definition lists
+	$out = preg_replace_callback( '/\n\* (.+?):?\n\n\t/', function( $matches ) {
+		$arg = str_replace( '`', '', $matches[1] );
+
+		return "\n$arg\n: ";
+	}, $out );
+
+	// convert tabs to spaces
+	$out = preg_replace( '/^\t/m', "  ", $out );
+
+	// prepend docblock notation
+	$out = preg_replace( '/^(.*)/m', "\t* \\1", $out );
+
+	file_put_contents( $path, $out );
+}
+
+foreach ( array_slice( $argv, 1 ) as $arg ) {
+	convert_file( $arg );
+}
