c676c4d7d0b591a2b2e594d7523472e335328350
scribu <mail@scribu.net>
2013-08-14 17:40:20 +0300

    Don't pretend we're in wp-admin, but continue to load all admin APIs.
    The WP_CLI context is not like the WP_ADMIN context. It's a different
    animal.
    
    See #674 for more details.
    
    Fixes #477, #519, #574, #579, #582, #669

--- php/wp-cli.php
+++ php/wp-cli.php
@@ -19,20 +19,14 @@ WP_CLI::get_runner()->before_wp_load();
 // Load wp-config.php code, in the global scope
 eval( WP_CLI::get_runner()->get_wp_config_code() );
 
-// Simulate a /wp-admin/ page load
-$_SERVER['PHP_SELF'] = '/wp-admin/index.php';
-define( 'WP_ADMIN', true );
-define( 'WP_NETWORK_ADMIN', false );
-define( 'WP_USER_ADMIN', false );
-
 // Load Core, mu-plugins, plugins, themes etc.
 require WP_CLI_ROOT . '/php/wp-settings-cli.php';
 
 // Fix memory limit. See http://core.trac.wordpress.org/ticket/14889
 @ini_set( 'memory_limit', -1 );
 
+// Load all the admin APIs, for convenience
 require ABSPATH . 'wp-admin/includes/admin.php';
-do_action( 'admin_init' );
 
 WP_CLI::get_runner()->after_wp_load();
 
