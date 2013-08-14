0b828831e6c9219963a66f2b3ff1e6fcd0e85f9a
scribu <mail@scribu.net>
2013-08-15 00:07:02 +0300

    move WP_CLI_BIN_DIR env variable to .travis.yml

--- bin/ci/build.sh
+++ bin/ci/build.sh
@@ -6,11 +6,10 @@ set -ex
 phpunit
 
 # Create the Phar file
-WP_CLI_BIN_DIR=/tmp/wp-cli-phar
 mkdir -p $WP_CLI_BIN_DIR
 php -dphar.readonly=0 utils/make-phar.php wp-cli.phar --quiet
 mv wp-cli.phar $WP_CLI_BIN_DIR/wp
 chmod +x $WP_CLI_BIN_DIR/wp
 
 # Run the functional tests
-WP_CLI_BIN_DIR=$WP_CLI_BIN_DIR php behat.phar --format progress
+php behat.phar --format progress
