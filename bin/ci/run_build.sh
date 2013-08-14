b46b4ea747cc687b747b9d8550fa8eb5a570589a
scribu <mail@scribu.net>
2013-08-14 23:20:29 +0300

    run the functional tests against the Phar file

--- bin/ci/run_build.sh
+++ bin/ci/run_build.sh
@@ -4,4 +4,13 @@ set -ex
 
 phpunit
 
-php behat.phar --format progress
+# Run the functional tests against the Phar file
+
+WP_CLI_BIN_DIR=/tmp/wp-cli-phar
+
+mkdir -p $WP_CLI_BIN_DIR
+php -dphar.readonly=0 utils/make-phar.php wp-cli.phar --quiet
+mv wp-cli.phar $WP_CLI_BIN_DIR/wp
+chmod +x $WP_CLI_BIN_DIR/wp
+
+WP_CLI_BIN_DIR=$WP_CLI_BIN_DIR php behat.phar --format progress
