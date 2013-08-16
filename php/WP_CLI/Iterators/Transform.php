262245904b21fcec6962eb2728ca520a7011e146
scribu <mail@scribu.net>
2013-08-16 03:03:55 +0300

    introduce iterator_map() and avoid converting iterator to array in `wp site list`

---
+++ php/WP_CLI/Iterators/Transform.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WP_CLI\Iterators;
+
+/**
+ * Aplies one or more callbacks to an item before returning it.
+ */
+class Transform extends \IteratorIterator {
+
+	private $transformers = array();
+
+	public function add_transform( $fn ) {
+		$this->transformers[] = $fn;
+	}
+
+	public function current() {
+		$value = parent::current();
+
+		foreach ( $this->transformers as $fn ) {
+			$value = $fn( $value );
+		}
+
+		return $value;
+	}
+}
+
