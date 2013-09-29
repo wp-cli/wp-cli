75699b2e88fdfcc6a1f733b273600c4e81dcefc0
Daniel Bachhuber <d@danielbachhuber.com>
2013-09-29 08:30:05 -0700

    Update `\WP_CLI\Utils::format_items()` to be smart enough about formatting a single item

--- php/utils.php
+++ php/utils.php
@@ -292,9 +292,14 @@ function format_items( $format, $items, $fields ) {
 			break;
 
 		case 'json':
-			$out = array();
-			foreach ( $items as $item ) {
-				$out[] = pick_fields( $item, $fields );
+
+			if ( is_array( $out ) ) {
+				$out = array();
+				foreach ( $items as $item ) {
+					$out[] = pick_fields( $item, $fields );
+				}
+			} else {
+				$out = pick_fields( $items, $fields );
 			}
 
 			echo json_encode( $out );
