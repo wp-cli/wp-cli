wp-cli/wp-cli

commit f6285d3b674e01fff81e37a2978a1436e1f94e69

pass subcommand implementations as callbacks
This allows child classes to implement only some of the subcommands.

php/commands/post.php
@@ -71,11 +71,9 @@ class Post_Command extends \WP_CLI\CommandWithDBObject {
 				$assoc_args['post_content'] = $input;
 		}
 
-		parent::create( $args, $assoc_args );
-	}
-
-	protected function _create( $params ) {
-		return wp_insert_post( $params, true );
+		parent::_create( $args, $assoc_args, function ( $params ) {
+			return wp_insert_post( $params, true );
+		} );
 	}
 
 	/**
@@ -94,11 +92,9 @@ class Post_Command extends \WP_CLI\CommandWithDBObject {
 	 *     wp post update 123 --post_name=something --post_status=draft
 	 */
 	public function update( $args, $assoc_args ) {
-		parent::update( $args, $assoc_args );
-	}
-
-	protected function _update( $params ) {
-		return wp_update_post( $params, true );
+		parent::_update( $args, $assoc_args, function ( $params ) {
+			return wp_update_post( $params, true );
+		} );
 	}
 
 	/**
@@ -216,19 +212,17 @@ class Post_Command extends \WP_CLI\CommandWithDBObject {
 		);
 		$assoc_args = array_merge( $defaults, $assoc_args );
 
-		parent::delete( $args, $assoc_args );
-	}
+		parent::_delete( $args, $assoc_args, function ( $post_id, $assoc_args ) {
+			$r = wp_delete_post( $post_id, $assoc_args['force'] );
 
-	protected function _delete( $post_id, $assoc_args ) {
-		$r = wp_delete_post( $post_id, $assoc_args['force'] );
+			if ( $r ) {
+				$action = $assoc_args['force'] ? 'Deleted' : 'Trashed';
 
-		if ( $r ) {
-			$action = $assoc_args['force'] ? 'Deleted' : 'Trashed';
-
-			return array( 'success', "$action post $post_id." );
-		} else {
-			return array( 'error', "Failed deleting post $post_id." );
-		}
+				return array( 'success', "$action post $post_id." );
+			} else {
+				return array( 'error', "Failed deleting post $post_id." );
+			}
+		} );
 	}
 
 	/**

php/WP_CLI/CommandWithDBObject.php
@@ -12,14 +12,10 @@ abstract class CommandWithDBObject extends \WP_CLI_Command {
 	protected $obj_type;
 	protected $obj_id_key = 'ID';
 
-	abstract protected function _create( $params );
-	abstract protected function _update( $params );
-	abstract protected function _delete( $obj_id, $assoc_args );
-
-	public function create( $args, $assoc_args ) {
+	protected function _create( $args, $assoc_args, $callback ) {
 		unset( $assoc_args[ $this->obj_id_key ] );
 
-		$obj_id = $this->_create( $assoc_args );
+		$obj_id = $callback( $assoc_args );
 
 		if ( is_wp_error( $obj_id ) ) {
 			\WP_CLI::error( $obj_id );
@@ -31,7 +27,7 @@ abstract class CommandWithDBObject extends \WP_CLI_Command {
 			\WP_CLI::success( "Created $this->obj_type $obj_id." );
 	}
 
-	public function update( $args, $assoc_args ) {
+	protected function _update( $args, $assoc_args, $callback ) {
 		$status = 0;
 
 		if ( empty( $assoc_args ) ) {
@@ -42,7 +38,7 @@ abstract class CommandWithDBObject extends \WP_CLI_Command {
 			$params = array_merge( $assoc_args, array( $this->obj_id_key => $obj_id ) );
 
 			$status = $this->success_or_failure( $this->wp_error_to_resp(
-				$this->_update( $params ),
+				$callback( $params ),
 				"Updated $this->obj_type $obj_id."
 			) );
 		}
@@ -50,11 +46,11 @@ abstract class CommandWithDBObject extends \WP_CLI_Command {
 		exit( $status );
 	}
 
-	public function delete( $args, $assoc_args ) {
+	protected function _delete( $args, $assoc_args, $callback ) {
 		$status = 0;
 
 		foreach ( $args as $obj_id ) {
-			$r = $this->_delete( $obj_id, $assoc_args );
+			$r = $callback( $obj_id, $assoc_args );
 			$status = $this->success_or_failure( $r );
 		}
 

php/commands/comment.php
@@ -35,24 +35,22 @@ class Comment_Command extends \WP_CLI\CommandWithDBObject {
 	 *     wp comment create --comment_post_ID=15 --comment_content="hello blog" --comment_author="wp-cli"
 	 */
 	public function create( $args, $assoc_args ) {
-		parent::create( $args, $assoc_args );
-	}
+		parent::_create( $args, $assoc_args, function ( $params ) {
+			$post = get_post( $params['comment_post_ID'] );
+			if ( !$post ) {
+				return new WP_Error( 'no_post', "Can't find post $comment_post_ID." );
+			}
 
-	protected function _create( $assoc_args ) {
-		$post = get_post( $assoc_args['comment_post_ID'] );
-		if ( !$post ) {
-			return new WP_Error( 'no_post', "Can't find post $comment_post_ID." );
-		}
+			// We use wp_insert_comment() instead of wp_new_comment() to stay at a low level and
+			// avoid wp_die() formatted messages or notifications
+			$comment_id = wp_insert_comment( $params );
 
-		// We use wp_insert_comment() instead of wp_new_comment() to stay at a low level and
-		// avoid wp_die() formatted messages or notifications
-		$comment_id = wp_insert_comment( $assoc_args );
+			if ( !$comment_id ) {
+				return new WP_Error( 'db_error', 'Could not create comment.' );
+			}
 
-		if ( !$comment_id ) {
-			return new WP_Error( 'db_error', 'Could not create comment.' );
-		}
-
-		return $comment_id;
+			return $comment_id;
+		} );
 	}
 
 	/**
@@ -71,15 +69,13 @@ class Comment_Command extends \WP_CLI\CommandWithDBObject {
 	 *     wp comment update 123 --comment_author='That Guy'
 	 */
 	public function update( $args, $assoc_args ) {
-		parent::update( $args, $assoc_args );
-	}
+		parent::_update( $args, $assoc_args, function ( $params ) {
+			if ( !wp_update_comment( $params ) ) {
+				return new WP_Error( 'Could not update comment.' );
+			}
 
-	protected function _update( $params ) {
-		if ( !wp_update_comment( $params ) ) {
-			return new WP_Error( 'Could not update comment.' );
-		}
-
-		return true;
+			return true;
+		} );
 	}
 
 	/**
@@ -207,17 +203,15 @@ class Comment_Command extends \WP_CLI\CommandWithDBObject {
 	 *     wp comment delete 1337 --force
 	 */
 	public function delete( $args, $assoc_args ) {
-		parent::delete( $args, $assoc_args );
-	}
+		parent::_delete( $args, $assoc_args, function ( $comment_id, $assoc_args ) {
+			$r = wp_delete_comment( $comment_id, isset( $assoc_args['force'] ) );
 
-	protected function _delete( $comment_id, $assoc_args ) {
-		$r = wp_delete_comment( $comment_id, isset( $assoc_args['force'] ) );
-
-		if ( $r ) {
-			return array( 'success', "Deleted comment $comment_id." );
-		} else {
-			return array( 'error', "Failed deleting comment $comment_id" );
-		}
+			if ( $r ) {
+				return array( 'success', "Deleted comment $comment_id." );
+			} else {
+				return array( 'error', "Failed deleting comment $comment_id" );
+			}
+		} );
 	}
 
 	private function call( $args, $status, $success, $failure ) {

php/commands/user.php
@@ -165,23 +165,22 @@ class User_Command extends \WP_CLI\CommandWithDBObject {
 		) );
 
 		foreach ( $args as $key => $arg ) {
-			$args[$key] = self::get_user( $arg )->ID;
-		}
-		parent::delete( $args, $assoc_args );
-	}
-
-	protected function _delete( $user_id, $assoc_args ) {
-		if ( is_multisite() ) {
-			$r = wpmu_delete_user( $user_id );
-		} else {
-			$r = wp_delete_user( $user_id, $assoc_args['reassign'] );
+			$args[ $key ] = self::get_user( $arg )->ID;
 		}
 
-		if ( $r ) {
-			return array( 'success', "Deleted user $user_id." );
-		} else {
-			return array( 'error', "Failed deleting user $user_id." );
-		}
+		parent::_delete( $args, $assoc_args, function ( $user_id, $assoc_args ) {
+			if ( is_multisite() ) {
+				$r = wpmu_delete_user( $user_id );
+			} else {
+				$r = wp_delete_user( $user_id, $assoc_args['reassign'] );
+			}
+
+			if ( $r ) {
+				return array( 'success', "Deleted user $user_id." );
+			} else {
+				return array( 'error', "Failed deleting user $user_id." );
+			}
+		} );
 	}
 
 	/**
@@ -237,7 +236,7 @@ class User_Command extends \WP_CLI\CommandWithDBObject {
 			$generated_pass = true;
 		}
 
-		$user_id = $this->_create( array(
+		$user_id = wp_insert_user( array(
 			'user_email' => $user_email,
 			'user_login' => $user_login,
 			'user_pass' => $user_pass,
@@ -264,10 +263,6 @@ class User_Command extends \WP_CLI\CommandWithDBObject {
 		}
 	}
 
-	protected function _create( $params ) {
-		return wp_insert_user( $params );
-	}
-
 	/**
 	 * Update a user.
 	 *
@@ -286,15 +281,11 @@ class User_Command extends \WP_CLI\CommandWithDBObject {
 	 *     wp user update mary --user_pass=marypass
 	 */
 	public function update( $args, $assoc_args ) {
-
 		foreach ( $args as $key => $arg ) {
-			$args[$key] = self::get_user( $arg )->ID;
+			$args[ $key ] = self::get_user( $arg )->ID;
 		}
-		parent::update( $args, $assoc_args, 'user' );
-	}
 
-	protected function _update( $params ) {
-		return wp_update_user( $params );
+		parent::_update( $args, $assoc_args, 'wp_update_user' );
 	}
 
 	/**

commit be316e0b0f8305fb7e7a554d8b6b018d1b3aa83e

wp post edit should no longer call parent::update
This broke in f6285d3 when CommandWithDBObject::update was removed and
_update made protected. It should have been defined as $this->update
from the beginning.

php/commands/post.php
@@ -119,7 +119,7 @@ class Post_Command extends \WP_CLI\CommandWithDBObject {
 		if ( $r === false )
 			\WP_CLI::warning( 'No change made to post content.', 'Aborted' );
 		else
-			parent::update( $args, array( 'post_content' => $r ) );
+			$this->update( $args, array( 'post_content' => $r ) );
 	}
 
 	protected function _edit( $content, $title ) {