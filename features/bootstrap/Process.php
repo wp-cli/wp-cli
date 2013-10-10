336ad5556c8d1fd394bd7b3c6da5a3c025869b8a
Francesco Laffi <francesco.laffi@gmail.com>
2013-10-10 21:09:41 +0200

    packages cache behat tests

--- features/bootstrap/Process.php
+++ features/bootstrap/Process.php
@@ -2,16 +2,17 @@
 
 class Process {
 
-	public static function create( $command, $cwd = null ) {
+	public static function create( $command, $cwd = null, $env = array() ) {
 		$proc = new self;
 
 		$proc->command = $command;
 		$proc->cwd = $cwd;
+		$proc->env = $env;
 
 		return $proc;
 	}
 
-	private $command, $cwd;
+	private $command, $cwd, $env;
 
 	private function __construct() {}
 
@@ -29,11 +30,12 @@ class Process {
 
 		// Ensure we're using the expected `wp` binary
 		$bin_dir = getenv( 'WP_CLI_BIN_DIR' ) ?: realpath( __DIR__ . "/../../bin" );
-
-		$proc = proc_open( $this->command, $descriptors, $pipes, $cwd, array(
+		$env = array_merge( $this->env, array(
 			'PATH' =>  $bin_dir . ':' . getenv( 'PATH' ),
 			'BEHAT_RUN' => 1
 		) );
+
+		$proc = proc_open( $this->command, $descriptors, $pipes, $cwd, $env );
 
 		$STDOUT = stream_get_contents( $pipes[1] );
 		fclose( $pipes[1] );
@@ -46,7 +48,8 @@ class Process {
 			'STDERR' => $STDERR,
 			'return_code' => proc_close( $proc ),
 			'command' => $this->command,
-			'cwd' => $cwd
+			'cwd' => $cwd,
+			'env' => $env
 		) );
 	}
 
