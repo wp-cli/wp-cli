7d154c6a1fe6e534f3eb5af1683191c40ebf38e8
scribu <mail@scribu.net>
2013-11-10 01:01:38 +0200

    keep 'not found' message in Fetcher child classes
    otherwise, might get a mismatch between the class and the message

--- php/WP_CLI/FetcherUser.php
+++ php/WP_CLI/FetcherUser.php
@@ -4,6 +4,8 @@ namespace WP_CLI;
 
 class FetcherUser extends Fetcher {
 
+	protected $msg = "Invalid user ID or login: '%s'";
+
 	public function get( $id_or_login ) {
 		if ( is_numeric( $id_or_login ) )
 			$user = get_user_by( 'id', $id_or_login );
