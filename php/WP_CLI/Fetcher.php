b6abde3bfb3e92b16a2a598fe28280207b9968c9
scribu <mail@scribu.net>
2013-11-10 00:15:47 +0200

    move utilities for validating plugin names to FetcherPlugin class

--- php/WP_CLI/Fetcher.php
+++ php/WP_CLI/Fetcher.php
@@ -4,13 +4,13 @@ namespace WP_CLI;
 
 interface Fetcher {
 
-	// Returns the object if found; otherwise returns false
+	// Returns the item if found; otherwise returns false
 	function get( $id );
 
-	// Returns the object if found; otherwise calls WP_CLI::error()
+	// Returns the item if found; otherwise calls WP_CLI::error()
 	function get_check( $id );
 
-	// Returns the list of found objects
+	// Returns the list of found items
 	function get_many( $ids );
 }
 
