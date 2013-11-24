d98dcff457c44539773ef6374e60feba2b231187
scribu <mail@scribu.net>
2013-11-24 20:03:56 +0200

    fix DB name replacement

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -43,7 +43,7 @@ install_test_suite() {
 
 	wget -nv -O wp-tests-config.php http://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
 	sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
-	sed $ioption "s/yourdbnamehere/$DB_NAME/" wp-tests-config.php
+	sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
 	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
 	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
 	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
