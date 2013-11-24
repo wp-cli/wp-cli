41da7bf7fe80631261cf6c24f71156eed184bdbe
scribu <mail@scribu.net>
2013-11-24 20:00:31 +0200

    refactor install-wp-tests.sh into 3 functions

--- templates/install-wp-tests.sh
+++ templates/install-wp-tests.sh
@@ -11,55 +11,65 @@ DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-master}
 
+WP_CORE_DIR=/tmp/wordpress/
+
 set -ex
 
-# set up a WP install
-WP_CORE_DIR=/tmp/wordpress/
-mkdir -p $WP_CORE_DIR
+install_wp() {
+	mkdir -p $WP_CORE_DIR
 
-if [ $WP_VERSION == 'latest' ]; then 
-	ARCHIVE_NAME='latest'
-else
-	ARCHIVE_NAME="wordpress-$WP_VERSION"
-fi
+	if [ $WP_VERSION == 'latest' ]; then 
+		local ARCHIVE_NAME='latest'
+	else
+		local ARCHIVE_NAME="wordpress-$WP_VERSION"
+	fi
 
-wget -nv -O /tmp/wordpress.tar.gz http://wordpress.org/${ARCHIVE_NAME}.tar.gz
-tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+	wget -nv -O /tmp/wordpress.tar.gz http://wordpress.org/${ARCHIVE_NAME}.tar.gz
+	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+}
 
-# portable in-place argument for both GNU sed and Mac OSX sed
-if [[ $(uname -s) == 'Darwin' ]]; then
-  ioption='-i ""'
-else
-  ioption='-i'
-fi
+install_test_suite() {
+	# portable in-place argument for both GNU sed and Mac OSX sed
+	if [[ $(uname -s) == 'Darwin' ]]; then
+		local ioption='-i ""'
+	else
+		local ioption='-i'
+	fi
 
-# set up testing suite
-mkdir -p $WP_TESTS_DIR
-cd $WP_TESTS_DIR
-svn co --quiet http://develop.svn.wordpress.org/trunk/tests/phpunit/includes/
+	# set up testing suite
+	mkdir -p $WP_TESTS_DIR
+	cd $WP_TESTS_DIR
+	svn co --quiet http://develop.svn.wordpress.org/trunk/tests/phpunit/includes/
 
-wget -nv -O wp-tests-config.php http://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
-sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
-sed $ioption "s/yourdbnamehere/$DB_NAME/" wp-tests-config.php
-sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
-sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
-sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+	wget -nv -O wp-tests-config.php http://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
+	sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
+	sed $ioption "s/yourdbnamehere/$DB_NAME/" wp-tests-config.php
+	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
+	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
+	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+}
 
-# parse DB_HOST for port or socket references
-PARTS=(${DB_HOST//\:/ })
-DB_HOSTNAME=${PARTS[0]};
-DB_SOCK_OR_PORT=${PARTS[1]};
-EXTRA=""
+install_db() {
+	# parse DB_HOST for port or socket references
+	local PARTS=(${DB_HOST//\:/ })
+	local DB_HOSTNAME=${PARTS[0]};
+	local DB_SOCK_OR_PORT=${PARTS[1]};
+	local EXTRA=""
 
-if ! [ -z $DB_HOSTNAME ] ; then
-  if [[ "$DB_SOCK_OR_PORT" =~ ^[0-9]+$ ]] ; then
-    EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-  elif ! [ -z $DB_SOCK_OR_PORT ] ; then
-    EXTRA=" --socket=$DB_SOCK_OR_PORT"
-  elif ! [ -z $DB_HOSTNAME ] ; then
-    EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
-  fi
-fi
+	if ! [ -z $DB_HOSTNAME ] ; then
+		if [[ "$DB_SOCK_OR_PORT" =~ ^[0-9]+$ ]] ; then
+			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
+		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
+			EXTRA=" --socket=$DB_SOCK_OR_PORT"
+		elif ! [ -z $DB_HOSTNAME ] ; then
+			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
+		fi
+	fi
 
-# create database
-mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	# create database
+	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+}
+
+install_wp
+install_test_suite
+install_db
