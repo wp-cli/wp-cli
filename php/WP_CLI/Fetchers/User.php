5a76534885a47782db3484862f9abc42edeac3ab
Daniel Bachhuber <d@danielbachhuber.com>
2013-12-26 12:26:51 -0800

    Perform `is_numeric()` checks before `is_email()`, as the former will be faster.

--- php/WP_CLI/Fetchers/User.php
+++ php/WP_CLI/Fetchers/User.php
@@ -8,10 +8,10 @@ class User extends Base {
 
 	public function get( $id_email_or_login ) {
 
-		if ( is_email( $id_email_or_login ) )
-			$user = get_user_by( 'email', $id_email_or_login );
-		else if ( is_numeric( $id_email_or_login ) )
+		if ( is_numeric( $id_email_or_login ) )
 			$user = get_user_by( 'id', $id_email_or_login );
+		else if ( is_email( $id_email_or_login ) )
+			$user = get_user_by( 'email', $id_email_or_login );
 		else
 			$user = get_user_by( 'login', $id_email_or_login );
 
