221456309c7dfefa5d3ba68a245010624772fac7
Daniel Bachhuber <d@danielbachhuber.com>
2013-12-29 10:21:07 -0800

    Try setting `HOME` in the Behat process
    6289de197b47393c2b3c5e3518191f52e3901c72 didn't work as expected. This is another attempt.

--- features/bootstrap/Process.php
+++ features/bootstrap/Process.php
@@ -32,7 +32,8 @@ class Process {
 		$bin_dir = getenv( 'WP_CLI_BIN_DIR' ) ?: realpath( __DIR__ . "/../../bin" );
 		$env = array_merge( $this->env, array(
 			'PATH' =>  $bin_dir . ':' . getenv( 'PATH' ),
-			'BEHAT_RUN' => 1
+			'BEHAT_RUN' => 1,
+			'HOME' => '/tmp',
 		) );
 
 		$proc = proc_open( $this->command, $descriptors, $pipes, $cwd, $env );
