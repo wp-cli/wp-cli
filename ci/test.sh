8973550d48e874fa768cfe204889432392ea07ef
scribu <mail@scribu.net>
2014-01-18 12:47:24 +0200

    test the package command against a direct clone of WP-CLI

--- ci/test.sh
+++ ci/test.sh
@@ -8,5 +8,8 @@ phpunit
 # Run the functional tests
 php behat.phar --format progress
 
+# Run the package tests against a direct clone of WP-CLI
+WP_CLI_BIN_DIR='' php behat.phar --format progress features/package.feature
+
 # Run CodeSniffer
 ./codesniffer/scripts/phpcs --standard=./ci/ php/
