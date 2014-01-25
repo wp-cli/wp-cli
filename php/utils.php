8768bedfeabe6e89b46dceb5cb5e36fc2b91803f
scribu <mail@scribu.net>
2014-01-25 16:41:09 +0200

    extract replace_path_consts() utility

--- php/utils.php
+++ php/utils.php
@@ -382,3 +382,21 @@ function is_windows() {
 	return strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
 }
 
+/**
+ * Replace magic constants in some PHP source code.
+ *
+ * @param string $source The PHP code to manipulate.
+ * @param string $path The path to use instead of the magic constants
+ */
+function replace_path_consts( $source, $path ) {
+	$replacements = array(
+		'__FILE__' => "'$path'",
+		'__DIR__'  => "'" . dirname( $path ) . "'"
+	);
+
+	$old = array_keys( $replacements );
+	$new = array_values( $replacements );
+
+	return str_replace( $old, $new, $source );
+}
+
