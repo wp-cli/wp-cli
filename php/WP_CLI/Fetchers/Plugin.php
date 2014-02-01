e614cb569c631cc7fc29c4774b50fe69efa5e06a
scribu <mail@scribu.net>
2014-02-01 23:52:19 +0200

    make local plugin lookup case-sensitive
    the old lookup method returned false positives on case-insensitive
    filesystems
    
    (plugin basenames are treated as case-sensitive by WordPress)
    
    fixes #958

--- php/WP_CLI/Fetchers/Plugin.php
+++ php/WP_CLI/Fetchers/Plugin.php
@@ -7,29 +7,11 @@ class Plugin extends Base {
 	protected $msg = "The '%s' plugin could not be found.";
 
 	public function get( $name ) {
-		$plugins = get_plugins( '/' . $name );
-
-		// some-plugin/the-plugin.php
-		while ( !empty( $plugins ) ) {
-			$file = key( $plugins );
-			array_shift( $plugins );
-
-			// ignore files inside a plugin's subdirectory (like WP does)
-			if ( dirname( $file ) == '.' ) {
-				return (object) array(
-					'name' => $name,
-					'file' => $name . '/' . $file
-				);
+		foreach ( get_plugins() as $file => $_ ) {
+			if ( $file === "$name.php" ||
+				( dirname( $file ) === $name && $name !== '.' ) ) {
+				return (object) compact( 'name', 'file' );
 			}
-		}
-
-		// some-plugin.php
-		$file = $name . '.php';
-
-		$plugins = get_plugins();
-
-		if ( isset( $plugins[ $file ] ) ) {
-			return (object) compact( 'name', 'file' );
 		}
 
 		return false;
