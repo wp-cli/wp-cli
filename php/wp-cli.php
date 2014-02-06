7926fba978ae901758a7794ff0352197e2624647
scribu <mail@scribu.net>
2014-02-06 02:02:26 +0200

    bump version to 0.14.0

--- php/wp-cli.php
+++ php/wp-cli.php
@@ -2,7 +2,7 @@
 
 // Can be used by plugins/themes to check if WP-CLI is running or not
 define( 'WP_CLI', true );
-define( 'WP_CLI_VERSION', '0.14.0-beta' );
+define( 'WP_CLI_VERSION', '0.14.0' );
 
 // Set common headers, to prevent warnings from plugins
 $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.0';
