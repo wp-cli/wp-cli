6857028f237537c95509e89f00ae433cb842efb4
Kailey Lampert <trepmal@gmail.com>
2014-02-07 12:30:43 -0800

    Add `wp role reset` - resets any given default role to default capabilities

--- php/commands/role.php
+++ php/commands/role.php
@@ -139,6 +139,178 @@ class Role_Command extends WP_CLI_Command {
 
 	}
 
+	/**
+	 * Reset any default role to default capabilities.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <role-key>
+	 * : The internal name of the role.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp role reset administrator
+	 *
+	 *     wp role reset author
+	 */
+	public function reset( $args ) {
+
+		self::persistence_check();
+
+		$role_key = array_shift( $args );
+
+		if ( empty( $role_key ) || ! in_array($role_key, array( 'administrator', 'editor', 'author', 'contributor', 'subscriber' ) ) )
+			WP_CLI::error( "Role key not provided, or is invalid." );
+
+		if ( $role_key == 'administrator') {
+			remove_role( $role_key );
+			add_role('administrator', 'Administrator');
+			$role = get_role('administrator');
+			$role->add_cap('switch_themes');
+			$role->add_cap('edit_themes');
+			$role->add_cap('activate_plugins');
+			$role->add_cap('edit_plugins');
+			$role->add_cap('edit_users');
+			$role->add_cap('edit_files');
+			$role->add_cap('manage_options');
+			$role->add_cap('moderate_comments');
+			$role->add_cap('manage_categories');
+			$role->add_cap('manage_links');
+			$role->add_cap('upload_files');
+			$role->add_cap('import');
+			$role->add_cap('unfiltered_html');
+			$role->add_cap('edit_posts');
+			$role->add_cap('edit_others_posts');
+			$role->add_cap('edit_published_posts');
+			$role->add_cap('publish_posts');
+			$role->add_cap('edit_pages');
+			$role->add_cap('read');
+			$role->add_cap('level_10');
+			$role->add_cap('level_9');
+			$role->add_cap('level_8');
+			$role->add_cap('level_7');
+			$role->add_cap('level_6');
+			$role->add_cap('level_5');
+			$role->add_cap('level_4');
+			$role->add_cap('level_3');
+			$role->add_cap('level_2');
+			$role->add_cap('level_1');
+			$role->add_cap('level_0');
+			$role->add_cap('edit_others_pages');
+			$role->add_cap('edit_published_pages');
+			$role->add_cap('publish_pages');
+			$role->add_cap('delete_pages');
+			$role->add_cap('delete_others_pages');
+			$role->add_cap('delete_published_pages');
+			$role->add_cap('delete_posts');
+			$role->add_cap('delete_others_posts');
+			$role->add_cap('delete_published_posts');
+			$role->add_cap('delete_private_posts');
+			$role->add_cap('edit_private_posts');
+			$role->add_cap('read_private_posts');
+			$role->add_cap('delete_private_pages');
+			$role->add_cap('edit_private_pages');
+			$role->add_cap('read_private_pages');
+			$role->add_cap('delete_users');
+			$role->add_cap('create_users');
+			$role->add_cap( 'unfiltered_upload' );
+			$role->add_cap( 'edit_dashboard' );
+			$role->add_cap( 'update_plugins' );
+			$role->add_cap( 'delete_plugins' );
+			$role->add_cap( 'install_plugins' );
+			$role->add_cap( 'update_themes' );
+			$role->add_cap( 'install_themes' );
+			$role->add_cap( 'update_core' );
+			$role->add_cap( 'list_users' );
+			$role->add_cap( 'remove_users' );
+
+			// Never used, will be removed. create_users or
+			// promote_users is the capability you're looking for.
+			$role->add_cap( 'add_users' );
+
+			$role->add_cap( 'promote_users' );
+			$role->add_cap( 'edit_theme_options' );
+			$role->add_cap( 'delete_themes' );
+			$role->add_cap( 'export' );
+
+		}
+		if ( 'editor' == $role_key ) {
+			remove_role( $role_key );
+			add_role('editor', 'Editor');
+			$role = get_role('editor');
+			$role->add_cap('moderate_comments');
+			$role->add_cap('manage_categories');
+			$role->add_cap('manage_links');
+			$role->add_cap('upload_files');
+			$role->add_cap('unfiltered_html');
+			$role->add_cap('edit_posts');
+			$role->add_cap('edit_others_posts');
+			$role->add_cap('edit_published_posts');
+			$role->add_cap('publish_posts');
+			$role->add_cap('edit_pages');
+			$role->add_cap('read');
+			$role->add_cap('level_7');
+			$role->add_cap('level_6');
+			$role->add_cap('level_5');
+			$role->add_cap('level_4');
+			$role->add_cap('level_3');
+			$role->add_cap('level_2');
+			$role->add_cap('level_1');
+			$role->add_cap('level_0');
+			$role->add_cap('edit_others_pages');
+			$role->add_cap('edit_published_pages');
+			$role->add_cap('publish_pages');
+			$role->add_cap('delete_pages');
+			$role->add_cap('delete_others_pages');
+			$role->add_cap('delete_published_pages');
+			$role->add_cap('delete_posts');
+			$role->add_cap('delete_others_posts');
+			$role->add_cap('delete_published_posts');
+			$role->add_cap('delete_private_posts');
+			$role->add_cap('edit_private_posts');
+			$role->add_cap('read_private_posts');
+			$role->add_cap('delete_private_pages');
+			$role->add_cap('edit_private_pages');
+			$role->add_cap('read_private_pages');
+
+		}
+		if ( 'author' == $role_key ) {
+			remove_role( $role_key );
+			add_role('author', 'Author');
+			$role = get_role('author');
+			$role->add_cap('upload_files');
+			$role->add_cap('edit_posts');
+			$role->add_cap('edit_published_posts');
+			$role->add_cap('publish_posts');
+			$role->add_cap('read');
+			$role->add_cap('level_2');
+			$role->add_cap('level_1');
+			$role->add_cap('level_0');
+			$role->add_cap('delete_posts');
+			$role->add_cap('delete_published_posts');
+		}
+		if ( 'contributor' == $role_key ) {
+			remove_role( $role_key );
+			add_role('contributor', 'Contributor');
+			$role = get_role('contributor');
+			$role->add_cap('edit_posts');
+			$role->add_cap('read');
+			$role->add_cap('level_1');
+			$role->add_cap('level_0');
+			$role->add_cap('delete_posts');
+		}
+		if ( 'subscriber' == $role_key ) {
+			remove_role( $role_key );
+			add_role('subscriber', 'Subscriber');
+			$role = get_role('subscriber');
+			$role->add_cap('read');
+			$role->add_cap('level_0');
+		}
+
+		WP_CLI::success( sprintf( "Role with key %s reset.", $role_key ) );
+
+	}
+
 	private static function persistence_check() {
 		global $wp_roles;
 
