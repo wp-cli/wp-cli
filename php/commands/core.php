79f2f06f6d688d7907ee2e75eb15e2e16d797f95
scribu <mail@scribu.net>
2014-02-09 15:40:45 +0200

    wp core install: validate admin email
    closes #358

--- php/commands/core.php
+++ php/commands/core.php
@@ -459,6 +459,10 @@ class Core_Command extends WP_CLI_Command {
 		$public = true;
 
 		// @codingStandardsIgnoreStart
+		if ( !is_email( $admin_email ) ) {
+			WP_CLI::error( "The '{$admin_email}' email address is invalid." );
+		}
+
 		$result = wp_install( $title, $admin_user, $admin_email, $public, '', $admin_password );
 
 		if ( is_wp_error( $result ) ) {
