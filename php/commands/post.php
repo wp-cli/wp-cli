0d2fc1d571800ed3797fd588f3c31334b6ade605
scribu <mail@scribu.net>
2014-02-11 03:17:30 +0200

    convert `wp *-meta` subcommands to `wp * meta`

--- php/commands/post.php
+++ php/commands/post.php
@@ -394,4 +394,22 @@ class Post_Command extends \WP_CLI\CommandWithDBObject {
 	}
 }
 
+/**
+ * Manage post custom fields.
+ *
+ * ## OPTIONS
+ *
+ * [--format=json]
+ * : Encode/decode values as JSON.
+ *
+ * ## EXAMPLES
+ *
+ *     wp post meta set 123 _wp_page_template about.php
+ */
+class Post_Meta_Command extends \WP_CLI\CommandWithMeta {
+	protected $meta_type = 'post';
+}
+
 WP_CLI::add_command( 'post', 'Post_Command' );
+WP_CLI::add_command( 'post meta', 'Post_Meta_Command' );
+
