fd40c3159eaa96585c2af69941e3f7d399623d05
scribu <mail@scribu.net>
2014-02-13 23:00:57 +0200

    wp eval-file: allow passing arbitrary arguments to the script
    this behavior is more useful than evaluating multiple files, which can
    be replicated using --require= multiple times
    
    closes #1014

--- features/eval.feature
+++ features/eval.feature
@@ -9,20 +9,14 @@ Feature: Evaluating PHP code and files.
       bool(true)
       """
 
-    Given a script1.php file:
+    Given a script.php file:
       """
       <?php
-      WP_CLI::line("script 1");
-      """
-    And a script2.php file:
-      """
-      <?php
-      WP_CLI::line("script 2");
+      WP_CLI::line( implode( ' ', $args ) );
       """
 
-    When I run `wp eval-file script1.php script2.php`
+    When I run `wp eval-file script.php foo bar`
     Then STDOUT should be:
       """
-      script 1
-      script 2
+      foo bar
       """
--- php/commands/eval-file.php
+++ php/commands/eval-file.php
@@ -5,19 +5,26 @@ class EvalFile_Command extends WP_CLI_Command {
 	/**
 	 * Load and execute a PHP file after loading WordPress.
 	 *
-	 * ## EXAMPLES
+	 * ## OPTIONS
 	 *
-	 * [<file>...]
+	 * <file>
 	 * : The path to the PHP file to execute.
+	 *
+	 * [<arg>...]
+	 * : One or more arguments to pass to the file.
 	 */
-	public function __invoke( $args, $assoc_args ) {
-		foreach ( $args as $file ) {
-			if ( !file_exists( $file ) ) {
-				WP_CLI::error( "'$file' does not exist." );
-			} else {
-				include( $file );
-			}
+	public function __invoke( $args ) {
+		$file = array_shift( $args );
+
+		if ( !file_exists( $file ) ) {
+			WP_CLI::error( "'$file' does not exist." );
 		}
+
+		self::_eval( $file, $args );
+	}
+
+	private static function _eval( $file, $args ) {
+		include( $file );
 	}
 }
 
