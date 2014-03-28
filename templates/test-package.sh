09e91d69cdfbd04dc6ffeea478a0f112aff34a62
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-28 10:45:17 -0700

    Allow the test bash script to be run from anywhere

--- templates/test-package.sh
+++ templates/test-package.sh
@@ -5,7 +5,8 @@ set -ex
 # Set up the environment
 WP_CLI_DIR=${WP_CLI_DIR-/tmp/wp-cli}
 PACKAGE_TEST_CONFIG_PATH=${PACKAGE_TEST_CONFIG_PATH-/tmp/wp-cli-package-test.yml}
+PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
 export PACKAGE_TEST_CONFIG_PATH=${PACKAGE_TEST_CONFIG_PATH}
 
 # Run the functional tests
-$WP_CLI_DIR/vendor/bin/behat --config=$WP_CLI_DIR/behat.yml features
+$WP_CLI_DIR/vendor/bin/behat --config=$WP_CLI_DIR/behat.yml $PACKAGE_DIR/features
