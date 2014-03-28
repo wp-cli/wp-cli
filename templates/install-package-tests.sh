af4a33dd34777211e7a31f2fb1824f9d8edb8a76
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-28 10:09:52 -0700

    Revert "We don't even need database creation here"
    This reverts commit 08656da1acf715b9c5be4156ec0ca608b5139105.
    
    Conflicts:
    	templates/install-package-tests.sh

--- templates/install-package-tests.sh
+++ templates/install-package-tests.sh
@@ -36,5 +36,13 @@ set_package_context() {
 
 }
 
+install_db() {
+
+	# For wp_cli_test too
+	mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot
+	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
+}
+
 install_wp_cli_suite
 set_package_context
+install_db
