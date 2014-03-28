8884ebe8e898719034eae35e951f00ffa7876da5
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-28 08:47:52 -0700

    Greatly simplify package test setup
    While it might be nice to eventually run tests against multiple version of WordPress, WP-CLI handles installing WordPress for us. A lot of this setup is unnecessary for package tests.

--- templates/install-package-tests.sh
+++ templates/install-package-tests.sh
@@ -1,67 +1,31 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 3 ]; then
-	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version]"
-	exit 1
-fi
-
-DB_NAME=$1
-DB_USER=$2
-DB_PASS=$3
-DB_HOST=${4-localhost}
-WP_VERSION=${5-latest}
-
 WP_CLI_DIR=${WP_CLI_DIR-/tmp/wp-cli}
-WP_CORE_DIR=/tmp/wordpress/
 
 set -ex
-
-install_wp() {
-	mkdir -p $WP_CORE_DIR
-
-	if [ $WP_VERSION == 'latest' ]; then 
-		local ARCHIVE_NAME='latest'
-	else
-		local ARCHIVE_NAME="wordpress-$WP_VERSION"
-	fi
-
-	wget -nv -O /tmp/wordpress.tar.gz http://wordpress.org/${ARCHIVE_NAME}.tar.gz
-	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
-
-	wget -nv -O $WP_CORE_DIR/wp-content/db.php https://raw.github.com/markoheijnen/wp-mysqli/master/db.php
-}
 
 install_wp_cli_suite() {
 
 	# Set up WP-CLI
-	git clone --quiet -b scaffold-package-tests https://github.com/wp-cli/wp-cli.git $WP_CLI_DIR
-	cd $WP_CLI_DIR
-	curl -sS https://getcomposer.org/installer | php
-	chmod +x composer.phar
-	./composer.phar install
+	if [ ! -d $WP_CLI_DIR ]
+		then
+			git clone --quiet -b scaffold-package-tests https://github.com/wp-cli/wp-cli.git $WP_CLI_DIR
+			cd $WP_CLI_DIR
+			curl -sS https://getcomposer.org/installer | php
+			chmod +x composer.phar
+			./composer.phar install
+	else
+		cd $WP_CLI_DIR
+		git pull
+	fi
 }
 
 install_db() {
-	# parse DB_HOST for port or socket references
-	local PARTS=(${DB_HOST//\:/ })
-	local DB_HOSTNAME=${PARTS[0]};
-	local DB_SOCK_OR_PORT=${PARTS[1]};
-	local EXTRA=""
 
-	if ! [ -z $DB_HOSTNAME ] ; then
-		if [[ "$DB_SOCK_OR_PORT" =~ ^[0-9]+$ ]] ; then
-			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
-			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif ! [ -z $DB_HOSTNAME ] ; then
-			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
-		fi
-	fi
-
-	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	# For wp_cli_test too
+	mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot
+	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1"' -uroot
 }
 
-install_wp
 install_wp_cli_suite
 install_db
