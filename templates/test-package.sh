7ee81a9885e28731859efc8b9ab6aea8f53d148b
Daniel Bachhuber <d@danielbachhuber.com>
2014-04-04 15:13:40 -0700

    Allow specific features to be run, but default to the entire set

--- templates/test-package.sh
+++ templates/test-package.sh
@@ -8,5 +8,12 @@ PACKAGE_TEST_CONFIG_PATH=${PACKAGE_TEST_CONFIG_PATH-/tmp/wp-cli-package-test.yml
 PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
 export PACKAGE_TEST_CONFIG_PATH=${PACKAGE_TEST_CONFIG_PATH}
 
+if [ ! -z "$1" ]
+  then
+    FEATURES_DIR="$1"
+else
+    FEATURES_DIR="$PACKAGE_DIR/features"
+fi
+
 # Run the functional tests
-$WP_CLI_DIR/vendor/bin/behat --config=$WP_CLI_DIR/behat.yml $PACKAGE_DIR/features
+$WP_CLI_DIR/vendor/bin/behat --config=$WP_CLI_DIR/behat.yml $FEATURES_DIR
