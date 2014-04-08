20099c177db002643cc2e9a924dc7183aa2c0d5d
Daniel Bachhuber <d@danielbachhuber.com>
2014-04-08 12:56:29 -0700

    Command to list registered sidebars

---
+++ php/commands/sidebar.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Manage sidebars.
+ */
+class Sidebar_Command extends WP_CLI_Command {
+
+	private $fields = array(
+		'name',
+		'id',
+		'description'
+	);
+
+	/**
+	 * List registered sidebars.
+	 * 
+	 * ## OPTIONS
+	 *
+	 * [--fields=<fields>]
+	 * : Limit the output to specific object fields. Defaults to name, id, description
+	 *
+	 * [--format=<format>]
+	 * : Accepted values: table, csv, json, count. Default: table
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp sidebar list --fields=role --format=csv
+	 *
+	 * @subcommand list
+	 */
+	public function _list( $args, $assoc_args ) {
+		global $wp_registered_sidebars;	
+
+		$output_sidebars = array();
+		foreach( $wp_registered_sidebars as $registered_sidebar ) {
+			$output_sidebars[] = (object)$registered_sidebar;
+		}
+
+		$formatter = new \WP_CLI\Formatter( $assoc_args, $this->fields );
+		$formatter->display_items( $output_sidebars );
+
+	}
+
+}
+
+WP_CLI::add_command( 'sidebar', 'Sidebar_Command' );
