32118f3a3f3e649247432f6f8fbdd2043a27a690
Ryan Duff <ryan@fusionized.com>
2014-04-17 13:12:53 -0400

    Wrap with quotes for ZSH support

--- utils/wp-completion.bash
+++ utils/wp-completion.bash
@@ -4,7 +4,7 @@ _wp_complete() {
 	local cur=${COMP_WORDS[COMP_CWORD]}
 
 	IFS=$'\n';  # want to preserve spaces at the end
-	local opts=( $(wp cli completions --line="$COMP_LINE" --point="$COMP_POINT") )
+	local opts="$(wp cli completions --line="$COMP_LINE" --point="$COMP_POINT")"
 
 	if [[ $opts = "<file>" ]]
 	then
