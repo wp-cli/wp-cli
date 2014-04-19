193ab8cac3d26a145292aa7232f216be2b994853
Gabor Javorszky <gabor@javorszky.co.uk>
2014-04-19 17:22:53 +0100

    Moved logging to a HHVM and PHP compatible mode
    Fixes #1134

--- php/WP_CLI/Loggers/Regular.php
+++ php/WP_CLI/Loggers/Regular.php
@@ -6,19 +6,22 @@ class Regular {
 
 	function __construct( $in_color ) {
 		$this->in_color = $in_color;
+		$this->stdout = fopen('php://stdout', 'w');
+		$this->stderr = fopen('php://stderr', 'w');
 	}
 
 	protected function write( $handle, $str ) {
-		fwrite( $handle, $str );
+
+		fwrite( $this->$handle, $str );
 	}
 
-	private function _line( $message, $label, $color, $handle = STDOUT ) {
+	private function _line( $message, $label, $color, $handle = 'stdout' ) {
 		$label = \cli\Colors::colorize( "$color$label:%n", $this->in_color );
 		$this->write( $handle, "$label $message\n" );
 	}
 
 	function info( $message ) {
-		$this->write( STDOUT, $message . "\n" );
+		$this->write( 'stdout', $message . "\n" );
 	}
 
 	function success( $message ) {
@@ -26,11 +29,11 @@ class Regular {
 	}
 
 	function warning( $message ) {
-		$this->_line( $message, 'Warning', '%C', STDERR );
+		$this->_line( $message, 'Warning', '%C', 'stderr' );
 	}
 
 	function error( $message ) {
-		$this->_line( $message, 'Error', '%R', STDERR );
+		$this->_line( $message, 'Error', '%R', 'stderr' );
 	}
 }
 
