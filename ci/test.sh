497de626eb7fe14cce69f560ed36a954b0c05223
Daniel Bachhuber <d@danielbachhuber.com>
2014-05-02 10:19:11 -0700

    Only run this one feature

--- ci/test.sh
+++ ci/test.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-cat script.gdb | sudo gdb –args ~/.phpenv/versions/$(phpenv version-name)/bin/php ~/.phpenv/versions/$(phpenv version-name)/bin/phpunit
+cat ci/script.gdb | sudo gdb –args ~/.phpenv/versions/$(phpenv version-name)/bin/php ~/.phpenv/versions/$(phpenv version-name)/bin/phpunit
 
 # Run the unit tests
-vendor/bin/phpunit
+# vendor/bin/phpunit
 
 # Run the functional tests
-vendor/bin/behat --format progress
+vendor/bin/behat --format progress features/media.feature
 
 # Run CodeSniffer
-./codesniffer/scripts/phpcs --standard=./ci/ php/
+# ./codesniffer/scripts/phpcs --standard=./ci/ php/
