389e85473ed804a87ca4bb0ff25dafe6c42950da
Daniel Bachhuber <d@danielbachhuber.com>
2014-05-02 12:07:10 -0700

    Properly run `gdb` with behat

--- ci/test.sh
+++ ci/test.sh
@@ -2,13 +2,11 @@
 
 set -ex
 
-cat ci/script.gdb | sudo gdb -args ~/.phpenv/versions/$(phpenv version-name)/bin/php ~/.phpenv/versions/$(phpenv version-name)/bin/phpunit
-
 # Run the unit tests
 # vendor/bin/phpunit
 
 # Run the functional tests
-vendor/bin/behat --format progress features/media.feature
+gdb --args ./vendor/bin/behat --format progress features/media.feature
 
 # Run CodeSniffer
 # ./codesniffer/scripts/phpcs --standard=./ci/ php/
