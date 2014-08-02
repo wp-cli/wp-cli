054e3e22855e0a2b7f10fb3db313209335a3ef39
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-02 11:16:44 -0400

    Update the docs some

--- php/commands/scaffold.php
+++ php/commands/scaffold.php
@@ -302,18 +302,28 @@ class Scaffold_Command extends WP_CLI_Command {
 	}
 
 	/**
-	 * Generate files needed for running PHPUnit tests.
+	 * Generate files needed for writing Behat tests for your command.
 	 *
 	 * ## DESCRIPTION
 	 *
 	 * These are the files that are generated:
 	 *
 	 * * `.travis.yml` is the configuration file for Travis CI
+	 * * `bin/install-package-tests.sh` will configure environment to run tests. Script expects WP_CLI_BIN_DIR and WP_CLI_CONFIG_PATH environment variables.
+	 * * `features/load-wp-cli.feature` is a basic test to confirm WP-CLI can load.
+	 * * `features/bootstrap`, `features/steps`, `features/extra` are Behat configuration files.
+	 * * `utils/generate-package-require-from-composer.php` generates a test config.yml file from your package's composer.json
+	 *
+	 * ## ENVIRONMENT
+	 *
+	 * The `features/bootstrap/FeatureContext.php` file expects the WP_CLI_BIN_DIR and WP_CLI_CONFIG_PATH environment variables.
+	 *
+	 * WP-CLI Behat framework uses Behat ~2.5.
 	 *
 	 * ## OPTIONS
 	 *
 	 * <dir>
-	 * : The package directory to generate tests for
+	 * : The package directory to generate tests for.
 	 *
 	 * ## EXAMPLE
 	 *
