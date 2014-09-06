97518998dc69e060bc63dc6944814b63623ec6e3
Daniel Bachhuber <d@danielbachhuber.com>
2014-09-06 10:59:17 -0700

    Denote available fields for `wp widget list`

--- php/commands/widget.php
+++ php/commands/widget.php
@@ -34,10 +34,21 @@ class Widget_Command extends WP_CLI_Command {
 	 * : ID for the corresponding sidebar.
 	 *
 	 * [--fields=<fields>]
-	 * : Limit the output to specific object fields. Defaults to name, id, description
+	 * : Limit the output to specific object fields.
 	 *
 	 * [--format=<format>]
 	 * : Accepted values: table, csv, json, count, ids. Default: table
+	 *
+	 * ## AVAILABLE FIELDS
+	 *
+	 * These fields will be displayed by default for each widget:
+	 *
+	 * * name
+	 * * id
+	 * * position
+	 * * options
+	 *
+	 * There are no optionally available fields.
 	 *
 	 * ## EXAMPLES
 	 *
