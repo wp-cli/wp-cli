53776e5c64c09376a7eadfc98e96285bbcb2032e
Daniel Bachhuber <d@danielbachhuber.com>
2014-12-19 12:33:43 -0800

    Update plugin fetcher to match on full path
    WordPress stores dirname/filename.php in the database, so this is a
    valid name to use

--- php/WP_CLI/Fetchers/Plugin.php
+++ php/WP_CLI/Fetchers/Plugin.php
@@ -21,6 +21,7 @@ class Plugin extends Base {
 	public function get( $name ) {
 		foreach ( get_plugins() as $file => $_ ) {
 			if ( $file === "$name.php" ||
+				( $name && $file === $name ) ||
 				( dirname( $file ) === $name && $name !== '.' ) ) {
 				return (object) compact( 'name', 'file' );
 			}
