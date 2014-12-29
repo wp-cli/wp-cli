2b7949c19d39fe276fac3c9942f887902928792f
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2014-12-29 14:37:57 +0100

    indent using tabs

--- php/commands/package.php
+++ php/commands/package.php
@@ -9,136 +9,136 @@ use WP_CLI\Process;
  * @package wp-cli
  */
 class Package_Command extends WP_CLI_Command {
-  private $home;
-  private $current_patj;
+	private $home;
+	private $current_patj;
 
-  public function __construct() {
-    $this->home    = getenv( 'HOME' ) . '/.wp-cli';
-    $this->current = getcwd();
-  }
+	public function __construct() {
+		$this->home    = getenv( 'HOME' ) . '/.wp-cli';
+		$this->current = getcwd();
+	}
 
-  /**
-   * Install a community package
-   *
-   * ## OPTIONS
-   *
-   * <package>
-   * : the slug of the package
-   *
-   * [--repo=<url>]
-   * : The package repository url
-   *
-   * [--version=<version>]
-   * : The package version/tag
-   *
-   * @subcommand install
-   *
-   * @when before_wp_load
-   */
-  public function install( $args, $assoc_args ) {
-    extract( $assoc_args );
-    $type = 'packagist';
+	/**
+	 * Install a community package
+	 *
+	 * ## OPTIONS
+	 *
+	 * <package>
+	 * : the slug of the package
+	 *
+	 * [--repo=<url>]
+	 * : The package repository url
+	 *
+	 * [--version=<version>]
+	 * : The package version/tag
+	 *
+	 * @subcommand install
+	 *
+	 * @when before_wp_load
+	 */
+	public function install( $args, $assoc_args ) {
+		extract( $assoc_args );
+		$type = 'packagist';
 
-    if ( ! isset( $version ) ) {
-      $version = 'dev-master';
-    }
+		if ( ! isset( $version ) ) {
+			$version = 'dev-master';
+		}
 
-    // when a repo has been provided set the type
-    if ( isset( $repo ) ) {
-      $type = 'vcs';
-    }
+		// when a repo has been provided set the type
+		if ( isset( $repo ) ) {
+			$type = 'vcs';
+		}
 
-    if ( ! file_exists( $this->home ) ) {
-      mkdir( $this->home );
-    }
+		if ( ! file_exists( $this->home ) ) {
+			mkdir( $this->home );
+		}
 
-    if ( ! file_exists( $this->home . '/composer.json' ) ) {
-      $this->create_composer_file();
-    }
+		if ( ! file_exists( $this->home . '/composer.json' ) ) {
+			$this->create_composer_file();
+		}
 
-    $package = $args[0];
+		$package = $args[0];
 
-    switch ( $type ) {
-      case 'vcs':
-        $this->install_vcs_package( $package, $version, $repo );
-        break;
+		switch ( $type ) {
+			case 'vcs':
+				$this->install_vcs_package( $package, $version, $repo );
+				break;
 
-      default:
-        $this->install_packagist_package( $package, $version );
-        break;
-    }
+			default:
+				$this->install_packagist_package( $package, $version );
+				break;
+		}
 
-  }
+	}
 
-  /**
-   * Remove a community package
-   *
-   * ## OPTIONS
-   *
-   * <package>
-   * : the slug of the package
-   *
-   * @subcommand remove
-   *
-   * @when before_wp_load
-   */
-  public function remove( $args, $assoc_args ) {
-    $package = $args[0];
+	/**
+	 * Remove a community package
+	 *
+	 * ## OPTIONS
+	 *
+	 * <package>
+	 * : the slug of the package
+	 *
+	 * @subcommand remove
+	 *
+	 * @when before_wp_load
+	 */
+	public function remove( $args, $assoc_args ) {
+		$package = $args[0];
 
-    $this->remove_package( $package );
-  }
+		$this->remove_package( $package );
+	}
 
-  private function create_composer_file() {
-    chdir( $this->home );
+	private function create_composer_file() {
+		chdir( $this->home );
 
-    exec( 'composer init --no-interaction --name="wp-cli/packages" --description="WP-CLI community packages"' );
+		exec( 'composer init --no-interaction --name="wp-cli/packages" --description="WP-CLI community packages"' );
 
-    chdir( $this->current );
-  }
+		chdir( $this->current );
+	}
 
-  private function install_packagist_package( $package, $version ) {
-    chdir( $this->home );
+	private function install_packagist_package( $package, $version ) {
+		chdir( $this->home );
 
-    // [TODO] implement packagist installs
-    exec( "composer require {$package}:{$version}" );
+		// [TODO] implement packagist installs
+		exec( "composer require {$package}:{$version}" );
 
-    exec( 'composer update' );
+		exec( 'composer update' );
 
-    chdir( $this->current );
-  }
+		chdir( $this->current );
+	}
 
-  private function install_satis_package( $package, $type, $url ) {
-    chdir( $this->home );
+	private function install_satis_package( $package, $type, $url ) {
+		chdir( $this->home );
 
-    // [TODO] implement Satis installs
+		// [TODO] implement Satis installs
 
-    chdir( $this->current );
-  }
+		chdir( $this->current );
+	}
 
-  private function install_vcs_package( $package, $version, $repo_url ) {
-    chdir( $this->home );
+	private function install_vcs_package( $package, $version, $repo_url ) {
+		chdir( $this->home );
 
-    // let composer know where to find this package
-    exec( "composer config repositories.{$package} vcs {$repo_url}" );
+		// let composer know where to find this package
+		exec( "composer config repositories.{$package} vcs {$repo_url}" );
 
-    // add the package to the require dependencies
-    exec( "composer require {$package}:{$version}" );
+		// add the package to the require dependencies
+		exec( "composer require {$package}:{$version}" );
 
-    // create/update the lock file
-    exec( 'composer update' );
+		// create/update the lock file
+		exec( 'composer update' );
 
-    chdir( $this->current );
-  }
+		chdir( $this->current );
+	}
 
-  private function remove_package( $package ) {
-    chdir( $this->home );
+	private function remove_package( $package ) {
+		chdir( $this->home );
 
-    exec( "composer remove {$package}" );
+		exec( "composer remove {$package}" );
 
-    exec( 'composer update' );
+		exec( 'composer update' );
 
-    chdir( $this->current );
-  }
+		chdir( $this->current );
+	}
 
 }
 
