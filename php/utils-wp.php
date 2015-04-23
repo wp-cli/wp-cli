352d3c4883da6572ccfadd8aab80498d971af629
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-23 05:57:21 -0700

    Periodically clear object cache on import
    WordPress' internal object cache can grow until all memory is consumed
    because cache invalidation is disabled. On large imports, this can fatal
    the import.

--- php/utils-wp.php
+++ php/utils-wp.php
@@ -131,3 +131,25 @@ function wp_register_unused_sidebar() {
 	));
 
 }
+
+/**
+ * Clear all of the caches for memory management
+ */
+function wp_clear_object_cache() {
+	global $wpdb, $wp_object_cache;
+
+	$wpdb->queries = array(); // or define( 'WP_IMPORTING', true );
+
+	if ( ! is_object( $wp_object_cache ) ) {
+		return;
+	}
+
+	$wp_object_cache->group_ops = array();
+	$wp_object_cache->stats = array();
+	$wp_object_cache->memcache_debug = array();
+	$wp_object_cache->cache = array();
+
+	if ( is_callable( $wp_object_cache, '__remoteset' ) ) {
+		$wp_object_cache->__remoteset(); // important
+	}
+}
