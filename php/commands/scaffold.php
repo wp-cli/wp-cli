f2c31cf28a59cb07a4006aba61904361726a9950
miya0001 <miya@wpist.me>
2015-05-11 15:27:01 +0900

    elseif to elseif :)

--- php/commands/scaffold.php
+++ php/commands/scaffold.php
@@ -528,7 +528,7 @@ class Scaffold_Command extends WP_CLI_Command {
 				WP_CLI::error( 'Invalid plugin specified.' );
 			}
 			$plugin_slug =  basename( $plugin_dir );
-		} elseif ( ! empty( $args[0] ) ) {
+		} else if ( ! empty( $args[0] ) ) {
 			$plugin_slug = $args[0];
 			$plugin_dir = WP_PLUGIN_DIR . "/$plugin_slug";
 		} else {
