505023389cac7bdda02bc1b4b097563a7facf2a5
Steve Grunwell <stevegrunwell@gmail.com>
2015-08-15 11:37:31 -0400

    Stick closer to the original language, since 'any' isn't truly the default value

--- php/commands/export.php
+++ php/commands/export.php
@@ -33,7 +33,7 @@ class Export_Command extends WP_CLI_Command {
 	 *
 	 * [--post_type=<post-type>]
 	 * : Export only posts with this post_type. Separate multiple post types with a
-	 * comma. Defaults to "any".
+	 * comma. Defaults to all.
 	 *
 	 * [--post__in=<pid>]
 	 * : Export all posts specified as a comma-separated list of IDs.
