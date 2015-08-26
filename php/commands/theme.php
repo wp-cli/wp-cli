284a1aa6df81c95017656a17b4298e72e123035b
Daniel Bachhuber <daniel.bachhuber@fusion.net>
2015-08-26 09:19:25 -0700

    Correct wording in theme documentation

--- php/commands/theme.php
+++ php/commands/theme.php
@@ -55,7 +55,7 @@ class Theme_Command extends \WP_CLI\CommandWithUpgrade {
 	 * : Optional number of results to display. Defaults to 10.
 	 *
 	 * [--field=<field>]
-	 * : Prints the value of a single field for each plugin.
+	 * : Prints the value of a single field for each theme.
 	 *
 	 * [--fields=<fields>]
 	 * : Ask for specific fields from the API. Defaults to name,slug,author,rating. Acceptable values:
@@ -447,7 +447,7 @@ class Theme_Command extends \WP_CLI\CommandWithUpgrade {
 	 * : Output summary as table or summary. Defaults to table.
 	 *
 	 * [--version=<version>]
-	 * : If set, the plugin will be updated to the specified version.
+	 * : If set, the theme will be updated to the specified version.
 	 *
 	 * [--dry-run]
 	 * : Preview which themes would be updated.
