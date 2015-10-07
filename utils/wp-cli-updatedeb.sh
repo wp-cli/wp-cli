095773b6ae125675ad16e56eed54251c87c132ae
Robin Schneider <ypid@riseup.net>
2015-10-08 00:15:16 +0200

    Support MariaDB client library.
    When mariadb-client is already installed on a system, installing wpcli
    currently requires to remove mariadb-client because mariadb-client and
    mysql-client are in conflict.

--- utils/wp-cli-updatedeb.sh
+++ utils/wp-cli-updatedeb.sh
@@ -31,7 +31,7 @@ Architecture: all
 Maintainer: Daniel Bachhuber <daniel@handbuilt.co>
 Section: php
 Priority: optional
-Depends: php5-cli, php5-mysql | php5-mysqlnd, mysql-client
+Depends: php5-cli, php5-mysql | php5-mysqlnd, mysql-client | mariadb-client
 Homepage: http://wp-cli.org/
 Description: wp-cli is a set of command-line tools for managing
  WordPress installations. You can update plugins, set up multisite
