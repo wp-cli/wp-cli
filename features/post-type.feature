0d5afaab128a6a541f9992b8b8dae9cf59c1b63d
Rodrigo Primo <rodrigo@hacklab.com.br>
2015-10-27 08:35:09 -0200

    add subcommand `wp post-type get <post-type>`

--- features/post-type.feature
+++ features/post-type.feature
@@ -9,3 +9,16 @@ Feature: Manage WordPress post types
   | name | label | description | hierarchical | public | capability_type |
   | post | Posts |             |              | 1      | post            |
   | page | Pages |             | 1            | 1      | page            |
+
+  Scenario: Get a post type
+    When I try `wp post-type get invalid-post-type`
+    Then STDERR should be:
+      """
+      Error: Post type invalid-post-type doesn't exist.
+      """
+
+    When I run `wp post-type get page`
+    Then STDOUT should be a table containing rows:
+      | Field       | Value     |
+      | name        | page      |
+      | label       | Pages     |
--- php/commands/post-type.php
+++ php/commands/post-type.php
@@ -61,6 +61,58 @@ class Post_Type_Command extends WP_CLI_Command {
 		$formatter->display_items( $types );
 	}
 
+	/**
+	 * Get a post type
+	 *
+	 * ## OPTIONS
+	 *
+	 * <post-type>
+	 * : Post type slug
+	 *
+	 * [--field=<field>]
+	 * : Instead of returning the whole taxonomy, returns the value of a single field.
+	 *
+	 * [--fields=<fields>]
+	 * : Limit the output to specific fields. Defaults to all fields.
+	 *
+	 * [--format=<format>]
+	 * : Accepted values: table, json, csv. Default: table
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp post-type get page --format=json
+	 */
+	public function get( $args, $assoc_args ) {
+		$post_type = get_post_type_object( $args[0] );
+
+		if ( ! $post_type ) {
+			WP_CLI::error( "Post type {$args[0]} doesn't exist." );
+		}
+
+		if ( empty( $assoc_args['fields'] ) ) {
+			$default_fields = array_merge( $this->fields, array(
+				'labels',
+				'cap'
+			) );
+
+			$assoc_args['fields'] = $default_fields;
+		}
+
+		$data = array(
+			'name'            => $post_type->name,
+			'label'           => $post_type->label,
+			'description'     => $post_type->description,
+			'hierarchical'    => $post_type->hierarchical,
+			'public'          => $post_type->public,
+			'capability_type' => $post_type->capability_type,
+			'labels'          => $post_type->labels,
+			'cap'             => $post_type->cap,
+		);
+
+		$formatter = $this->get_formatter( $assoc_args );
+		$formatter->display_item( $data );
+	}
+
 	private function get_formatter( &$assoc_args ) {
 		return new \WP_CLI\Formatter( $assoc_args, $this->fields, 'post-type' );
 	}
