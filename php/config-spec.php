89780321c294eaf48ddaf315217e2a12eecb95ac
Viktor Szépe <viktor@szepe.net>
2015-11-03 23:43:41 +0100

    Suppressing "Deprecated: Methods with the same name..." on PHP7 only

--- php/config-spec.php
+++ php/config-spec.php
@@ -66,7 +66,7 @@ return array(
 	'debug' => array(
 		'runtime' => '',
 		'file' => '<bool>',
-		'default' => false,
+		'default' => true,
 		'desc' => 'Show all PHP errors; add verbosity to WP-CLI bootstrap',
 	),
 
