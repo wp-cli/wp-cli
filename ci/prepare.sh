326c3bb2b86ea2639c38d1585fc09dc8d1c06d35
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-21 10:31:51 -0800

    Introduce a dedicated job for testing Git clone installation

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -15,10 +15,17 @@ then
 fi
 
 # the Behat test suite will pick up the executable found in $WP_CLI_BIN_DIR
-mkdir -p $WP_CLI_BIN_DIR
-php -dphar.readonly=0 utils/make-phar.php wp-cli.phar --quiet --version=$CLI_VERSION
-mv wp-cli.phar $WP_CLI_BIN_DIR/wp
-chmod +x $WP_CLI_BIN_DIR/wp
+if [[ $BUILD == 'git' ]]
+then
+	export WP_CLI_BIN_DIR=bin/wp
+	echo $CLI_VERSION > VERSION
+else
+	export WP_CLI_BIN_DIR=/tmp/wp-cli-phar
+	mkdir -p $WP_CLI_BIN_DIR
+	php -dphar.readonly=0 utils/make-phar.php wp-cli.phar --quiet --version=$CLI_VERSION
+	mv wp-cli.phar $WP_CLI_BIN_DIR/wp
+	chmod +x $WP_CLI_BIN_DIR/wp
+fi
 
 # Install CodeSniffer things
 ./ci/prepare-codesniffer.sh
