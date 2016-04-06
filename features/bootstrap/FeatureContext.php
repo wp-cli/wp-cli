4648af607bdda3327220c98c055c4969b789fedd
Daniel Bachhuber <daniel@handbuilt.co>
2016-04-06 15:19:43 -0700

    Ensure vendor libraries are available
    When Behat has been globally installed, our project autoload isn't
    always called.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -30,6 +30,7 @@ if ( file_exists( __DIR__ . '/utils.php' ) ) {
 } else {
 	require_once __DIR__ . '/../../php/utils.php';
 	require_once __DIR__ . '/../../php/WP_CLI/Process.php';
+	require_once __DIR__ . '/../../vendor/autoload.php';
 }
 
 /**
