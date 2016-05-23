da74d1ad05c930e86b07dc7ee2aadca4f41168ad
ernilambar <nilambar@outlook.com>
2016-05-23 16:02:10 +0545

    Examples for transient main class

--- php/commands/transient.php
+++ php/commands/transient.php
@@ -5,7 +5,25 @@
  *
  * ## EXAMPLES
  *
- *     wp transient set my_key my_value 300
+ *     # Set transient
+ *     $ wp transient set sample_key "test data" 3600
+ *     Success: Transient added.
+ *
+ *     # Get transient
+ *     $ wp transient get sample_key
+ *     test data
+ *
+ *     # Delete transient
+ *     $ wp transient delete sample_key
+ *     Success: Transient deleted.
+ *
+ *     # Delete expired transients
+ *     $ wp transient delete-expired
+ *     Success: 12 expired transients deleted from the database.
+ *
+ *     # Delete all transients
+ *     $ wp transient delete-all
+ *     Success: 14 transients deleted from the database.
  */
 class Transient_Command extends WP_CLI_Command {
 
