e3c435d8862a01ebd595fa85f25d0c14aca3b075
ernilambar <nilambar@outlook.com>
2016-05-26 16:37:50 +0545

    Examples for media main class

--- php/commands/media.php
+++ php/commands/media.php
@@ -3,6 +3,20 @@
 /**
  * Manage attachments.
  *
+ * ## EXAMPLES
+ *
+ *     # Re-generate all thumbnails, without confirmation
+ *     $ wp media regenerate --yes
+ *     Found 3 images to regenerate.
+ *     Regenerated thumbnails for "Sydney Harbor Bridge" (ID 760).
+ *     Regenerated thumbnails for "Boardwalk" (ID 757).
+ *     Regenerated thumbnails for "Sunburst Over River" (ID 756).
+ *     Success: Finished regenerating all images.
+ *
+ *     # Import a local image and set it to be the featured image for a post
+ *     $ wp media import ~/Downloads/image.png --post_id=123 --title="A downloaded picture" --featured_image
+ *     Success: Imported file /home/person/Downloads/image.png as attachment ID 1753 and attached to post 123 as featured image.
+ *
  * @package wp-cli
  */
 class Media_Command extends WP_CLI_Command {
