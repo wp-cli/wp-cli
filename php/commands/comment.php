016447447f3c053e0ccb5e03433f499a81a92a6e
Nilambar Sharma <nilambar@outlook.com>
2016-05-28 12:01:12 +0545

    Examples for comment main

--- php/commands/comment.php
+++ php/commands/comment.php
@@ -5,8 +5,22 @@
  *
  * ## EXAMPLES
  *
- *     # delete all spam comments.
- *     wp comment delete $(wp comment list --status=spam --format=ids)
+ *     # Create comment
+ *     $ wp comment create --comment_post_ID=15 --comment_content="hello blog" --comment_author="wp-cli"
+ *     Success: Created comment 932.
+ *
+ *     # Update comment
+ *     $ wp comment update 123 --comment_author='That Guy'
+ *     Success: Updated comment 123.
+ *
+ *     # Delete comment
+ *     $ wp comment delete 1337 --force
+ *     Success: Deleted comment 1337.
+ *
+ *     # Delete all spam comments.
+ *     $ wp comment delete $(wp comment list --status=spam --format=ids)
+ *     Success: Deleted comment 264.
+ *     Success: Deleted comment 262.
  *
  * @package wp-cli
  */
