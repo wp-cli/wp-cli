daf08407233c9c435ce41cdbb66255ad946ea2dd
ernilambar <nilambar@outlook.com>
2016-05-31 16:20:56 +0545

    Examples for menu location commands

--- php/commands/menu.php
+++ php/commands/menu.php
@@ -651,13 +651,21 @@ class Menu_Item_Command extends WP_CLI_Command {
  * ## EXAMPLES
  *
  *     # List available menu locations
- *     wp menu location list
+ *     $ wp menu location list
+ *     +----------+-------------------+
+ *     | location | description       |
+ *     +----------+-------------------+
+ *     | primary  | Primary Menu      |
+ *     | social   | Social Links Menu |
+ *     +----------+-------------------+
  *
  *     # Assign the 'primary-menu' menu to the 'primary' location
- *     wp menu location assign primary-menu primary
+ *     $ wp menu location assign primary-menu primary
+ *     Success: Assigned location to menu.
  *
  *     # Remove the 'primary-menu' menu from the 'primary' location
- *     wp menu location remove primary-menu primary
+ *     $ wp menu location remove primary-menu primary
+ *     Success: Removed location from menu.
  */
 class Menu_Location_Command extends WP_CLI_Command {
 
