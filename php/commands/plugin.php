4a4c0c724b3bf26e16689d08ec0564507845c13f
roelveldhuizen <roel@inesta.nl>
2016-06-29 17:38:37 +0200

    #3069 resolve

--- php/commands/plugin.php
+++ php/commands/plugin.php
@@ -335,6 +335,12 @@ class Plugin_Command extends \WP_CLI\CommandWithUpgrade {
 
 			deactivate_plugins( $plugin->file, false, $network_wide );
 
+			if (!is_network_admin()) {
+				update_option('recently_activated', array($plugin->file => time()) + (array)get_option('recently_activated'));
+			} else {
+				update_site_option('recently_activated', array($plugin->file => time()) + (array)get_site_option('recently_activated'));
+			}
+
 			$this->active_output( $plugin->name, $plugin->file, $network_wide, "deactivate" );
 
 			if ( \WP_CLI\Utils\get_flag_value( $assoc_args, 'uninstall' ) ) {
