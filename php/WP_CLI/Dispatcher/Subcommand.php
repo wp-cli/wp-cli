wp-cli/wp-cli

commit 05ff88bf66c7276fc95cd80f9fa8f85547c55b2f

Support registering `default` and `options` for positional/assoc args
`default` fills the value when it isn't set. `options` validates the
filled value against an enum of values.

YAML for the win:
```
--volume=<number>
: Sets the volume.
---
default: 10
---
```

php/WP_CLI/Dispatcher/Subcommand.php
@@ -252,9 +252,47 @@ class Subcommand extends CompositeCommand {
 				implode( ' ', $unknown_positionals ) );
 		}
 
-		list( $errors, $to_unset ) = $validator->validate_assoc(
+		$synopsis_spec = \WP_CLI\SynopsisParser::parse( $synopsis );
+		$i = 0;
+		$errors = array( 'fatal' => array(), 'warning' => array() );
+		foreach( $synopsis_spec as $spec ) {
+			if ( ! empty( $spec['repeating'] ) ) {
+				continue;
+			}
+			if ( 'positional' === $spec['type'] ) {
+				$spec_args = $this->docparser->get_arg_args( $spec['name'] );
+				if ( ! isset( $args[ $i ] ) ) {
+					if ( isset( $spec_args['default'] ) ) {
+						$args[ $i ] = $spec_args['default'];
+					}
+				}
+				if ( isset( $args[ $i ] ) && isset( $spec_args['options'] ) ) {
+					if ( ! in_array( $args[ $i ], $spec_args['options'] ) ) {
+						\WP_CLI::error( 'Invalid value specified for positional arg.' );
+					}
+				}
+				$i++;
+			} else if ( 'assoc' === $spec['type'] ) {
+				$spec_args = $this->docparser->get_param_args( $spec['name'] );
+				if ( ! isset( $assoc_args[ $spec['name'] ] ) ) {
+					if ( isset( $spec_args['default'] ) ) {
+						$assoc_args[ $spec['name'] ] = $spec_args['default'];
+					}
+				}
+				if ( isset( $assoc_args[ $spec['name'] ] ) && isset( $spec_args['options'] ) ) {
+					if ( ! in_array( $assoc_args[ $spec['name'] ], $spec_args['options'] ) ) {
+						$errors['fatal'][ $spec['name'] ] = "Invalid value specified for '{$spec['name']}'";
+					}
+				}
+			}
+		}
+
+		list( $returned_errors, $to_unset ) = $validator->validate_assoc(
 			array_merge( \WP_CLI::get_config(), $extra_args, $assoc_args )
 		);
+		foreach( array( 'fatal', 'warning' ) as $error_type ) {
+			$errors[ $error_type ] = array_merge( $errors[ $error_type ], $returned_errors[ $error_type ] );
+		}
 
 		if ( $this->name != 'help' ) {
 			foreach ( $validator->unknown_assoc( $assoc_args ) as $key ) {
@@ -276,7 +314,7 @@ class Subcommand extends CompositeCommand {
 
 		array_map( '\\WP_CLI::warning', $errors['warning'] );
 
-		return $to_unset;
+		return array( $to_unset, $args, $assoc_args, $extra_args );
 	}
 
 	/**
@@ -294,7 +332,7 @@ class Subcommand extends CompositeCommand {
 			$prompted_once = true;
 		}
 
-		$to_unset = $this->validate_args( $args, $assoc_args, $extra_args );
+		list( $to_unset, $args, $assoc_args, $extra_args ) = $this->validate_args( $args, $assoc_args, $extra_args );
 
 		foreach ( $to_unset as $key ) {
 			unset( $assoc_args[ $key ] );

features/command.feature
@@ -354,3 +354,118 @@ Feature: WP-CLI Commands
       [--meal=<meal>]
           A type of meal.
       """
+
+  Scenario: Register a command with default and accepted arguments.
+    Given an empty directory
+    And a test-cmd.php file:
+      """
+      <?php
+      /**
+       * An amazing command for managing burritos.
+       *
+       * [<bar>]
+       * : This is the bar argument.
+       * ---
+       * default: burrito
+       * ---
+       *
+       * [<shop>]
+       * : This is where you buy burritos.
+       * ---
+       * options:
+       *   - left_coast_siesta
+       *   - cha cha cha
+       * ---
+       *
+       * [--burrito=<burrito>]
+       * : This is the burrito argument.
+       * ---
+       * options:
+       *   - beans
+       *   - veggies
+       * ---
+       *
+       * @when before_wp_load
+       */
+      $foo = function( $args, $assoc_args ) {
+        $out = array(
+          'bar'     => isset( $args[0] ) ? $args[0] : '',
+          'shop'    => isset( $args[1] ) ? $args[1] : '',
+          'burrito' => isset( $assoc_args['burrito'] ) ? $assoc_args['burrito'] : '',
+        );
+        WP_CLI::print_value( $out, array( 'format' => 'yaml' ) );
+      };
+      WP_CLI::add_command( 'foo', $foo );
+      """
+
+    When I run `wp --require=test-cmd.php foo --help`
+    Then STDOUT should contain:
+      """
+      [<bar>]
+          This is the bar argument.
+          ---
+          default: burrito
+          ---
+      """
+    And STDOUT should contain:
+      """
+      [--burrito=<burrito>]
+          This is the burrito argument.
+          ---
+          options:
+            - beans
+            - veggies
+          ---
+      """
+
+    When I run `wp --require=test-cmd.php foo`
+    Then STDOUT should be YAML containing:
+      """
+      bar: burrito
+      shop:
+      burrito:
+      """
+
+    When I run `wp --require=test-cmd.php foo ''`
+    Then STDOUT should be YAML containing:
+      """
+      bar:
+      shop:
+      burrito:
+      """
+
+    When I run `wp --require=test-cmd.php foo apple --burrito=veggies`
+    Then STDOUT should be YAML containing:
+      """
+      bar: apple
+      shop:
+      burrito: veggies
+      """
+
+    When I try `wp --require=test-cmd.php foo apple --burrito=meat`
+    Then STDERR should contain:
+      """
+      Error: Parameter errors:
+       Invalid value specified for 'burrito' (This is the burrito argument.)
+      """
+
+    When I try `wp --require=test-cmd.php foo apple --burrito=''`
+    Then STDERR should contain:
+      """
+      Error: Parameter errors:
+       Invalid value specified for 'burrito' (This is the burrito argument.)
+      """
+
+    When I try `wp --require=test-cmd.php foo apple taco_del_mar`
+    Then STDERR should contain:
+      """
+      Error: Invalid value specified for positional arg.
+      """
+
+    When I run `wp --require=test-cmd.php foo apple 'cha cha cha'`
+    Then STDOUT should be YAML containing:
+      """
+      bar: apple
+      shop: cha cha cha
+      burrito:
+      """

php/WP_CLI/DocParser.php
@@ -2,6 +2,8 @@
 
 namespace WP_CLI;
 
+use Spyc;
+
 /**
  * Parse command attributes from its PHPdoc.
  * Used to determine execution characteristics (arguments, etc.).
@@ -112,6 +114,19 @@ class DocParser {
 	}
 
 	/**
+	 * Get the arguments for a given argument.
+	 *
+	 * @param string $name Argument's doc name.
+	 * @return mixed|null
+	 */
+	public function get_arg_args( $name ) {
+		if ( preg_match( "/\[?<{$name}>.+(\n: (.+?)(\n|$))?\n---\n(.+)\n---/sU", $this->docComment, $matches ) ) {
+			return Spyc::YAMLLoadString( $matches[4] );
+		}
+		return null;
+	}
+
+	/**
 	 * Get the description for a given parameter.
 	 *
 	 * @param string $key Parameter's key.
@@ -124,6 +139,21 @@ class DocParser {
 		}
 
 		return '';
+	}
+
+	/**
+	 * Get the arguments for a given parameter.
+	 *
+	 * @param string $key Parameter's key.
+	 * @return mixed|null
+	 */
+	public function get_param_args( $key ) {
+
+		if ( preg_match( "/\[?--{$key}=.+(\n: (.+?)(\n|$))?\n---\n(.+)\n---/sU", $this->docComment, $matches ) ) {
+			return Spyc::YAMLLoadString( $matches[4] );
+		}
+
+		return null;
 	}
 
 }

php/class-wp-cli.php
@@ -313,7 +313,18 @@ class WP_CLI {
 				$long_desc = '';
 				$bits = explode( ' ', $synopsis );
 				foreach( $args['synopsis'] as $key => $arg ) {
-					$long_desc .= $bits[ $key ] . PHP_EOL . ': ' . $arg['description'] . PHP_EOL . PHP_EOL;
+					$long_desc .= $bits[ $key ] . PHP_EOL . ': ' . $arg['description'] . PHP_EOL;
+					$yamlify = array();
+					foreach( array( 'options', 'default' ) as $key ) {
+						if ( isset( $arg[ $key ] ) ) {
+							$yamlify[ $key ] = $arg[ $key ];
+						}
+					}
+					if ( ! empty( $yamlify ) ) {
+						$long_desc .= \Spyc::YAMLDump( $yamlify );
+						$long_desc .= '---' . PHP_EOL;
+					}
+					$long_desc .= PHP_EOL;
 				}
 				if ( ! empty( $long_desc ) ) {
 					$long_desc = rtrim( $long_desc, PHP_EOL );

tests/test-doc-parser.php
@@ -94,5 +94,46 @@ EOB
 		;
 		$this->assertEquals( $longdesc, $doc->get_longdesc() );
 	}
+
+	public function test_desc_parses_yaml() {
+		$longdesc = <<<EOB
+## OPTIONS
+
+<genre>...
+: Start with one or more genres.
+---
+options:
+  - rock
+  - electronic
+default: rock
+---
+
+--volume=<number>
+: Sets the volume.
+---
+default: 10
+---
+
+--artist=<artist-name>
+: Limit to a specific artist.
+
+## EXAMPLES
+
+wp rock-on electronic --volume=11
+
+EOB;
+		$doc = new DocParser( $longdesc );
+		$this->assertEquals( 'Start with one or more genres.', $doc->get_arg_desc( 'genre' ) );
+		$this->assertEquals( 'Sets the volume.', $doc->get_param_desc( 'volume' ) );
+		$this->assertEquals( array(
+			'options' => array( 'rock', 'electronic' ),
+			'default' => 'rock',
+		), $doc->get_arg_args( 'genre' ) );
+		$this->assertEquals( array(
+			'default' => 10,
+		), $doc->get_param_args( 'volume' ) );
+		$this->assertNull( $doc->get_param_args( 'artist' ) );
+	}
+
 }
 

commit 032dbfe098517a8ddec03c34c7626c8a01fc6f3b

Respect wp-cli.yml default values when applying argument defaults
Bug introduced in 05ff88bf66c7276fc95cd80f9fa8f85547c55b2f

php/WP_CLI/Dispatcher/Subcommand.php
@@ -287,7 +287,7 @@ class Subcommand extends CompositeCommand {
 				$i++;
 			} else if ( 'assoc' === $spec['type'] ) {
 				$spec_args = $docparser->get_param_args( $spec['name'] );
-				if ( ! isset( $assoc_args[ $spec['name'] ] ) ) {
+				if ( ! isset( $assoc_args[ $spec['name'] ] ) && ! isset( $extra_args[ $spec['name'] ] ) ) {
 					if ( isset( $spec_args['default'] ) ) {
 						$assoc_args[ $spec['name'] ] = $spec_args['default'];
 					}