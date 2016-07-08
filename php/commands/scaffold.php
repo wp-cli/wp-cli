9a2673bcfba9dcd835963029afa27f776a53176b
ernilambar <nilambar@outlook.com>
2016-07-08 09:49:50 +0545

    Add doc for generated files in scaffold plugin command

--- php/commands/scaffold.php
+++ php/commands/scaffold.php
@@ -403,6 +403,23 @@ class Scaffold_Command extends WP_CLI_Command {
 	/**
 	 * Generate starter code for a plugin.
 	 *
+	 * ## OVERVIEW
+	 *
+	 * The following files are generated for your plugin by this command:
+	 *
+	 * * `plugin-slug.php` is the main PHP plugin file.
+	 * * `readme.txt` is the readme file for the plugin.
+	 * * `package.json` needed by NPM holds various metadata relevant to the project.
+	 * * `Gruntfile.js` is the JS file containing Grunt tasks.
+	 * * `phpunit.xml.dist` is the configuration file for PHPUnit.
+	 * * `.editorconfig` is the configuration file for Editor.
+	 * * `.gitignore` tells which files (or patterns) git should ignore.
+	 * * `.distignore` tells which files and folders should be ignored in distribution.
+	 * * `.travis.yml` is the configuration file for Travis CI.
+	 * * `bin/install-wp-tests.sh` configures the WordPress test suite and a test database.
+	 * * `tests/bootstrap.php` is the file that makes the current plugin active when running the test suite.
+	 * * `tests/test-sample.php` is a sample file containing the actual tests.
+	 *
 	 * ## OPTIONS
 	 *
 	 * <slug>
