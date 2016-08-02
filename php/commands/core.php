6fa817e4faf31e9c36ac213f2932615038e29285
Rahul Prajapati <rahul.prajapati@rtcamp.com>
2016-08-02 22:52:05 +0530

    Added language o/p in 'version --extra' command doc

--- php/commands/core.php
+++ php/commands/core.php
@@ -930,6 +930,7 @@ EOT;
 	 *     WordPress version: 4.5.2
 	 *     Database revision: 36686
 	 *     TinyMCE version:   4.310 (4310-20160418)
+	 *     Package Language:  en_US
 	 *
 	 * @when before_wp_load
 	 */
