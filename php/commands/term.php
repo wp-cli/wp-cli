b1ee12b923c927eced71789bb635739b29a34263
Nilambar Sharma <nilambar@outlook.com>
2016-08-03 21:09:48 +0545

    Improve doc for term generate command

--- php/commands/term.php
+++ php/commands/term.php
@@ -361,10 +361,16 @@ class Term_Command extends WP_CLI_Command {
 	 * : The taxonomy for the generated terms.
 	 *
 	 * [--count=<number>]
-	 * : How many terms to generate. Default: 100
+	 * : How many terms to generate?
+	 * ---
+	 * default: 100
+	 * ---
 	 *
 	 * [--max_depth=<number>]
-	 * : Generate child terms down to a certain depth. Default: 1
+	 * : Generate child terms down to a certain depth.
+	 * ---
+	 * default: 1
+	 * ---
 	 *
 	 * [--format=<format>]
 	 * : Render output in a particular format.
@@ -377,12 +383,12 @@ class Term_Command extends WP_CLI_Command {
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     # Generate post categories
+	 *     # Generate post categories.
 	 *     $ wp term generate category --count=10
 	 *     Generating terms  100% [=========] 0:02 / 0:02
 	 *
-	 *     # Add meta to every generated term
-	 *     $ wp term generate category --format=ids --count=3 | xargs -0 -d ' ' -I % wp term meta add % foo bar
+	 *     # Add meta to every generated term.
+	 *     $ wp term generate category --format=ids --count=3 | xargs -d ' ' -I % wp term meta add % foo bar
 	 *     Success: Added custom field.
 	 *     Success: Added custom field.
 	 *     Success: Added custom field.
