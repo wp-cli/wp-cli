8ea8ef3085ce19662864834af2e35fa3edfe71aa
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-21 08:28:53 -0700

    Update docs for installing from a .zip

--- php/commands/package.php
+++ php/commands/package.php
@@ -136,20 +136,25 @@ class Package_Command extends WP_CLI_Command {
 	 * * Package name from WP-CLI's package index.
 	 * * Git URL accessible by the current shell user.
 	 * * Path to a directory on the local machine.
+	 * * Local or remote .zip file.
 	 *
-	 * Note: When installing a local directory, WP-CLI simply registers a
+	 * When installing a local directory, WP-CLI simply registers a
 	 * reference to the directory. If you move or delete the directory, WP-CLI's
 	 * reference breaks.
 	 *
+	 * When installing a .zip file, WP-CLI extracts the package to
+	 * `~/.wp-cli/packages/local/<package-name>`.
+	 *
 	 * ## OPTIONS
 	 *
-	 * <name|git|path>
-	 * : Name, git URL, or directory path for the package to install. Names can
-	 * optionally include a version constraint (e.g. wp-cli/server-command:@stable)
+	 * <name|git|path|zip>
+	 * : Name, git URL, directory path, or .zip file for the package to install.
+	 * Names can optionally include a version constraint
+	 * (e.g. wp-cli/server-command:@stable).
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     # Install the latest development version from the package index
+	 *     # Install the latest development version from the package index.
 	 *     $ wp package install wp-cli/server-command
 	 *     Installing package wp-cli/server-command (dev-master)
 	 *     Updating /home/person/.wp-cli/packages/composer.json to require the package...
@@ -167,11 +172,14 @@ class Package_Command extends WP_CLI_Command {
 	 *     ---
 	 *     Success: Package installed successfully.
 	 *
-	 *     # Install the latest stable version
+	 *     # Install the latest stable version.
 	 *     $ wp package install wp-cli/server-command:@stable
 	 *
-	 *     # Install a package hosted at a git URL
+	 *     # Install a package hosted at a git URL.
 	 *     $ wp package install git@github.com:runcommand/hook.git
+	 *
+	 *     # Install a package in a .zip file.
+	 *     $ wp package install google-sitemap-generator-cli.zip
 	 */
 	public function install( $args, $assoc_args ) {
 		list( $package_name ) = $args;
