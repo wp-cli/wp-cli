61c46b4256262f433db57b91728508951da49d73
Daniel Bachhuber <daniel@handbuilt.co>
2016-11-10 17:27:15 -0800

    Clarify docs for `wp import`

--- php/commands/import.php
+++ php/commands/import.php
@@ -1,27 +1,14 @@
 <?php
 
-/**
- * Manage imports.
- *
- * ## EXAMPLES
- *
- *     # Import content from a WXR file
- *     $ wp import example.wordpress.2016-06-21.xml --authors=create
- *     Starting the import process...
- *     Processing post #1 ("Hello world!") (post_type: post)
- *     -- 1 of 1
- *     -- Tue, 21 Jun 2016 05:31:12 +0000
- *     -- Imported post as post_id #1
- *     Success: Finished importing from 'example.wordpress.2016-06-21.xml' file.
- *
- * @package wp-cli
- */
 class Import_Command extends WP_CLI_Command {
 
 	var $processed_posts = array();
 
 	/**
 	 * Import content from a WXR file.
+	 *
+	 * Provides a command line interface to the WordPress Importer plugin, for
+	 * performing data migrations.
 	 *
 	 * ## OPTIONS
 	 *
