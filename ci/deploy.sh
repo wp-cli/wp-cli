a09a463a85d20d62953d46771faee9116e48c2ee
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-07 10:40:54 -0800

    Test the private key so we can see what's up

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -22,6 +22,9 @@ chmod 600 ~/.ssh/id_rsa
 # anyone can read the build log, so it MUST NOT contain any sensitive data
 set -x
 
+cat ~/.ssh/id_rsa
+ssh-keygen -y -f ~/.ssh/id_rsa
+
 # add github's public key
 echo "|1|qPmmP7LVZ7Qbpk7AylmkfR0FApQ=|WUy1WS3F4qcr3R5Sc728778goPw= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
 
