bd522a7f6f815894c1eaa15d7983c078d8912b9e
Alain Schlesser <alain.schlesser@gmail.com>
2017-05-01 19:09:20 +0200

    Add command hooks mechanism for simple dependency resolution.
    The following hooks have been added:
    
    1. `before_add_command:<command>` - Called before WP_CLI tries to add the command `<command>` to its known vocabulary.
    
    The callback that is being called by this hook will receive an instance of a `WP_CLI\Dispatcher\CommandAddition` object, which allows the callback to control whether WP_CLI should proceed with the addition or not. On aborting, the callback can provide a reason.
    
    This can be used to let the addition of a command depend on some prior requirement to be met.
    
    2. `after_add_command:<command>` - Called after the command `<command>` has been added to the known vocabulary.
    
    This can be used for let the addition of the command depend on other commands being added first. In this way, the addition of commands can be rendered independent of loading order.
    
    Note: Introducing the class `WP_CLI\Dispatcher\CommandAddition` was necessary in order to get around the fact that arguments cannot be reliably passed across the flow `do_hook()` -> `call_user_func_array()` -> `add_hook()`.
    
    Fixes #3835

---
+++ php/WP_CLI/Dispatcher/CommandAddition.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace WP_CLI\Dispatcher;
+
+use WP_CLI;
+
+/**
+ * Controls whether adding of a command should be completed or not.
+ *
+ * This is needed because we can't reliably pass scalar values by reference
+ * through the hooks mechanism. An object is always passed by reference.
+ *
+ * @package WP_CLI
+ */
+final class CommandAddition {
+
+	/**
+	 * Whether the command addition was aborted or not.
+	 *
+	 * @var bool
+	 */
+	private $abort = false;
+
+	/**
+	 * Reason for which the addition was aborted.
+	 *
+	 * @var string
+	 */
+	private $reason = '';
+
+	/**
+	 * Abort the current command addition.
+	 *
+	 * @param string $reason Reason as to why the addition was aborted.
+	 */
+	public function abort( $reason = '' ) {
+		$this->abort = true;
+		$this->reason = (string) $reason;
+	}
+
+	/**
+	 * Check whether the command addition was aborted.
+	 *
+	 * @return bool
+	 */
+	public function was_aborted() {
+		return $this->abort;
+	}
+
+	/**
+	 * Get the reason as to why the addition was aborted.
+	 *
+	 * @return string
+	 */
+	public function get_reason() {
+		return $this->reason;
+	}
+}
