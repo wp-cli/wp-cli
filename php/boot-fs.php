37a8a10b19f458105b3cf859732915b433b423b9
Alain Schlesser <alain.schlesser@gmail.com>
2017-05-05 17:12:02 +0200

    Add `WP_CLI\Utils\is_bundled_command( $command )` function.
    This function loads the Composer classmap we've built through the autoloader splitting functionality, and checks whether a given class is contained within that classmap.
    
    It accepts both a fully qualified class name as a string, as well as an object that should be a subtype of `WP_CLI\Dispatcher\CompositeCommand`.
    
    See #3886

--- php/boot-fs.php
+++ php/boot-fs.php
@@ -14,5 +14,11 @@ if ( version_compare( PHP_VERSION, '5.3.0', '<' ) ) {
 
 define( 'WP_CLI_ROOT', dirname( __DIR__ ) );
 
+if ( file_exists( WP_CLI_ROOT . '/vendor/autoload.php' ) ) {
+	define( 'WP_CLI_VENDOR_DIR' , WP_CLI_ROOT . '/vendor' );
+} elseif ( file_exists( dirname( dirname( WP_CLI_ROOT ) ) . '/autoload.php' ) ) {
+	define( 'WP_CLI_VENDOR_DIR' , dirname( dirname( WP_CLI_ROOT ) ) );
+}
+
 include_once WP_CLI_ROOT . '/php/wp-cli.php';
 
