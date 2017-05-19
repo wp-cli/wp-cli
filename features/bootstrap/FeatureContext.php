55663fe4c1fc5de9fb0dcc1bebeeb95878736656
Alain Schlesser <alain.schlesser@gmail.com>
2017-05-19 12:24:36 +0200

    Adapt Behat bootstrap paths to allow for `wp-cli/wp-cli` to be dependency.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -32,7 +32,11 @@ if ( file_exists( __DIR__ . '/utils.php' ) ) {
 	require_once __DIR__ . '/../../php/utils.php';
 	require_once __DIR__ . '/../../php/WP_CLI/Process.php';
 	require_once __DIR__ . '/../../php/WP_CLI/ProcessRun.php';
-	require_once __DIR__ . '/../../vendor/autoload.php';
+	if ( file_exists( __DIR__ . '/../../vendor/autoload.php' ) ) {
+		require_once __DIR__ . '/../../vendor/autoload.php';
+	} else if ( file_exists( __DIR__ . '/../../../../autoload.php' ) ) {
+		require_once __DIR__ . '/../../../../autoload.php';
+	}
 }
 
 /**
