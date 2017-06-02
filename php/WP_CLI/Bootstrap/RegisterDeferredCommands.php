c51885f53334db1ec61fdb63c9a9ca57573e7485
Alain Schlesser <alain.schlesser@gmail.com>
2017-06-02 13:31:32 +0200

    Move deferred command additions to be processed after `'after_wp_load'` hook.
    For the command additions within (mu-)plugins to work, they need to be processed after all plugins have actually been loaded.
    
    Fixes #4122

--- php/WP_CLI/Bootstrap/RegisterDeferredCommands.php
+++ php/WP_CLI/Bootstrap/RegisterDeferredCommands.php
@@ -21,6 +21,18 @@ final class RegisterDeferredCommands implements BootstrapStep {
 	 * @return BootstrapState Modified state to pass to the next step.
 	 */
 	public function process( BootstrapState $state ) {
+		\WP_CLI::add_hook(
+			'after_wp_load',
+			array( $this, 'add_deferred_commands' )
+		);
+
+		return $state;
+	}
+
+	/**
+	 * Add deferred commands that are still waiting to be processed.
+	 */
+	public function add_deferred_commands() {
 		$deferred_additions = \WP_CLI::get_deferred_additions();
 
 		foreach ( $deferred_additions as $name => $addition ) {
@@ -30,7 +42,5 @@ final class RegisterDeferredCommands implements BootstrapStep {
 				$addition['args']
 			);
 		}
-
-		return $state;
 	}
 }
