96aa2d7cc6d9831758ac472aa7fa51490767a427
Daniel Bachhuber <daniel@handbuilt.co>
2017-06-28 06:59:52 -0700

    Clean up the example a bit

--- php/class-wp-cli.php
+++ php/class-wp-cli.php
@@ -358,7 +358,7 @@ class WP_CLI {
 	 * ```
 	 * # Register a custom 'foo' command to output a supplied positional param.
 	 * #
-	 * # $ wp foo bar --baz=qux
+	 * # $ wp foo bar --append=qux
 	 * # Success: bar qux
 	 *
 	 * /**
@@ -367,10 +367,13 @@ class WP_CLI {
 	 *  * <message>
 	 *  * : An awesome message to display
 	 *  *
+	 *  * --append=<message>
+	 *  * : An awesome message to append to the original message.
+	 *  *
 	 *  * @when before_wp_load
 	 *  *\/
-	 * $foo = function( $args, $args_assoc ) {
-	 *     WP_CLI::success( $args[0] . ' ' . $args_assoc['baz'] );
+	 * $foo = function( $args, $assoc_args ) {
+	 *     WP_CLI::success( $args[0] . ' ' . $assoc_args['append'] );
 	 * };
 	 * WP_CLI::add_command( 'foo', $foo );
 	 * ```
