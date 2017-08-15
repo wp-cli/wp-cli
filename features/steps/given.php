f17155b42fe324e14009bd6fa028eb53c25718a2
miya0001 <miya@wpist.me>
2017-08-15 15:19:17 +0900

    fixed typo

--- features/steps/given.php
+++ features/steps/given.php
@@ -79,7 +79,7 @@ $steps->Given( '/^a WP install with debug mode$/',
 	function ( $world ) {
 		$world->install_wp();
 		$extra_php = "define( \"WP_DEBUG\", true );\n";
-		$world->create_config( null, extra_php );
+		$world->create_config( null, $extra_php );
 	}
 );
 
