86b6d32da13b63da4323a78e5b8469c2853baa3f
gitlost <burmartke@gmail.com>
2017-08-28 14:06:50 +0100

    http_request: add url to error msgs, only re-try if local CACert problem.

--- php/utils.php
+++ php/utils.php
@@ -580,13 +580,17 @@ function http_request( $method, $url, $data = null, $headers = array(), $options
 		$request = \Requests::request( $url, $headers, $data, $method, $options );
 		return $request;
 	} catch( \Requests_Exception $ex ) {
+		// CURLE_SSL_CACERT_BADFILE only defined for PHP >= 7.
+		if ( 'curlerror' !== $ex->getType() || ! in_array( curl_errno( $ex->getData() ), array( CURLE_SSL_CONNECT_ERROR, CURLE_SSL_CERTPROBLEM, 77 /*CURLE_SSL_CACERT_BADFILE*/ ), true ) ) {
+			\WP_CLI::error( sprintf( "Failed to get url '%s': %s.", $url, $ex->getMessage() ) );
+		}
 		// Handle SSL certificate issues gracefully
-		\WP_CLI::warning( $ex->getMessage() );
+		\WP_CLI::warning( sprintf( "Re-trying without verify after failing to get verified url '%s' %s.", $url, $ex->getMessage() ) );
 		$options['verify'] = false;
 		try {
 			return \Requests::request( $url, $headers, $data, $method, $options );
 		} catch( \Requests_Exception $ex ) {
-			\WP_CLI::error( $ex->getMessage() );
+			\WP_CLI::error( sprintf( "Failed to get non-verified url '%s' %s.", $url, $ex->getMessage() ) );
 		}
 	}
 }
