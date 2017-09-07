ca8519ead4b6de7c9ee39b8359536511b0f374d7
Gary Jones <gary@gamajo.com>
2017-09-07 01:32:26 +0100

    CS: Fix WordPress.PHP.YodaConditions.NotYoda
    See #4058.

--- php/utils.php
+++ php/utils.php
@@ -166,7 +166,7 @@ function is_path_absolute( $path ) {
 		return true;
 	}
 
-	return $path[0] === '/';
+	return '/' === $path[0];
 }
 
 /**
@@ -418,7 +418,7 @@ function mysql_host_to_cli_args( $raw_host ) {
 		if ( is_numeric( $extra ) ) {
 			$assoc_args['port'] = intval( $extra );
 			$assoc_args['protocol'] = 'tcp';
-		} elseif ( $extra !== '' ) {
+		} elseif ( '' !== $extra ) {
 			$assoc_args['socket'] = $extra;
 		}
 	} else {
@@ -880,7 +880,7 @@ function parse_str_to_argv( $arguments ) {
 	$argv = array_map(
 		function( $arg ) {
 			foreach ( array( '"', "'" ) as $char ) {
-				if ( $char === substr( $arg, 0, 1 ) && $char === substr( $arg, -1 ) ) {
+				if ( substr( $arg, 0, 1 ) === $char && substr( $arg, -1 ) === $char ) {
 					$arg = substr( $arg, 1, -1 );
 					break;
 				}
@@ -923,7 +923,7 @@ function basename( $path, $suffix = '' ) {
 function isPiped() {
 	$shellPipe = getenv( 'SHELL_PIPE' );
 
-	if ( $shellPipe !== false ) {
+	if ( false !== $shellPipe ) {
 		return filter_var( $shellPipe, FILTER_VALIDATE_BOOLEAN );
 	} else {
 		return (function_exists( 'posix_isatty' ) && ! posix_isatty( STDOUT ));
