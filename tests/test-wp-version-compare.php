ca76d34c80d5e6014ac89c63ea4c9e8f792db970
Daniel Bachhuber <daniel@handbuilt.co>
2017-10-03 15:49:51 -0700

    Add tests for `rc` string too

--- tests/test-wp-version-compare.php
+++ tests/test-wp-version-compare.php
@@ -48,6 +48,21 @@ class WPVersionCompareTest extends PHPUnit_Framework_TestCase {
 		$this->assertFalse( Utils\wp_version_compare( '4.9', '>=' ) );
 		$this->assertTrue( Utils\wp_version_compare( '4.9', '<' ) );
 
+		$GLOBALS['wp_version'] = '4.9-rc1-47000';
+		$this->assertTrue( Utils\wp_version_compare( '4.8', '>=' ) );
+		$this->assertFalse( Utils\wp_version_compare( '4.8', '<' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9-alpha-40870-src', '>' ) );
+		$this->assertFalse( Utils\wp_version_compare( '4.9-alpha-40870-src', '=' ) );
+		$this->assertFalse( Utils\wp_version_compare( '4.9-beta1', '<' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9-beta1', '>' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9-beta1-45000', '>' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9-beta2-45550', '>' ) );
+		$this->assertFalse( Utils\wp_version_compare( '4.9-beta2-45550', '<' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9-rc2', '<' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9-rc2-48000', '<' ) );
+		$this->assertFalse( Utils\wp_version_compare( '4.9', '>=' ) );
+		$this->assertTrue( Utils\wp_version_compare( '4.9', '<' ) );
+
 		$GLOBALS['wp_version'] = '4.9';
 		$this->assertTrue( Utils\wp_version_compare( '4.8', '>=' ) );
 		$this->assertFalse( Utils\wp_version_compare( '4.8', '<' ) );
