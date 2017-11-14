436cb41b3ae081635b5166e348d230ecd89c63a2
gitlost <gitlost@users.noreply.github.com>
2017-11-14 15:29:48 +0000

    Typo abitrary -> arbitrary.

--- php/class-wp-cli.php
+++ php/class-wp-cli.php
@@ -398,7 +398,7 @@ class WP_CLI {
 	 *    @type callable $before_invoke Callback to execute before invoking the command.
 	 *    @type callable $after_invoke  Callback to execute after invoking the command.
 	 *    @type string   $shortdesc     Short description (80 char or less) for the command.
-	 *    @type string   $longdesc      Description of abitrary length for examples, etc.
+	 *    @type string   $longdesc      Description of arbitrary length for examples, etc.
 	 *    @type string   $synopsis      The synopsis for the command (string or array).
 	 *    @type string   $when          Execute callback on a named WP-CLI hook (e.g. before_wp_load).
 	 *    @type bool     $is_deferred   Whether the command addition had already been deferred.
