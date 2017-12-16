60aa163a00063c27346024c3c2d5443009c9b449
John Blackbourn <johnbillion@gmail.com>
2017-12-16 14:25:00 +0100

    Add some more suggestions for mis-typed arguments.

--- php/utils.php
+++ php/utils.php
@@ -1100,7 +1100,9 @@ function glob_brace( $pattern, $dummy_flags = null ) {
 function get_suggestion( $target, array $options, $threshold = 2 ) {
 
 	$suggestion_map = array(
+		'add' => 'create',
 		'check' => 'check-update',
+		'capability' => 'cap',
 		'clear' => 'flush',
 		'decrement' => 'decr',
 		'del' => 'delete',
@@ -1116,6 +1118,7 @@ function get_suggestion( $target, array $options, $threshold = 2 ) {
 		'regen' => 'regenerate',
 		'rep' => 'replace',
 		'repl' => 'replace',
+		'trash' => 'delete',
 		'v' => 'version',
 	);
 
