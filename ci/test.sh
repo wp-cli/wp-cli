831a9dc594a92a566934733e40f2c37543c8b2fd
Viktor Szépe <viktor@szepe.net>
2018-05-13 12:36:29 +0200

    install WP

--- ci/test.sh
+++ ci/test.sh
@@ -9,6 +9,8 @@ echo vendor/bin/behat $BEHAT_TAGS --strict features/cli.feature
 
 wget https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar
 
+php -r 'require "features/bootstrap/FeatureContext.php"; $f=new FeatureContext(); $f->install_wp();'
+
 php wp-cli-nightly.phar package install wp-cli/scaffold-package-command
 find /home/travis/.wp-cli/ -type f
 php wp-cli-nightly.phar cli has-command scaffold package
