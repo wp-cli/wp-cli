84b77449b559d90a701f4d1c634842c2c095ae04
Viktor Szépe <viktor@szepe.net>
2018-05-13 12:08:28 +0200

    focus on the error

--- ci/test.sh
+++ ci/test.sh
@@ -5,4 +5,10 @@ set -ex
 BEHAT_TAGS=$(php ci/behat-tags.php)
 
 # Run the functional tests
-vendor/bin/behat $BEHAT_TAGS --strict features/cli.feature
+echo vendor/bin/behat $BEHAT_TAGS --strict features/cli.feature
+
+
+wp package install wp-cli/scaffold-package-command
+wget https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar
+
+php wp-cli-nightly.phar package install wp-cli/scaffold-package-command
