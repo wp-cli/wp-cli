cf9bdf32ae03c4c5e9ac4b67498b3b0a80bb0644
Viktor Szépe <viktor@szepe.net>
2018-05-13 12:14:51 +0200

    fix test
    A line was left there...

--- ci/test.sh
+++ ci/test.sh
@@ -7,8 +7,6 @@ BEHAT_TAGS=$(php ci/behat-tags.php)
 # Run the functional tests
 echo vendor/bin/behat $BEHAT_TAGS --strict features/cli.feature
 
-
-wp package install wp-cli/scaffold-package-command
 wget https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar
 
 php wp-cli-nightly.phar package install wp-cli/scaffold-package-command
