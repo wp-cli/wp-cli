240e3dc26b8b7558430350b2c2c59b261b8f3684
Viktor Szépe <viktor@szepe.net>
2018-05-13 12:22:42 +0200

    go deeper

--- ci/test.sh
+++ ci/test.sh
@@ -10,3 +10,5 @@ echo vendor/bin/behat $BEHAT_TAGS --strict features/cli.feature
 wget https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar
 
 php wp-cli-nightly.phar package install wp-cli/scaffold-package-command
+find /home/travis/.wp-cli/ -type f
+php wp-cli-nightly.phar cli has-command scaffold package
