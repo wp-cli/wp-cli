c235094fa32c1a5eff937ae4fbd10d87497ddb7c
Pascal Birchler <hello@pascalbirchler.ch>
2018-08-04 11:35:28 +0200

    Load CLI_Cache_Command class

--- php/commands/cli.php
+++ php/commands/cli.php
@@ -4,6 +4,10 @@ if ( ! class_exists( 'CLI_Command' ) ) {
 	require_once __DIR__ . '/src/CLI_Command.php';
 }
 
+if ( ! class_exists( 'CLI_Cache_Command' ) ) {
+	require_once __DIR__ . '/src/CLI_Cache_Command.php';
+}
+
 WP_CLI::add_command( 'cli', 'CLI_Command' );
 
 WP_CLI::add_command( 'cli cache', 'CLI_Cache_Command' );
