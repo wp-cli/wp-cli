b23f535221370cafb7e5d9ae32b3d1c963a89124
Vladimir Roudakov <v_roudakov@yahoo.com>
2018-10-28 00:25:54 +1000

    [#4856] Fixed linting.

--- php/utils.php
+++ php/utils.php
@@ -227,7 +227,7 @@ function esc_cmd( $cmd ) {
 }
 
 /**
- * Gets path to Wordpress configuration.
+ * Gets path to WordPress configuration.
  *
  * @return string
  */
@@ -239,8 +239,8 @@ function locate_wp_config() {
 
 		if ( file_exists( ABSPATH . 'wp-config.php' ) ) {
 			$path = ABSPATH . 'wp-config.php';
-    } elseif ( @file_exists( dirname( ABSPATH ) . '/wp-config.php' ) && ! @file_exists( dirname( ABSPATH ) . '/wp-settings.php' ) ) {
-      $path = dirname( ABSPATH ) . '/wp-config.php';
+		} elseif ( @file_exists( dirname( ABSPATH ) . '/wp-config.php' ) && ! @file_exists( dirname( ABSPATH ) . '/wp-settings.php' ) ) {
+			$path = dirname( ABSPATH ) . '/wp-config.php';
 		}
 
 		if ( $path ) {
