88997bc382f397c67148495e96479a82efd8a100
Samy Rabih <contact@lahaut.info>
2020-05-25 21:22:01 +0200

    Upgrade to allow plugin and theme name completion
    Cleaning
    
    Cleaning
    
    IFS fix

--- utils/wp-completion.bash
+++ utils/wp-completion.bash
@@ -3,20 +3,42 @@
 _wp_complete() {
 	local OLD_IFS="$IFS"
 	local cur=${COMP_WORDS[COMP_CWORD]}
-
+	local command=${COMP_WORDS[1]}
+	local subcommand=${COMP_WORDS[2]}
+	
 	IFS=$'\n';  # want to preserve spaces at the end
-	local opts="$(wp cli completions --line="$COMP_LINE" --point="$COMP_POINT")"
+	COMPREPLY=""
 
-	if [[ "$opts" =~ \<file\>\s* ]]
+	if [[ "$command" = "plugin" ]]
 	then
-		COMPREPLY=( $(compgen -f -- $cur) )
-	elif [[ $opts = "" ]]
+		case "$subcommand" in
+		activate|deactivate|update|delete|get|is-active|path|status|uninstall|verify-checksums)
+			COMPREPLY=( $(cd $(wp plugin path); compgen -d -- "${cur}" ) )
+			;;
+		esac
+	elif [[ "$command" = "theme" ]]
 	then
-		COMPREPLY=( $(compgen -f -- $cur) )
-	else
-		COMPREPLY=( ${opts[*]} )
+		case "$subcommand" in
+		activate|delete|disable|enable|is-active|is-installed|path|status|update)
+			COMPREPLY=( $(cd $(wp theme path); compgen -d -- "${cur}" ) )
+			;;
+		esac
 	fi
+	if [[ "$COMPREPLY" = "" ]]
+	then
+		local opts="$(wp cli completions --line="$COMP_LINE" --point="$COMP_POINT")"
 
+		if [[ "$opts" =~ \<file\>\s* ]]
+		then
+			COMPREPLY=( $(compgen -f -- $cur) )
+		elif [[ $opts = "" ]]
+		then
+			COMPREPLY=( $(compgen -f -- $cur) )
+		else
+			COMPREPLY=( ${opts[*]} )
+		fi
+	fi
+	
 	IFS="$OLD_IFS"
 	return 0
 }
