84ac7f85a588b5a2d84d4ab197535cb081f53692
Leon Grünewald <l_gruenewald@web.de>
2020-08-26 21:26:36 +0200

    Remove extra space

--- php/commands/src/Help_Command.php
+++ php/commands/src/Help_Command.php
@@ -117,7 +117,7 @@ class Help_Command extends WP_CLI_Command {
 
 		$pager = getenv( 'PAGER' );
 		if ( false === $pager ) {
-			$pager =  'more';
+			$pager = 'more';
 		}
 
 		// For Windows 7 need to set code page to something other than Unicode (65001) to get around "Not enough memory." error with `more.com` on PHP 7.1+.
