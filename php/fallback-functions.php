683e180de3043c5a939db5fe532702bccd5ac0d8
Alain Schlesser <alain.schlesser@gmail.com>
2021-03-05 16:55:54 +0000

    Only redeclare ini_set() on PHP 8

--- php/fallback-functions.php
+++ php/fallback-functions.php
@@ -4,6 +4,6 @@
  * This file contains fallback functions that might have been disabled but are required nevertheless.
  */
 
-if ( ! function_exists( 'ini_set' ) ) {
+if ( PHP_MAJOR_VERSION >= 8 && ! function_exists( 'ini_set' ) ) {
 	function ini_set() {}
 }
