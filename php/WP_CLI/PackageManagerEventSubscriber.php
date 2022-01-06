a5bfb1ff7855b51f5ea4f145bb5c69d335726bcb
Alain Schlesser <alain.schlesser@gmail.com>
2022-01-06 10:54:53 -0500

    Apply suggestions from code review

--- php/WP_CLI/PackageManagerEventSubscriber.php
+++ php/WP_CLI/PackageManagerEventSubscriber.php
@@ -27,7 +27,7 @@ class PackageManagerEventSubscriber implements EventSubscriberInterface {
 	}
 
 	/**
-	 * Pre install operation message.
+	 * Pre-install operation message.
 	 *
 	 * @param \Composer\Installer\PackageEvent $event
 	 *
@@ -39,7 +39,7 @@ class PackageManagerEventSubscriber implements EventSubscriberInterface {
 	}
 
 	/**
-	 * Post install operation log.
+	 * Post-install operation log.
 	 *
 	 * @param \Composer\Installer\PackageEvent $event
 	 *
