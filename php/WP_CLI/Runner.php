e1a9bc91c5c0c7b59d13ca62889495375f5c0743
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-02-08 03:56:47 -0800

    Abstract further to allow for future additions
    Co-authored-by: Alain Schlesser <alain.schlesser@gmail.com>

--- php/WP_CLI/Runner.php
+++ php/WP_CLI/Runner.php
@@ -1778,9 +1778,15 @@ class Runner {
 		foreach ( $hooks as $hook ) {
 			add_filter( $hook, $wp_cli_filter_active_theme, 999 );
 		}
-		// Remove block pattern loading
-		remove_action( 'init', '_register_theme_block_patterns' );
-		remove_action( 'init', 'gutenberg_register_theme_block_patterns' );
+
+		// Remove theme-related actions not directly tied into the theme lifecycle.
+		$theme_related_actions = [
+			'init' => '_register_theme_block_patterns',          // Block patterns registration in WP Core.
+			'init' => 'gutenberg_register_theme_block_patterns', // Block patterns registration in the GB plugin.
+		];
+		foreach ( $theme_related_actions as $action => $callback ) {
+			remove_action( $action, $callback );
+		}
 
 		// Clean up after the TEMPLATEPATH and STYLESHEETPATH constants are defined
 		WP_CLI::add_wp_hook(
