a79f3f96332fa33aed5448c4df44e6c9a195fb0e
Aditya Anurag <adityaanurag2003@gmail.com>
2025-10-03 22:57:13 +0530

    Fix: Normalize Windows path detection and improve PathTest coverage (fixes #6115)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,19 @@
-<?phpif ( ! defined( 'WP_CLI_ROOT' ) ) {	define( 'WP_CLI_ROOT', dirname( __DIR__ ) );}if ( file_exists( WP_CLI_ROOT . '/vendor/autoload.php' ) ) {	define( 'WP_CLI_VENDOR_DIR', WP_CLI_ROOT . '/vendor' );} elseif ( file_exists( dirname( dirname( WP_CLI_ROOT ) ) . '/autoload.php' ) ) {	define( 'WP_CLI_VENDOR_DIR', dirname( dirname( WP_CLI_ROOT ) ) );}require_once WP_CLI_VENDOR_DIR . '/autoload.php';require_once WP_CLI_ROOT . '/php/utils.php';require_once WP_CLI_ROOT . '/bundle/rmccue/requests/src/Autoload.php';require_once __DIR__ . '/includes/wpdb.php';\WpOrg\Requests\Autoload::register();$wp_cli_bootstrap1 = dirname( __DIR__ ) . '/vendor/wp-cli-tests/bootstrap.php';$wp_cli_bootstrap2 = dirname( __DIR__ ) . '/vendor/wp-cli/wp-cli-tests/tests/bootstrap.php';if ( file_exists( $wp_cli_bootstrap1 ) ) {	require_once $wp_cli_bootstrap1;} elseif ( file_exists( $wp_cli_bootstrap2 ) ) {	require_once $wp_cli_bootstrap2;} else {	fwrite( STDERR, "Could not find wp-cli-tests bootstrap.php\n" );	exit( 1 );}
+<?php
 
+if ( ! defined( 'WP_CLI_ROOT' ) ) {
+	define( 'WP_CLI_ROOT', dirname( __DIR__ ) );
+}
 
+if ( file_exists( WP_CLI_ROOT . '/vendor/autoload.php' ) ) {
+	define( 'WP_CLI_VENDOR_DIR', WP_CLI_ROOT . '/vendor' );
+} elseif ( file_exists( dirname( dirname( WP_CLI_ROOT ) ) . '/autoload.php' ) ) {
+	define( 'WP_CLI_VENDOR_DIR', dirname( dirname( WP_CLI_ROOT ) ) );
+}
 
+require_once WP_CLI_VENDOR_DIR . '/autoload.php';
+require_once WP_CLI_ROOT . '/php/utils.php';
+require_once WP_CLI_ROOT . '/bundle/rmccue/requests/src/Autoload.php';
 
+require_once __DIR__ . '/includes/wpdb.php';
+
+\WpOrg\Requests\Autoload::register();
