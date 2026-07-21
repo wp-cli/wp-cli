1fb78a678fd919b911f27904f71be0ed2e0f272d
Pascal Birchler <pascalb@google.com>
2026-07-21 10:54:45 +0200

    Simplify debug message in Runner::auto_check_update()

--- php/WP_CLI/Runner.php
+++ php/WP_CLI/Runner.php
@@ -2726,7 +2726,7 @@ class Runner {
 			]
 		);
 		if ( $result->return_code ) {
-			WP_CLI::debug( 'Background update check failed: ' . ( is_object( $result ) ? $result->stderr : '' ), 'auto-update' );
+			WP_CLI::debug( 'Background update check failed: ' . $result->stderr, 'auto-update' );
 			return;
 		}
 		$count = $result->stdout;
