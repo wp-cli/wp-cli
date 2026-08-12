wp-cli/wp-cli

commit 3335484b72a537850b45be12a8a2b488c910f77f

Fix ssh option with docker scheme ignored when set at root of `wp-cli.yml` (#6249)
Co-authored-by: swissspidy <841956+swissspidy@users.noreply.github.com>
Co-authored-by: Pascal Birchler <pascalb@google.com>

features/flags.feature
@@ -393,7 +393,7 @@ Feature: Global flags
     When I try `WP_CLI_STRICT_ARGS_MODE=1 wp --debug --ssh=/ --ssh-args="-o BatchMode=yes" --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh '-o BatchMode=yes' -T -vvv '' 'WP_CLI_STRICT_ARGS_MODE=1 wp
+      Running SSH command: ssh '-o BatchMode=yes' -T -vvv '' 'WP_CLI_STRICT_ARGS_MODE=1 WP_CLI_SSH_RUN=1 wp
       """
 
   @skip-windows @skip-macos
@@ -401,7 +401,7 @@ Feature: Global flags
     When I try `wp --debug --ssh=wordpress:/my/path --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -T -vvv 'wordpress' 'cd '\''/my/path'\''; wp
+      Running SSH command: ssh -T -vvv 'wordpress' 'cd '\''/my/path'\''; WP_CLI_SSH_RUN=1 wp
       """
 
   @skip-windows @skip-macos
@@ -413,11 +413,60 @@ Feature: Global flags
       """
 
   @skip-windows @skip-macos
+  Scenario: SSH flag should support Docker Compose Run
+    When I try `WP_CLI_DOCKER_NO_INTERACTIVE=1 wp --debug --ssh=docker-compose-run:user@wordpress --version`
+    Then STDERR should match /Running SSH command: (docker compose|docker-compose) run --user 'user' (-T )?-e WP_CLI_SSH_RUN=1 'wordpress' wp/
+
+  @skip-windows @skip-macos
+  Scenario: Root-level ssh config should support Docker scheme
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: docker:user@wordpress
+      """
+    When I try `WP_CLI_DOCKER_NO_INTERACTIVE=1 wp --debug --version`
+    Then STDERR should contain:
+      """
+      Running SSH command: docker exec --user 'user' 'wordpress' sh -c
+      """
+
+  @skip-windows @skip-macos
+  Scenario: Root-level ssh config should support Docker scheme with an alias
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: docker:user@wordpress
+      @local:
+        path: /var/www/html
+      """
+    When I try `WP_CLI_DOCKER_NO_INTERACTIVE=1 wp @local --debug --version`
+    Then STDERR should contain:
+      """
+      Running SSH command: docker exec --user 'user' --workdir '/var/www/html'
+      """
+
+  Scenario: wp cli info skips WordPress loading when already running inside container
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: docker:user@wordpress
+      """
+    When I try `WP_CLI_SSH_RUN=1 wp cli info --debug`
+    Then STDOUT should contain:
+      """
+      WP-CLI version:
+      """
+    And STDERR should contain:
+      """
+      Skipping SSH from config file: already running inside an SSH/container session.
+      """
+
+  @skip-windows @skip-macos
   Scenario: SSH args should be passed to SSH command
     When I try `wp --debug --ssh=wordpress --ssh-args="-o ConnectTimeout=5" --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh '-o ConnectTimeout=5' -T -vvv 'wordpress' 'wp
+      Running SSH command: ssh '-o ConnectTimeout=5' -T -vvv 'wordpress' 'WP_CLI_SSH_RUN=1 wp
       """
 
   @skip-windows @skip-macos
@@ -425,7 +474,7 @@ Feature: Global flags
     When I try `wp --debug --ssh=wordpress --ssh-args="-o ConnectTimeout=5" --ssh-args="-o ServerAliveInterval=10" --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh '-o ConnectTimeout=5' '-o ServerAliveInterval=10' -T -vvv 'wordpress' 'wp
+      Running SSH command: ssh '-o ConnectTimeout=5' '-o ServerAliveInterval=10' -T -vvv 'wordpress' 'WP_CLI_SSH_RUN=1 wp
       """
 
   @skip-windows @skip-macos

features/aliases.feature
@@ -308,7 +308,69 @@ Feature: Create shortcuts to specific WordPress installs
     When I try `wp @foo --debug --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -T -vvv '' 'wp --debug --version'
+      Running SSH command: ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -T -vvv '' 'WP_CLI_SSH_RUN=1 wp --debug --version'
+      """
+
+  Scenario: Alias can opt out of root-level ssh configuration
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: docker:user@wordpress
+      @local:
+        ssh: false
+      """
+    When I try `wp @local --debug --version`
+    Then STDERR should not contain:
+      """
+      Running SSH command:
+      """
+
+  @skip-windows @skip-macos
+  Scenario: Explicit CLI --ssh flag overrides alias ssh and root ssh
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: docker:root@wordpress
+      @foo:
+        ssh: docker:alias@wordpress
+      """
+    When I try `WP_CLI_DOCKER_NO_INTERACTIVE=1 wp @foo --debug --ssh=docker:cli@wordpress --version`
+    Then STDERR should contain:
+      """
+      Running SSH command: docker exec --user 'cli' 'wordpress' sh -c
+      """
+
+  @skip-windows @skip-macos
+  Scenario: Relative-path alias with inherited root ssh preserves relative path
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: docker:user@wordpress
+      @foo:
+        path: relative/path
+      """
+    When I try `WP_CLI_DOCKER_NO_INTERACTIVE=1 wp @foo --debug --version`
+    Then STDERR should contain:
+      """
+      --workdir 'relative/path'
+      """
+
+  @skip-windows @skip-macos
+  Scenario: Alias inherits root-level proxyjump, key, and ssh_config defaults
+    Given an empty directory
+    And a wp-cli.yml file:
+      """
+      ssh: user@host
+      proxyjump: rootjump
+      key: rootkey.key
+      ssh_config: /root/ssh_config
+      @foo:
+        path: /other/path
+      """
+    When I try `wp @foo --debug --version`
+    Then STDERR should contain:
+      """
+      Running SSH command: ssh -F '/root/ssh_config' -J 'rootjump' -i 'rootkey.key' -T -vvv 'user@host' 'WP_CLI_SSH_RUN=1 env WP_CLI_RUNTIME_ALIAS=
       """
 
   @skip-windows @skip-macos
@@ -384,7 +446,7 @@ Feature: Create shortcuts to specific WordPress installs
     When I try `wp @foo plugin list --debug`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -T -vvv 'user@host' 'cd '\''/path/to/wordpress'\''; wp plugin list --debug'
+      Running SSH command: ssh -T -vvv 'user@host' 'cd '\''/path/to/wordpress'\''; WP_CLI_SSH_RUN=1 wp plugin list --debug'
       """
 
   @skip-windows @skip-macos
@@ -399,7 +461,7 @@ Feature: Create shortcuts to specific WordPress installs
     When I try `wp @foo post create --post_title=My Title --debug`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -T -vvv 'user@host' 'cd '\''/path/to/wordpress'\''; wp post create --post_title=My Title
+      Running SSH command: ssh -T -vvv 'user@host' 'cd '\''/path/to/wordpress'\''; WP_CLI_SSH_RUN=1 wp post create --post_title=My Title
       """
 
   @skip-windows @skip-macos
@@ -415,7 +477,7 @@ Feature: Create shortcuts to specific WordPress installs
     When I try `wp @foo --debug --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -T -vvv 'user@host' 'env WP_CLI_RUNTIME_ALIAS=
+      Running SSH command: ssh -T -vvv 'user@host' 'WP_CLI_SSH_RUN=1 env WP_CLI_RUNTIME_ALIAS=
       """
     And STDERR should contain:
       """
@@ -468,7 +530,7 @@ Feature: Create shortcuts to specific WordPress installs
     When I try `wp @foo --debug --version`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -T -vvv 'user@host' 'env WP_CLI_RUNTIME_ALIAS=
+      Running SSH command: ssh -T -vvv 'user@host' 'WP_CLI_SSH_RUN=1 env WP_CLI_RUNTIME_ALIAS=
       """
     And STDERR should contain:
       """

php/WP_CLI/Configurator.php
@@ -59,6 +59,9 @@ class Configurator {
 	/**
 	 * Arguments that can be used in an alias.
 	 *
+	 * Note: Connection settings ('ssh', 'ssh-args', 'proxyjump', 'key')
+	 * defined at the root level also act as defaults for aliases.
+	 *
 	 * @var array<int, string>
 	 */
 	private static $alias_spec = [
@@ -131,12 +134,13 @@ class Configurator {
 	/**
 	 * Add the given alias to the internal aliases array.
 	 *
-	 * @param string        $key The alias name (with or without @ prefix).
-	 * @param array<mixed>  $value The alias configuration.
-	 * @param string        $yml_file_dir The directory of the YAML file for path resolution.
+	 * @param string       $key The alias name (with or without @ prefix).
+	 * @param array<mixed> $value The alias configuration.
+	 * @param string       $yml_file_dir The directory of the YAML file for path resolution.
+	 * @param array<mixed> $yaml The parsed YAML configuration array.
 	 * @return void
 	 */
-	private function add_alias( $key, $value, $yml_file_dir ) {
+	private function add_alias( $key, $value, $yml_file_dir, $yaml = [] ) {
 		if ( preg_match( '#' . self::ALIAS_REGEX . '#', $key ) ) {
 			// Remove the @ character from the alias name
 			$key = substr( $key, 1 );
@@ -155,9 +159,15 @@ class Configurator {
 
 					// Interpolate environment variables in alias values.
 					$val_arr[ $i ] = self::interpolate_env_vars( $val_arr[ $i ] );
-					if ( 'path' === $i && ! isset( $val_arr['ssh'] ) && is_string( $val_arr[ $i ] ) ) {
-						self::absolutize( $val_arr[ $i ], $yml_file_dir );
-						$val_arr[ $i ] = Path::normalize( $val_arr[ $i ] );
+					if ( 'path' === $i ) {
+						$has_alias_ssh = isset( $value['ssh'] ) && false !== $value['ssh'] && '' !== $value['ssh'];
+						$has_root_ssh  = ( ! isset( $val_arr['ssh'] ) || ( false !== $value['ssh'] && '' !== $value['ssh'] ) )
+							&& ( ! empty( $this->config['ssh'] ) || ! empty( $yaml['ssh'] ) );
+
+						if ( ! $has_alias_ssh && ! $has_root_ssh && is_string( $val_arr[ $i ] ) ) {
+							self::absolutize( $val_arr[ $i ], $yml_file_dir );
+							$val_arr[ $i ] = Path::normalize( $val_arr[ $i ] );
+						}
 					}
 					$this->aliases[ $key ][ $i ] = $val_arr[ $i ];
 					$is_alias                    = true;
@@ -457,13 +467,13 @@ class Configurator {
 		foreach ( $yaml as $key => $value ) {
 			if ( preg_match( '#' . self::ALIAS_REGEX . '#', $key ) ) {
 				if ( is_array( $value ) ) {
-					$this->add_alias( $key, $value, $yml_file_dir );
+					$this->add_alias( $key, $value, $yml_file_dir, $yaml );
 				}
 			} elseif ( 'aliases' === $key ) {
 				if ( is_array( $value ) ) {
 					foreach ( $value as $alias => $alias_config ) {
 						if ( is_string( $alias ) && is_array( $alias_config ) ) {
-							$this->add_alias( $alias, $alias_config, $yml_file_dir );
+							$this->add_alias( $alias, $alias_config, $yml_file_dir, $yaml );
 						}
 					}
 				}
@@ -482,7 +492,7 @@ class Configurator {
 				self::arrayify( $value );
 				$this->config[ $key ] = array_merge( (array) $this->config[ $key ], $value );
 			} else {
-				if ( $current_alias && in_array( $key, self::$alias_spec, true ) ) {
+				if ( $current_alias && in_array( $key, self::$alias_spec, true ) && ! in_array( $key, [ 'ssh', 'ssh-args', 'proxyjump', 'key', 'ssh_config' ], true ) ) {
 					continue;
 				}
 				$this->config[ $key ] = $value;

php/WP_CLI/Runner.php
@@ -987,8 +987,11 @@ class Runner {
 		}
 
 		$env_vars = '';
-		if ( getenv( 'WP_CLI_STRICT_ARGS_MODE' ) ) {
-			$env_vars .= 'WP_CLI_STRICT_ARGS_MODE=1 ';
+		if ( ! isset( $bits['scheme'] ) || 'docker-compose-run' !== $bits['scheme'] ) {
+			if ( getenv( 'WP_CLI_STRICT_ARGS_MODE' ) ) {
+				$env_vars .= 'WP_CLI_STRICT_ARGS_MODE=1 ';
+			}
+			$env_vars .= 'WP_CLI_SSH_RUN=1 ';
 		}
 
 		$wp_binary = Utils\get_env_or_config( 'WP_CLI_SSH_BINARY' ) ?: 'wp';
@@ -1111,6 +1114,28 @@ class Runner {
 			}
 		}
 
+		// Set default values.
+		$scheme = isset( $bits['scheme'] ) ? $bits['scheme'] : null;
+		foreach ( [ 'scheme', 'user', 'host', 'port', 'path', 'key', 'proxyjump', 'ssh_config' ] as $bit ) {
+			if ( empty( $bits[ $bit ] ) ) {
+				// For 'path', only fall back to runner config for docker schemes which require --workdir.
+				// SSH schemes pass alias path via WP_CLI_RUNTIME_ALIAS to the remote WP-CLI instance.
+				$should_fallback = 'path' !== $bit || in_array( $scheme, [ 'docker', 'docker-compose', 'docker-compose-run' ], true );
+
+				if ( $should_fallback ) {
+					if ( ! empty( $this->config[ $bit ] ) && is_scalar( $this->config[ $bit ] ) ) {
+						$bits[ $bit ] = (string) $this->config[ $bit ];
+					} elseif ( ! empty( $this->extra_config[ $bit ] ) && is_scalar( $this->extra_config[ $bit ] ) ) {
+						$bits[ $bit ] = (string) $this->extra_config[ $bit ];
+					}
+				}
+			}
+
+			if ( ! isset( $bits[ $bit ] ) ) {
+				$bits[ $bit ] = null;
+			}
+		}
+
 		if ( ! empty( $this->alias ) ) {
 			$alias_config = isset( $this->aliases[ $this->alias ] ) ? $this->aliases[ $this->alias ] : false;
 
@@ -1173,7 +1198,12 @@ class Runner {
 		}
 
 		if ( 'docker-compose-run' === $bits['scheme'] ) {
-			$command = '%s run %s%s%s%s%s%s %s';
+			$command = '%s run %s%s%s%s%s%s%s %s';
+
+			$env_flags = '-e WP_CLI_SSH_RUN=1 ';
+			if ( getenv( 'WP_CLI_STRICT_ARGS_MODE' ) ) {
+				$env_flags .= '-e WP_CLI_STRICT_ARGS_MODE=1 ';
+			}
 
 			$escaped_command = sprintf(
 				$command,
@@ -1183,6 +1213,7 @@ class Runner {
 				$bits['path'] ? '--workdir ' . escapeshellarg( $bits['path'] ) . ' ' : '',
 				$is_stdout_tty || Utils\get_env_or_config( 'WP_CLI_DOCKER_NO_TTY' ) ? '' : '-T ',
 				$is_stdin_tty || Utils\get_env_or_config( 'WP_CLI_DOCKER_NO_INTERACTIVE' ) ? '' : '-i ',
+				$env_flags,
 				escapeshellarg( $bits['host'] ),
 				$wp_command
 			);
@@ -2025,6 +2056,16 @@ class Runner {
 
 		if ( empty( $this->arguments ) ) {
 			$this->arguments[] = 'help';
+		}
+
+		if ( $this->config['ssh'] ) {
+			// Don't recurse if SSH came from config file and we're already in an SSH/container session.
+			// Still allow SSH if it was explicitly passed via --ssh= on the CLI (present in runtime_config).
+			$ssh_from_cli = isset( $this->runtime_config['ssh'] ) && '' !== $this->runtime_config['ssh'];
+			if ( getenv( 'WP_CLI_SSH_RUN' ) && ! $ssh_from_cli ) {
+				$this->config['ssh'] = false;
+				WP_CLI::debug( 'Skipping SSH from config file: already running inside an SSH/container session. Use the `--ssh` flag to override.', 'bootstrap' );
+			}
 		}
 
 		// Protect 'cli info' from most of the runtime,

commit 0413accbf08087b3d10111d671756c1d39a4c7bb

Fix trailing-newline SSH scenario left failing on main by #6249 (#6387)
Co-authored-by: Claude <noreply@anthropic.com>

features/flags.feature
@@ -493,7 +493,7 @@ Feature: Global flags
     When I try `newline_arg="$(printf 'foo\nx')"; wp --debug --ssh=wordpress option get "${newline_arg%x}"`
     Then STDERR should contain:
       """
-      Running SSH command: ssh -T -vvv 'wordpress' 'wp --debug option get '\''foo
+      Running SSH command: ssh -T -vvv 'wordpress' 'WP_CLI_SSH_RUN=1 wp --debug option get '\''foo
       '\'''
       """
 